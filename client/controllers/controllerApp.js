ca213f1ef6d1f81a34affb1d2ffa2b52c31dbbb8
Casey Flynn <cflynn.us@gmail.com>
2014-06-05 15:58:20 -0700

    conditionally applying class to body element on home page

--- client/controllers/controllerApp.js
+++ client/controllers/controllerApp.js
@@ -3,6 +3,5 @@ var app = require('app');
 app.controller('ControllerApp', ['$scope', '$state', function ($scope, $state) {
   var rootData = {};
   $scope.rootData = rootData;
-
   rootData.state = $state;
 }]);
