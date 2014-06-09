3ae6eeb0b3afa4de78928d77c6f8e9ab0e0399ac
runnabro <tli415@gmail.com>
2014-06-09 11:36:26 -0700

    remove stats, owner info

--- client/app.js
+++ client/app.js
@@ -1,4 +1,4 @@
 var angular  = require('angular');
 var uiRouter = require('angular-ui-router');
-var ace      = require('ace');
+// var ace      = require('ace');
 module.exports = angular.module('app', [uiRouter]);
