76be88a9957337401f3b0629013c4d2e5cf533b5
Casey Flynn <cflynn.us@gmail.com>
2014-06-16 13:29:37 -0700

    comments for clarity

--- client/controllers/header/controllerHeader.js
+++ client/controllers/header/controllerHeader.js
@@ -5,17 +5,20 @@ app.controller('ControllerHeader', ['$scope', '$window', function ($scope, $wind
   $scope.dataHeader = dataHeader;
 
   dataHeader.togglePopover = function (popoverName, eventA) {
-    var $elParent = $(eventA.currentTarget).parent('li.btn').children('div.popover');
     if (dataHeader['show' + popoverName]) {
       eventA.stopPropagation();
       return;
     }
     dataHeader['show' + popoverName] = true;
-    $elParent.off('click').on('click', function (eventC) {
+    // prevent popover from minimizing when clicking inside popover
+    var $elPopover = $(eventA.currentTarget).parent('li.btn').children('div.popover');
+    $elPopover.off('click').on('click', function (eventC) {
       if ($(this).has($(eventC.target))) {
         eventC.stopPropagation();
       }
     });
+    // setTimeout prevents callback registered below from firing for THIS click event
+    // (we want it to fire on the next click instead)
     setTimeout(function () {
       $(window).one('click', function (eventB) {
         $scope.$apply(function () {
