23ec3eb3e1dcff2e6429ea8b2521e105d03faec1
Casey Flynn <cflynn.us@gmail.com>
2014-06-17 13:54:47 -0700

    controller organization and api integration

--- client/controllers/controllerApp.js
+++ client/controllers/controllerApp.js
@@ -2,11 +2,11 @@ var app = require('app');
 app.controller('ControllerApp', [
   '$rootScope',
   '$scope',
-  '$state', function ($rootScope,
-                      $scope,
-                      $state) {
+  '$state',
+  function ($rootScope,
+            $scope,
+            $state) {
   var rootData = {};
   $scope.rootData = rootData;
   rootData.state = $state;
-
 }]);
