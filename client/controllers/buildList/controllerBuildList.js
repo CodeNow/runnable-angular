21fba01ac851e41c4befa495c805a875f4ab37cd
Casey Flynn <cflynn.us@gmail.com>
2014-06-19 18:57:48 -0700

    simplifying out-of-scope click detection for popover views to improve testability and generally please the Angular Gods

--- client/controllers/buildList/controllerBuildList.js
+++ client/controllers/buildList/controllerBuildList.js
@@ -16,33 +16,17 @@ function ControllerBuildList ($scope,
                               async,
                               $window,
                               hasProps) {
-  var dataBuildList = $scope.dataBuildList = {};
 
+  var dataBuildList = $scope.dataBuildList = {};
   dataBuildList.popoverChangeRecipe = {};
 
-  dataBuildList.togglePopover = function (popoverName, eventA) {
-    if (dataBuildList['show' + popoverName]) {
-      eventA.stopPropagation();
-      return;
-    }
+  $scope.$on('app-document-click', function () {
+    dataBuildList['showChangeRecipe'] = false;
+    dataBuildList['popoverChangeRecipe'].filter = '';
+  });
+  dataBuildList.togglePopover = function (popoverName, event) {
+    event.stopPropagation();
     dataBuildList['show' + popoverName] = true;
-    dataBuildList['popover' + popoverName].filter = '';
-    // prevent popover from minimizing when clicking inside popover
-    var $elPopover = $(eventA.currentTarget).children('.popover');
-    $elPopover.off('click').on('click', function (eventC) {
-      if ($(this).has($(eventC.target))) {
-        eventC.stopPropagation();
-      }
-    });
-    // setTimeout prevents callback registered below from firing for THIS click event
-    // (we want it to fire on the next click instead)
-    setTimeout(function () {
-      $(window).one('click', function (eventB) {
-        $scope.$apply(function () {
-          dataBuildList['show' + popoverName] = false;
-        });
-      });
-    }, 1);
   };
 
   async.waterfall([
--- client/controllers/controllerLayout.js
+++ client/controllers/controllerLayout.js
@@ -1,4 +1,12 @@
-var app = require('app');
-app.controller('ControllerLayout', ['$scope', function ($scope) {
-  $scope.dataLayout = {};
-}]);
+var app  = require('app');
+var deps = [
+  '$scope'
+];
+deps.push(ControllerLayout);
+app.controller('ControllerLayout', deps);
+function ControllerLayout ($scope) {
+  var dataLayout = $scope.dataLayout = {};
+  dataLayout.click = function () {
+    $scope.$broadcast('app-document-click');
+  };
+}
