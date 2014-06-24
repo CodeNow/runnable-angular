03062cf3581818c07e9ce31e346ab3c86b6e99f9
Casey Flynn <cflynn.us@gmail.com>
2014-06-24 14:25:16 -0700

    node_modules +coverify +karma-coverage

--- client/controllers/build/controllerBuild.js
+++ client/controllers/build/controllerBuild.js
@@ -14,50 +14,50 @@ function ControllerBuild ($scope,
                           $stateParams) {
   var dataBuild = $scope.dataBuild = {};
 
-  async.waterfall([
-    function tempHelper (cb) {
-      if (user.id()) {
-        cb();
-      } else {
-        user.login('runnableUser9', 'asdfasdf9', function () {
-          cb();
-        });
-      }
-    },
-    function fetchProject (cb) {
-      var projects = user.fetchProjects({
-        ownerUsername: $stateParams.ownerUsername,
-        name:          $stateParams.name
-      }, function (err, body) {
-        if(err) return cb(err); // error handling
-        cb(null, projects.models[0]);
-      });
-    },
-    function fetchEnvironment (project, cb) {
-      // TODO error check
-      var environmentJSON = project.toJSON().environments.filter(hasProps({name: 'master'}))[0];
-      var environment = project.newEnvironment(environmentJSON);
-      cb(null, project, environment);
-    },
-    function fetchBuild (project, environment, cb) {
-      var build = environment.fetchBuild($stateParams.buildId, function (err, body) {
-        if (err) return cb(err); // TODO error handling
-        cb(null, project, environment, build);
-      });
-    },
-    function newBuildVersion (project, environment, build, cb) {
-      var versionId = build.toJSON().versions[0];
-      var version = build.newVersion(versionId);
-      cb(null, project, environment, build, version);
-    },
-    function fetchRootFiles (project, environment, build, version, cb) {
-      var rootDirFiles = version.fetchFiles({Prefix: '/'}, function () {
-        //...... TODO
-      });
-    }
-  ], function (err, project, environment, build) {
-    console.log(arguments);
-    $scope.$apply(function () {});
-  });
+  // async.waterfall([
+  //   function tempHelper (cb) {
+  //     if (user.id()) {
+  //       cb();
+  //     } else {
+  //       user.login('runnableUser9', 'asdfasdf9', function () {
+  //         cb();
+  //       });
+  //     }
+  //   },
+  //   function fetchProject (cb) {
+  //     var projects = user.fetchProjects({
+  //       ownerUsername: $stateParams.ownerUsername,
+  //       name:          $stateParams.name
+  //     }, function (err, body) {
+  //       if(err) return cb(err); // error handling
+  //       cb(null, projects.models[0]);
+  //     });
+  //   },
+  //   function fetchEnvironment (project, cb) {
+  //     // TODO error check
+  //     var environmentJSON = project.toJSON().environments.filter(hasProps({name: 'master'}))[0];
+  //     var environment = project.newEnvironment(environmentJSON);
+  //     cb(null, project, environment);
+  //   },
+  //   function fetchBuild (project, environment, cb) {
+  //     var build = environment.fetchBuild($stateParams.buildId, function (err, body) {
+  //       if (err) return cb(err); // TODO error handling
+  //       cb(null, project, environment, build);
+  //     });
+  //   },
+  //   function newBuildVersion (project, environment, build, cb) {
+  //     var versionId = build.toJSON().versions[0];
+  //     var version = build.newVersion(versionId);
+  //     cb(null, project, environment, build, version);
+  //   },
+  //   function fetchRootFiles (project, environment, build, version, cb) {
+  //     var rootDirFiles = version.fetchFiles({Prefix: '/'}, function () {
+  //       //...... TODO
+  //     });
+  //   }
+  // ], function (err, project, environment, build) {
+  //   console.log(arguments);
+  //   $scope.$apply(function () {});
+  // });
 
 }
