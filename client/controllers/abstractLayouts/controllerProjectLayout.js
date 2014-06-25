87904aa2eb2552ee8093620134d7b8465476665b
Casey Flynn <cflynn.us@gmail.com>
2014-06-25 01:04:44 -0700

    generating project build page links in header template

--- client/controllers/abstractLayouts/controllerProjectLayout.js
+++ client/controllers/abstractLayouts/controllerProjectLayout.js
@@ -8,11 +8,12 @@ app.controller('ControllerProjectLayout', ControllerProjectLayout);
  */
 function ControllerProjectLayout ($scope,
                                   async,
-                                  $stateParams,
                                   user) {
   var dataProjectLayout = $scope.dataProjectLayout = {};
-
   dataProjectLayout.name = $scope.dataApp.state.params.name;
+  dataProjectLayout.getProjectBuildListUrl = function (name) {
+    return '/' + $scope.dataApp.state.params.ownerUsername + '/' + name + '/master/';
+  };
 
   async.waterfall([
     // temporary helper
@@ -40,28 +41,4 @@ function ControllerProjectLayout ($scope,
       dataProjectLayout.projects = projects;
     });
   });
-
-  // dataHeader.togglePopover = function (popoverName, eventA) {
-  //   if (dataHeader['show' + popoverName]) {
-  //     eventA.stopPropagation();
-  //     return;
-  //   }
-  //   dataHeader['show' + popoverName] = true;
-  //   // prevent popover from minimizing when clicking inside popover
-  //   var $elPopover = $(eventA.currentTarget).parent('li.btn').children('div.popover');
-  //   $elPopover.off('click').on('click', function (eventC) {
-  //     if ($(this).has($(eventC.target))) {
-  //       eventC.stopPropagation();
-  //     }
-  //   });
-  //   // setTimeout prevents callback registered below from firing for THIS click event
-  //   // (we want it to fire on the next click instead)
-  //   setTimeout(function () {
-  //     $(window).one('click', function (eventB) {
-  //       $scope.$apply(function () {
-  //         dataHeader['show' + popoverName] = false;
-  //       });
-  //     });
-  //   }, 1);
-  // };
 }
