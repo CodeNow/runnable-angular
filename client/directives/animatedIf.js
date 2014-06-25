dc56e23119e91295f4d1e0e624cdaeda66e49765
Casey Flynn <cflynn.us@gmail.com>
2014-06-24 17:58:45 -0700

    adding in new animated-if directive

---
+++ client/directives/animatedIf.js
@@ -0,0 +1,43 @@
+var app = require('app');
+app.directive('animatedIf', function($animate) {
+  return {
+    transclude: 'element',
+    priority: 600,
+    terminal: true,
+    restrict: 'A',
+    $$tlb: true,
+    scope: {
+      'animatedIf': '=',
+      'animatedIfEnterCallback': '&',
+      'animatedIfLeaveCallback': '&'
+    },
+    link: function ($scope, $element, $attr, ctrl, $transclude) {
+        var block, childScope;
+        $scope.$watch('animatedIf', function ngIfWatchAction(value, oldValue) {
+          if (value) {
+            if (!childScope) {
+              //childScope = $scope.$new();
+              childScope = $scope.$parent.$new();
+              $transclude(childScope, function (clone) {
+                clone[clone.length++] = document.createComment(' end ngIf: ' + $attr.ngIf + ' ');
+                block = {
+                  clone: clone
+                };
+                var callback = !oldValue && $scope.animatedIfEnterCallback ? $scope.animatedIfEnterCallback : (function() {});
+                $animate.enter(clone, $element.parent(), $element, callback);
+              });
+            }
+          } else {
+            if (childScope) {
+              childScope.$destroy();
+              childScope = null;
+            }
+            if (block) {
+              $animate.leave(block.clone, ($scope.animatedIfLeaveCallback || (function() {})));
+              block = null;
+            }
+          }
+        });
+    }
+  };
+});
