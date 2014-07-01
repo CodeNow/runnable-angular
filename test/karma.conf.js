4d4898e8123895ea345f5831771d554bd4a1178e
Casey Flynn <cflynn.us@gmail.com>
2014-06-30 22:39:23 -0700

    adding growl repoter to config

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -75,7 +75,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'unit/**/*.js':               ['browserify', 'sourcemap']
+      'unit/**/*.js': ['browserify', 'sourcemap']
     },
 
 
@@ -89,7 +89,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['saucelabs', 'mocha'],
+    reporters: ['saucelabs', 'mocha', 'growl'],
 
     coverageReporter: {
       type: 'html',
