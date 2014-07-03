6bc7c42699e6ac74bdd9c9a708573a9605cb3a62
Casey Flynn <cflynn.us@gmail.com>
2014-07-03 11:54:22 -0700

    lint fix and replacing src w/ ng-src

--- client/controllers/build/controllerBuild.js
+++ client/controllers/build/controllerBuild.js
@@ -14,10 +14,9 @@ function ControllerBuild (
   $stateParams
 ) {
 
-  this.scope = $scope;
   var self = ControllerBuild;
   var dataBuild = $scope.dataBuild = self.initState($stateParams);
-  angular.extend($scope.dataBuild, self.initPopoverState($stateParams));
+  angular.extend(dataBuild, self.initPopoverState($stateParams));
 
   dataBuild.getPopoverButtonText = self.getPopoverButtonText;
   dataBuild.resetInputModelValue = function (event) {
@@ -48,60 +47,99 @@ function ControllerBuild (
     dataBuild.togglePopover();
   });
 
-  return this;
+  async.waterfall([
+    $scope.dataApp.holdUntilAuth,
+    function fetchProject (cb) {
+      var projects = user.fetchProjects({
+        ownerUsername: $stateParams.ownerUsername,
+        name:          $stateParams.name
+      }, function (err, body) {
+        if(err) return cb(err); // error handling
+        cb(null, projects.models[0]);
+      });
+    },
+    function fetchEnvironment (project, cb) {
+      // TODO error check
+      var environmentJSON = project.toJSON().environments.filter(hasProps({name: 'master'}))[0];
+      var environment = project.newEnvironment(environmentJSON);
+      cb(null, project, environment);
+    },
+    function fetchBuild (project, environment, cb) {
+      var build = environment.fetchBuild($stateParams.buildId, function (err, body) {
+        if (err) return cb(err); // TODO error handling
+        cb(null, project, environment, build);
+      });
+    },
+    function newBuildVersion (project, environment, build, cb) {
+      var versionId = build.toJSON().versions[0];
+      var version = build.newVersion(versionId);
+      cb(null, project, environment, build, version);
+    },
+    function fetchRootFiles (project, environment, build, version, cb) {
+      var rootDirFiles = version.fetchFiles({Prefix: '/'}, function () {
+        //...... TODO
+      });
+    }
+  ], function (err, project, environment, build) {
+    console.log(arguments);
+    $scope.$apply(function () {});
+  });
+
+
 }
 
 ControllerBuild.fetchData = function (
   async,
   user,
-  $stateParams
-) {};
-
-  // async.waterfall([
-  //   function tempHelper (cb) {
-  //     if (user.id()) {
-  //       cb();
-  //     } else {
-  //       user.login('runnableUser9', 'asdfasdf9', function () {
-  //         cb();
-  //       });
-  //     }
-  //   },
-  //   function fetchProject (cb) {
-  //     var projects = user.fetchProjects({
-  //       ownerUsername: $stateParams.ownerUsername,
-  //       name:          $stateParams.name
-  //     }, function (err, body) {
-  //       if(err) return cb(err); // error handling
-  //       cb(null, projects.models[0]);
-  //     });
-  //   },
-  //   function fetchEnvironment (project, cb) {
-  //     // TODO error check
-  //     var environmentJSON = project.toJSON().environments.filter(hasProps({name: 'master'}))[0];
-  //     var environment = project.newEnvironment(environmentJSON);
-  //     cb(null, project, environment);
-  //   },
-  //   function fetchBuild (project, environment, cb) {
-  //     var build = environment.fetchBuild($stateParams.buildId, function (err, body) {
-  //       if (err) return cb(err); // TODO error handling
-  //       cb(null, project, environment, build);
-  //     });
-  //   },
-  //   function newBuildVersion (project, environment, build, cb) {
-  //     var versionId = build.toJSON().versions[0];
-  //     var version = build.newVersion(versionId);
-  //     cb(null, project, environment, build, version);
-  //   },
-  //   function fetchRootFiles (project, environment, build, version, cb) {
-  //     var rootDirFiles = version.fetchFiles({Prefix: '/'}, function () {
-  //       //...... TODO
-  //     });
-  //   }
-  // ], function (err, project, environment, build) {
-  //   console.log(arguments);
-  //   $scope.$apply(function () {});
-  // });
+  $stateParams,
+  cb
+) {
+  async.waterfall([
+    function tempHelper (cb) {
+      if (user.id()) {
+        cb();
+      } else {
+        user.login('runnableUser9', 'asdfasdf9', function () {
+          cb();
+        });
+      }
+    },
+    function fetchProject (cb) {
+      var projects = user.fetchProjects({
+        ownerUsername: $stateParams.ownerUsername,
+        name:          $stateParams.name
+      }, function (err, body) {
+        if(err) return cb(err); // error handling
+        cb(null, projects.models[0]);
+      });
+    },
+    function fetchEnvironment (project, cb) {
+      // TODO error check
+      var environmentJSON = project.toJSON().environments.filter(hasProps({name: 'master'}))[0];
+      var environment = project.newEnvironment(environmentJSON);
+      cb(null, project, environment);
+    },
+    function fetchBuild (project, environment, cb) {
+      var build = environment.fetchBuild($stateParams.buildId, function (err, body) {
+        if (err) return cb(err); // TODO error handling
+        cb(null, project, environment, build);
+      });
+    },
+    function newBuildVersion (project, environment, build, cb) {
+      var versionId = build.toJSON().versions[0];
+      var version = build.newVersion(versionId);
+      cb(null, project, environment, build, version);
+    },
+    function fetchRootFiles (project, environment, build, version, cb) {
+      var rootDirFiles = version.fetchFiles({Prefix: '/'}, function () {
+        //...... TODO
+      });
+    }
+  ], function (err, project, environment, build) {
+    console.log(arguments);
+    $scope.$apply(function () {});
+  });
+};
 
 ControllerBuild.resetInputModelValue = function (event, inputHasBeenClicked) {
   if (event && typeof event.stopPropagation === 'function') {
