57b1d19e3d6724e71f13dee52a0ca6632f52df0e
Casey Flynn <cflynn.us@gmail.com>
2014-07-08 13:01:59 -0700

    refactoring layout into directives

---
+++ client/directives/directiveTabsDropdown.js
@@ -0,0 +1,20 @@
+require('app')
+  .directive('tabsDropdown', factoryTabsDropdown);
+/**
+ * tabsDropdown Directive
+ * @ngInject
+ */
+function factoryTabsDropdown (
+  $timeout,
+  user,
+  holdUntilAuth,
+  async
+) {
+  return {
+    restrict: 'E',
+    templateUrl: 'viewTabsDropdown',
+    replace: true,
+    scope: {},
+    link: function ($scope, element, attrs) {}
+  };
+}
