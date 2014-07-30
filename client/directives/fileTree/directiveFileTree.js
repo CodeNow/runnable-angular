cd300f02d833c2d9f23aadb257edb8a228f2a341
Casey Flynn <cflynn.us@gmail.com>
2014-07-29 21:43:27 -0700

    updating file-tree directive proper names

--- client/directives/fileTree/directiveFileTree.js
+++ client/directives/fileTree/directiveFileTree.js
@@ -13,16 +13,15 @@ function fileTree(
     templateUrl: 'viewFileTree',
     replace: true,
     scope: {
-      version: '=',
+      versionOrContext: '=',
       openFiles: '='
     },
     link: function ($scope, element, attrs) {
       var actions = $scope.actions = {};
       var data = $scope.data = {};
-
       function init() {
-        data.rootDir = $scope.version.newDir({
-          id: $scope.version.id() + 'newdir',
+        data.rootDir = $scope.versionOrContext.newDir({
+          id: $scope.versionOrContext.id() + 'newdir',
           isDir: true,
           path: '',
           name: '/'
@@ -35,23 +34,6 @@ function fileTree(
           init();
         }
       });
-
-      actions.createFile = function () {
-        var name = prompt('please enter name');
-        /*
-        var file = $scope.version.createFile({
-          name: '',
-          path: '/test/',
-          isDir: true
-        }, function () {
-        });
-        var coll = $scope.version.fetchFiles({path: '/'}, function () {});
-        coll.add(file);
-        $timeout(function () {
-          $scope.$apply();
-        });
-        */
-      };
     }
   };
 }
