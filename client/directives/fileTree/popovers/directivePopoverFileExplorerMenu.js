f44852779c567bb8402bc17e63681a15d64b967e
Casey Flynn <cflynn.us@gmail.com>
2014-08-04 17:46:36 -0700

    naming files/folders logic

--- client/directives/fileTree/popovers/directivePopoverFileExplorerMenu.js
+++ client/directives/fileTree/popovers/directivePopoverFileExplorerMenu.js
@@ -33,36 +33,49 @@ function popoverFileExplorerMenu(
         var regexp1 = /^undefined$/;
         var regexp2 = /^undefined \([0-9]+\)$/;
 
-        var models = $scope.dir.contents.models
-          .slice()
-          .filter(function (model) {
+        var names = $scope.dir.contents.models
+          .map(function (model) {
+            return model.attrs.name;
+          })
+          .filter(function (name) {
             // verify model is correct type and has undefined name
-            return regexp1.test(model.attrs.name) || regexp2.test(model.attrs.name);
+            return regexp1.test(name) || regexp2.test(name);
           })
           .sort(function (m1, m2) {
-            var n1 = m1.attrs.name.match(/[0-9]+/);
-            var n2 = m2.attrs.name.match(/[0-9]+/);
+            var n1 = m1.match(/[0-9]+/);
+            var n2 = m2.match(/[0-9]+/);
             if (n1 === null) {
-              n1 = [-1];
+              n1 = ['0'];
             }
             if (n2 === null) {
-              n2 = [-1];
+              n2 = ['0'];
             }
             n1 = parseInt(n1[0]);
             n2 = parseInt(n2[0]);
-            return n1 < n2;
+            return n1 - n2;
           });
 
-          var name = 'undefined';
-          if (models.length) {
-            var lastIndex = models[0].attrs.name.match(/[0-9]+/);
-            if (Array.isArray(lastIndex)) {
-              name += ' (' + (parseInt(lastIndex[0]) + 1) + ')';
-            } else {
-              name += ' (1)';
-            }
+        // let would be nice
+        var index = 0;
+        for (var i=0, len=names.length; i < len; i++) {
+          if (names[i] === 'undefined') {
+            index = 0;
+          } else {
+            index = names[i].match(/[0-9]+/)[0];
           }
-          return name;
+          // find skipped indexes
+          if (index > i) {
+            index = i - 1;
+            break;
+          }
+        }
+
+        var name = 'undefined';
+        if (index > 0) {
+          name += ' (' + (index+1) + ')';
+        }
+
+        return name;
       };
 
       actions.createFile = function () {
