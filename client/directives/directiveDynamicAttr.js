e4f60b2251615c3f5b1beaf0dd96eb7b6821d55d
Casey Flynn <cflynn.us@gmail.com>
2014-08-11 14:45:01 -0700

    new directive for dynamic inclusion of attributes

---
+++ client/directives/directiveDynamicAttr.js
@@ -0,0 +1,41 @@
+require('app')
+  .directive('dynamicAttr', dynamicAttr);
+/**
+ * dynamicAttr Directive
+ * @ngInject
+ */
+function dynamicAttr(
+  keypather
+) {
+  return {
+    restrict: 'A',
+    link: function ($scope, element, attrs) {
+
+      var jQuery = require('jquery');
+      var parsedAttrs = JSON.parse(attrs.dynamicAttr);
+      var properties = Object.keys(parsedAttrs);
+      var values = properties.map(function (prop) {
+        return properties[prop];
+      });
+
+      var scope = {
+        parsedAttrs: parsedAttrs,
+        properties: properties,
+        values: values,
+        jQuery: jQuery,
+        keypather: keypather
+      };
+
+      properties.forEach(function (prop) {
+        $scope.$watch(parsedAttrs[prop], function(val) {
+          if (val) {
+            jQuery(element).attr(prop, keypather.get($scope, parsedAttrs[prop]));
+          } else {
+            jQuery(element).removeAttr(prop);
+          }
+        });
+      }, scope);
+
+    }
+  };
+}
