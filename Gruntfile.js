1c8cb3777d2df424c9b47e34aea05a814d80963a
Casey Flynn <cflynn.us@gmail.com>
2014-08-13 15:56:22 -0700

    grunt host name fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -350,6 +350,9 @@ module.exports = function(grunt) {
     var clientPath = path.join(__dirname, 'client');
     var configObj = {};
     configObj.host = process.env.API_HOST || 'http://api.runnable3.net';
+    if (configObj.host.charAt(configObj.host.length-1) === '/') {
+      configObj.host = configObj.host.substr(0, configObj.host.length-1);
+    }
     var configJSON = JSON.stringify(configObj);
     fs.writeFile(path.join(clientPath, 'config', 'json', 'api.json'), configJSON, function () {
       done();
