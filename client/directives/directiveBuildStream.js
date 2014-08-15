a329df377ae5c8e4d5c6181a126c9d8e9ec90e6a
Nathan219 <Nathan219@gmail.com>
2014-08-15 14:06:59 -0700

    clears out any output that should be hidden due to any \r

--- client/directives/directiveBuildStream.js
+++ client/directives/directiveBuildStream.js
@@ -1,5 +1,8 @@
 require('app')
   .directive('buildStream', buildStream);
+
+
+
 /**
  * @ngInject
  */
@@ -26,13 +29,13 @@ function buildStream(
         if (buildId) {
           var build = $scope.build;
           if (build.succeeded()) {
-            $scope.stream.data = $scope.build.attrs.contextVersions[0].build.log;
+            $scope.stream.data = parseReturns($scope.build.attrs.contextVersions[0].build.log);
             $rootScope.safeApply();
           } else if (build.failed()) {
             var contextVersion = build.contextVersions.models[0];
             if (build.contextVersions.models)
               $scope.stream = {
-                data: contextVersion.attrs.build.log ||
+                data: parseReturns(contextVersion.attrs.build.log) ||
                   contextVersion.attrs.build.error.message ||
                   "Unknown Build Error Occurred"
               };
@@ -48,7 +51,7 @@ function buildStream(
         var build = $scope.build;
         var buildStream = primus.createBuildStream(build);
         var addToStream = function (data) {
-          $scope.stream.data += data;
+          $scope.stream.data += parseReturns(data);
           $rootScope.safeApply();
           jQuery('html, body').scrollTop(10000);
         };
@@ -71,4 +74,37 @@ function buildStream(
       }
     }
   };
+}
+
+function parseReturns(data) {
+  // Split the data by the \n.
+  var splitData= data.split('\n');
+  var parsedData= '';
+  // Each of the strings in the array may have a \r at the end of them
+  splitData.forEach(function (line, index){
+    if (line) {
+      var firstReturn= line.indexOf('\r');
+      if (firstReturn === 0) {
+        line = line.slice(1);
+      }
+      // If the first found return is at the end, or not in it at all, then skip this
+      if (firstReturn !== -1 && firstReturn !== line.length - 1) {
+        // remove the \r from the end
+        var tempLine = line;
+        while(tempLine && tempLine.lastIndexOf('\r') === tempLine.length - 1) {
+          tempLine = tempLine.slice(0, -1);
+        }
+        var lastIndex= tempLine.lastIndexOf('\r');
+        if (lastIndex > 0) {
+          line = line.slice(lastIndex);
+        }
+      }
+      // Since split will cause the last item to be an empty string
+      // if the last character was a \n, we add \ns only when this is NOT the
+      // last item.  If the last item isn't empty, then it didn't have a \n at
+      // the end
+      parsedData += line + ((index !== splitData.length -1) ? '\n' : '');
+    }
+  });
+  return parsedData || data;
 }
