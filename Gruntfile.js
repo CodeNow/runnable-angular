74427454724e7c0d57d54dff5e4276935bcef979
Casey Flynn <cflynn.us@gmail.com>
2014-08-20 09:59:10 -0700

    Ok the git hooks were getting annoying...

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,10 +22,10 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     githooks: {
       all: {
-        'pre-commit':    'jshint:prod',
-        'pre-push':      'bgShell:karma',
-        'post-merge':    'bgShell:npm-install',
-        'post-checkout': 'bgShell:npm-install'
+        'pre-commit':    'jshint:prod'
+      //  'pre-push':      'bgShell:karma',
+      //  'post-merge':    'bgShell:npm-install',
+      //  'post-checkout': 'bgShell:npm-install'
       }
     },
     concurrent: {
