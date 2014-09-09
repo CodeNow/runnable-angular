6f39fda48dc5c2bf8d940bd7568d314d745f370a
SomeKittens <rkoutnik@gmail.com>
2014-09-09 13:51:38 -0700

    initial setup

--- client/directives/directiveRepoList.js
+++ client/directives/directiveRepoList.js
@@ -4,17 +4,27 @@ require('app')
  * @ngInject
  */
 function repoList (
-
+  $rootScope
 ) {
   return {
     restrict: 'E',
     scope: {
-      instance: '='
+      build: '='
     },
     templateUrl: 'viewRepoList',
     replace: true,
-    link: function (elem, $scope) {
-
+    link: function ($scope, elem) {
+      $scope.$watch('build.contextVersions.models[0]', function (n) {
+        if (n && n.fetch) {
+          n.fetch(function (err) {
+            if (err) {
+              throw err;
+            }
+            $rootScope.safeApply();
+            console.log(n.appCodeVersions.models);
+          });
+        }
+      });
     }
   };
 }
