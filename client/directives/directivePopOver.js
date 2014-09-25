c7f35daa164ba366c91c5f3bd7c4257e9781b504
Casey Flynn <cflynn.us@gmail.com>
2014-09-25 15:07:33 -0700

    removing nightmares

--- client/directives/directivePopOver.js
+++ client/directives/directivePopOver.js
@@ -5,78 +5,20 @@ require('app')
  * @ngInject
  */
 function popOver(
-  $templateCache,
-  $timeout,
-  $compile,
   jQuery
 ) {
   return {
     restrict: 'E',
-    //templateUrl: function ($element, attrs) {
-    //  return attrs.template;
-    //},
-    //replace: true,
+    templateUrl: function ($element, attrs) {
+      return attrs.template;
+    },
+    replace: true,
     scope: {
       data: '=',
       actions: '&'
     },
     link: function ($scope, element, attrs) {
-
       var $ = jQuery;
-      var templateJADE = $templateCache.get(attrs.template);
-      var $template = angular.element(templateJADE);
-      var $element;
-      var options;
-      var $body = $('body');
-
-      try {
-        options = JSON.parse(attrs.popOverOptions);
-      } catch (e) {
-        options = {};
-      }
-      options.left = (typeof options.left !== 'undefined') ? options.left : 0;
-      options.top = (typeof options.top !== 'undefined') ? options.top : 0;
-      options.class = (typeof options.class !== 'undefined') ? options.class : false;
-
-      function position () {
-        var $e = $(element);
-        var eStyle = {
-          top: ($e.offset().top + options.top) + 'px',
-          left: ($e.offset().left + options.left) + 'px'
-        };
-        return eStyle;
-      }
-
-      $scope.$watch('data.show', function (show) {
-        if (show) {
-          $element = $compile($template)($scope);
-          $element.css(position());
-          if (options.class) {
-            $element.addClass(options.class);
-          }
-          $body.append($element);
-        } else {
-          if ($element) {
-            /**
-             * Synchronously removing reference to element
-             * in $element so if data.show becomes true again
-             * before this element is removed, the UI will create
-             * a different popover and the original popover will be
-             * removed in the background
-             */
-            var $t = $element;
-            $element = null;
-            $timeout(function () {
-              if ($t) {
-                // will trigger nested directives to remove themselves from DOM
-                angular.element($t).scope().$broadcast('$destroy');
-                $t.remove();
-              }
-            }, 150);
-          }
-        }
-      });
-
       var clickHandler = $.proxy(function (event) {
         event.stopPropagation();
       }, $scope);
