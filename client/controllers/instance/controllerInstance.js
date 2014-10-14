3ff77b605efe8456bc366a1ed8a7e44dc9e1d635
SomeKittens <rkoutnik@gmail.com>
2014-10-13 17:34:20 -0700

    wip

--- client/controllers/instance/controllerInstance.js
+++ client/controllers/instance/controllerInstance.js
@@ -442,12 +442,13 @@ function ControllerInstance(
         githubUsername: $state.params.userName,
         name: $state.params.instanceName
       })
-      .cacheFetch(function updateDom(instance, cached, cb) {
-        if (!instance) {
+      .cacheFetch(function updateDom(instances, cached, cb) {
+        if (!instances.models.length) {
           return cb();
           // TODO
           // return $state.go(404);
         }
+        var instance = instances.models[0];
         instance.state = {
           name: instance.attrs.name + ''
         };
@@ -465,7 +466,8 @@ function ControllerInstance(
         $scope.safeApply();
         cb();
       })
-      .resolve(function (err, instance, cb) {
+      .resolve(function (err, instances, cb) {
+        var instance = instances.models[0];
         if (!keypather.get(instance, 'containers.models') || !instance.containers.models.length) {
           return cb(new Error('Instance not found'));
         }
