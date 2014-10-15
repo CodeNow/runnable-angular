9090e41c7a41663135d47249e4c3cf974a623b56
SomeKittens <rkoutnik@gmail.com>
2014-10-15 16:15:47 -0700

    added fork test

--- test/e2e/forkBox.e2e.js
+++ test/e2e/forkBox.e2e.js
@@ -2,3 +2,21 @@
  * Tests a user's ability to fork a running box
  */
 
+var util = require('./helpers/util');
+
+var InstancePage = require('./pages/InstancePage');
+
+describe('fork box', function() {
+  it('should allow the forking of a box', function() {
+    var instance = new InstancePage('Test-Rename');
+    instance.get();
+
+    instance.gearMenu.forkBox('Test-Fork');
+
+    util.waitForUrl(util.processUrl('/runnable-doobie/Test-Fork'));
+
+    browser.wait(function () {
+      return util.hasClass(instance.statusIcon, 'running');
+    });
+  });
+});
--- test/e2e/popovers/GearMenu.js
+++ test/e2e/popovers/GearMenu.js
@@ -5,12 +5,23 @@ function GearMenu () {
   this.menu = util.createGetter(by.css('#wrapper > main > header > div.secondary-actions > a > div > div.popover-content'));
 
   this.renameItem = util.createGetter(by.cssContainingText('#wrapper > main > header > div.secondary-actions > a > div > div.popover-content > ul > li', 'Rename Box'));
+  this.forkItem = util.createGetter(by.cssContainingText('#wrapper > main > header > div.secondary-actions > a > div > div.popover-content > ul > li', 'Fork to a New Box'));
   this.deleteItem = util.createGetter(by.cssContainingText('#wrapper > main > header > div.secondary-actions > a > div > div.popover-content > ul > li', 'Delete Box'));
 
   this.modalRename = {
+    // This works, but throws specificity warnings
+    // Selecting by CSS throws other errors
     input: util.createGetter(by.model('data.instance.state.name')),
     cancel: util.createGetter(by.buttonText('Cancel')),
     rename: util.createGetter(by.buttonText('Rename Box'))
+  };
+
+  this.modalFork = {
+    // This one needs to be CSS.
+    // Don't ask me why
+    input: util.createGetter(by.css('body > div.modal.modal-fork.ng-scope.in > div > div.modal-body > form > label > div.input-group.validate.ng-valid > span')),
+    fork: util.createGetter(by.buttonText('Fork Box')),
+    cancel: util.createGetter(by.buttonText('Cancel'))
   };
 
   this.modalDelete = util.createGetter(by.buttonText('Delete Box'));
@@ -26,15 +37,17 @@ function GearMenu () {
         return self.gear.get().click();
       }
       return;
+    }).then(function() {
+      return browser.wait(function() {
+        return self.renameItem.get().isDisplayed();
+      });
     });
   };
 
   this.renameBox = function(newName) {
     var self = this;
     this.openIfClosed();
-    browser.wait(function() {
-      return self.renameItem.get().isDisplayed();
-    });
+
     self.renameItem.get().click();
     browser.wait(function() {
       return self.modalRename.input.get().isDisplayed();
@@ -45,12 +58,24 @@ function GearMenu () {
     self.modalRename.rename.get().click();
   };
 
+  this.forkBox = function (forkName) {
+    var self = this;
+    this.openIfClosed();
+
+    self.forkItem.get().click();
+    browser.wait(function() {
+      return self.modalFork.fork.get().isDisplayed();
+    });
+
+    self.modalFork.input.get().clear();
+    self.modalFork.input.get().sendKeys(forkName);
+    self.modalFork.fork.get().click();
+  };
+
   this.deleteBox = function() {
     var self = this;
     this.openIfClosed();
-    browser.wait(function() {
-      return self.deleteItem.get().isDisplayed();
-    });
+
     self.deleteItem.get().click();
     browser.wait(function() {
       return self.modalDelete.get().isDisplayed();
