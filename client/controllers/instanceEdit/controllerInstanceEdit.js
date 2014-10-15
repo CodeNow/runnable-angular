ff4c218b25a22a1ef724ef9400a98e331f0bd18a
Casey Flynn <cflynn.us@gmail.com>
2014-10-15 13:41:57 -0700

    helper restore state

--- client/controllers/instanceEdit/controllerInstanceEdit.js
+++ client/controllers/instanceEdit/controllerInstanceEdit.js
@@ -34,6 +34,12 @@ function ControllerInstanceEdit(
     showExplorer: false
   };
 
+  function instanceDefaultState (instance) {
+    instance.state = {
+      name: instance.attrs.name + '-copy'
+    };
+  }
+
   /*********************************
   * popoverGearMenu
   *********************************/
@@ -81,6 +87,9 @@ function ControllerInstanceEdit(
         // cb() will reset data.instance.state
         // important to call after PATCH
         cb(); //removes modal
+      },
+      cancel: function () {
+        instanceDefaultState(data.instance);
       }
     },
     forkInstance: function (env) {
@@ -257,10 +266,7 @@ function ControllerInstanceEdit(
           // return $state.go(404);
         }
         var instance = instances.models[0];
-        instance.state = {
-          name: instance.attrs.name + ''
-        };
-
+        instanceDefaultState(instance);
         data.instance = instance;
         pgm.data.dataModalRename.instance = instance;
         pgm.data.dataModalDelete.instance = instance;
