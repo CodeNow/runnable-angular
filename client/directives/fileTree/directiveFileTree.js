091104b17d200595a935cf89b8f1d9c4f1444266
Casey Flynn <cflynn.us@gmail.com>
2014-10-21 13:56:46 -0700

    reducing inputs to directiveFileTree

--- client/directives/fileTree/directiveFileTree.js
+++ client/directives/fileTree/directiveFileTree.js
@@ -5,21 +5,76 @@ require('app')
  * @ngInject
  */
 function fileTree(
-  $timeout,
-  keypather
+  async,
+  QueryAssist,
+  $rootScope,
+  $state,
+  $stateParams,
+  user
 ) {
   return {
     restrict: 'E',
     templateUrl: 'viewFileTree',
     replace: true,
     scope: {
-      readOnly: '=',
-      rootDir: '=',
       openItems: '='
     },
     link: function ($scope, element, attrs) {
+
       var actions = $scope.actions = {};
       var data = $scope.data = {};
+
+      switch($state.$current.name) {
+        case 'instance.edit':
+          break;
+        case 'instance.instance':
+          break;
+        case 'instance.setup':
+          $scope.readOnly = false;
+          break;
+      }
+
+      function fetchUser (cb) {
+        new QueryAssist(user, cb)
+          .wrapFunc('fetchUser')
+          .query('me')
+          .cacheFetch(function (user, cached, cb) {
+            $scope.user = user;
+            $rootScope.safeApply();
+            cb();
+          })
+          .resolve(function (err, user, cb) {
+            if (err) throw err;
+            cb();
+          })
+          .go();
+      }
+
+      function fetchBuild (cb) {
+        new QueryAssist($scope.user, cb)
+          .wrapFunc('fetchBuild')
+          .query($stateParams.buildId)
+          .cacheFetch(function (build, cached, cb) {
+            $scope.build = build;
+            $rootScope.safeApply();
+            cb();
+          })
+          .resolve(function (err, build, cb) {
+            if (err) throw err;
+            cb();
+          })
+          .go();
+      }
+
+      async.series([
+        fetchUser,
+        fetchBuild
+      ], function (err) {
+        if (err) throw err;
+        $scope.rootDir = $scope.build.contextVersions.models[0].rootDir;
+        $rootScope.safeApply();
+      });
+
       $scope.$watch('rootDir', function (newVal, oldVal) {
         if (newVal) {
           var rootDir = $scope.rootDir;
