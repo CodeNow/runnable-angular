055d1ca4c9de564d10e07243a9fe5392f1cadf82
SomeKittens <rkoutnik@gmail.com>
2014-10-24 10:38:34 -0700

    slight refactor

--- test/e2e/directives/ActivePanel.js
+++ test/e2e/directives/ActivePanel.js
@@ -4,9 +4,8 @@ function ActivePanel () {
   this.activePanel = util.createGetter(by.css('#wrapper > main > section.views.with-add-tab.ng-scope > div.active-panel.ng-scope.loaded.ace-runnable-dark'));
 
   this.ace = util.createGetter(by.css('#wrapper > main > section.views.ng-scope > div.active-panel.ng-scope.loaded.ace-runnable-dark > pre > div.ace_scroller > div'));
-  // What was this for?
-  this.aceComment = util.createGetter(by.css('#wrapper > main > section.views.ng-scope > div.active-panel.ng-scope.loaded.ace-runnable-dark > pre > div.ace_scroller > div > div.ace_layer.ace_text-layer > div > span'));
-
+  this.aceDiv = util.createGetter(by.css('div.ace_content'));
+  this.inputElm = util.createGetter(by.css('textarea.ace_text-input'));
 
   this.aceLoaded = function () {
     return this.ace.get().isPresent();
@@ -15,11 +14,8 @@ function ActivePanel () {
   // http://stackoverflow.com/q/25675973/1216976
   // https://github.com/angular/protractor/issues/1273
   this.writeToFile = function (contents) {
-    var aceDiv = element(by.css('div.ace_content'));
-    var inputElm = element(by.css('textarea.ace_text-input'));
-
-    browser.actions().doubleClick(aceDiv).perform();
-    return inputElm.sendKeys(contents);
+    browser.actions().doubleClick(this.aceDiv.get()).perform();
+    return this.inputElm.get().sendKeys(contents);
   };
 
   this.clearActiveFile = function () {
