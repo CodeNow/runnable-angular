f2a7a01d58c97822a9305fa9784d6ed4367f0acb
Casey Flynn <cflynn.us@gmail.com>
2014-10-25 19:00:09 -0700

    further sketching out of tests

--- test/unit/directives/directiveRunnableEditRepoCommit.unit.js
+++ test/unit/directives/directiveRunnableEditRepoCommit.unit.js
@@ -4,20 +4,18 @@ var sinon   = require('sinon');
 var colors  = require('colors');
 var angular = require('angular');
 var mocks   = require('../api-mocks');
+var expect  = chai.expect;
 var directiveTemplate = require('../../fixtures/directiveTemplate');
+var host = require('../../../client/config/json/api.json').host;
 require('browserify-angular-mocks');
 
-var host = require('../../../client/config/json/api.json').host;
-
-var expect = chai.expect;
 var $compile,
+    $httpBackend,
     $scope,
     user;
 
 describe('directiveRunnableEditRepoCommit'.bold.underline.blue, function () {
-
   var ctx = {};
-
   describe('basic', function () {
     beforeEach(angular.mock.module('app'));
     beforeEach(function () {
@@ -28,26 +26,43 @@ describe('directiveRunnableEditRepoCommit'.bold.underline.blue, function () {
     beforeEach(function () {
       angular.mock.inject(function (
         _$compile_,
+        _$httpBackend_,
         _$rootScope_,
         _user_
       ) {
         $compile = _$compile_;
+        $httpBackend = _$httpBackend_;
         $scope = _$rootScope_;
         user = _user_;
       });
       ctx.element = angular.element(ctx.template);
     });
-    beforeEach(function (done) {
+    beforeEach(function () {
       user.reset(mocks.user);
       ctx.acv = user
         .newContext('contextId')
         .newVersion('versionId')
-        .newAppCodeVersion(mocks.acv);
-
+        .newAppCodeVersion(mocks.appCodeVersions.index);
       $scope.acv = ctx.acv;
     });
+    /*
+    beforeEach(function (done) {
+      var acvUrl = host + '/' + ctx.acv.path();
+      $httpBackend
+        .when('GET', acvUrl)
+        .respond(mocks.appCodeVersions.index);
+      $httpBackend.expectGET(acvUrl);
+      debugger;
+      ctx.acv.fetch(function () {
+        console.log(arguments);
+        console.log(ctx.acv.attrs);
+        done();
+      });
+    });
+    */
     beforeEach(function () {
       $compile(ctx.element)($scope);
+      debugger;
       $scope.$digest();
     });
     it('should display', function () {
