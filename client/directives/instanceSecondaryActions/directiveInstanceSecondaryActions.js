53c49c81d96da1bc0c9f6198ab28d2c8945b2e8f
Casey Flynn <cflynn.us@gmail.com>
2014-10-26 18:31:28 -0700

    renaming popover view file names

--- client/directives/instanceSecondaryActions/directiveInstanceSecondaryActions.js
+++ client/directives/instanceSecondaryActions/directiveInstanceSecondaryActions.js
@@ -4,13 +4,29 @@ require('app')
  * @ngInject
  */
 function RunnableInstanceSecondaryActions (
+  async,
+  QueryAssist,
+  $rootScope,
+  $state,
+  user
 ) {
   return {
     restrict: 'E',
     templateUrl: 'viewInstanceSecondaryActions',
     replace: true,
-    scope: {},
+    scope: {
+      saving: '='
+    },
     link: function ($scope, elem, attrs) {
+
+      $scope.popoverGearMenu = {data:{}, actions:{}};
+      $scope.popoverGearMenu.data.show = false;
+
+      $scope.saving = false;
+
+      $scope.goToEdit = function () {
+      };
+
     }
   };
 }
