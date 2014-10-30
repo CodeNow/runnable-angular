43f46b593e4aa2a5f52f4775f726a7da13527242
SomeKittens <rkoutnik@gmail.com>
2014-10-29 18:18:10 -0700

    updates to conform with SAN-479 fixes

---
+++ test/unit/directives/directiveRunnableDockerValidation.unit.js
@@ -0,0 +1,77 @@
+var main    = require('main');
+var chai    = require('chai');
+var sinon   = require('sinon');
+var colors  = require('colors');
+var angular = require('angular');
+require('browserify-angular-mocks');
+
+var expect = chai.expect;
+
+describe.only('directiveDockerValidation'.bold.underline.blue, function() {
+  var element;
+  var $scope;
+  var $rootScope;
+  var openItems;
+  var fakeFile;
+  function initState () {
+    angular.mock.module('app');
+    angular.mock.inject(function($compile, _$rootScope_, $timeout){
+      $rootScope = _$rootScope_;
+      $scope = $rootScope.$new();
+
+      $scope.openItems = {
+        activeHistory: {
+          last: function () {
+            return fakeFile;
+          }
+        }
+      };
+      $rootScope.safeApply = function(cb) {
+        $timeout(function() {
+          $scope.$digest();
+        });
+      };
+
+      var tpl = '<runnable-docker-validation ' +
+          'open-items="openItems"' +
+          '></runnable-docker-validation>';
+
+        element = $compile(tpl)($scope);
+        $scope.$digest();
+    });
+  }
+  beforeEach(initState);
+
+  it('Should return valid when given something other than a dockerfile', function() {
+    fakeFile = {
+      attrs: {
+        body: 'Runnable is the best!'
+      },
+      id: function () {
+        return 'Not a Dockerfile';
+      }
+    };
+    $scope.$digest();
+
+    var validDockerfile = element.isolateScope().validDockerfile;
+    expect(validDockerfile).to.be.an.Object;
+    expect(validDockerfile.valid).to.be.true;
+  });
+
+  it('Should run the validator when given a Dockerfile', function() {
+    // Using an invalid dockerfile here - otherwise we can't be sure the validator was run
+    fakeFile = {
+      attrs: {
+        body: 'FROM dockerfile/nodejs\nCDM sleep 123456789'
+      },
+      id: function () {
+        return '/Dockerfile';
+      }
+    };
+    $scope.$digest();
+
+    var validDockerfile = element.isolateScope().validDockerfile;
+    expect(validDockerfile).to.be.an.Object;
+    expect(validDockerfile.valid).to.be.false;
+  });
+});
