db316c0dd066be54dee37769e623ae9bef80ca10
Casey Flynn <cflynn.us@gmail.com>
2014-11-04 10:46:49 -0800

    term fix, file rename

--- client/directives/directiveTerm.js
+++ client/directives/directiveTerm.js
@@ -19,13 +19,19 @@ function term(
     scope: {},
     link: function ($scope, elem) {
 
-      var termStream;
+      var streams, termStream, eventsStream;
       /**
        * creates instance of terminal w/ default 
        * settings and attaches to elem.
        * - unbinds events on $destroy
        */
       var terminal = helperSetupTerminal($scope, elem);
+
+      async.series([
+        fetchUser,
+        fetchInstance
+      ], function () {
+      });
 
       bind(primus, 'offline', function () {
         terminal.writeln('');
@@ -75,7 +81,6 @@ function term(
             }
             var instance = instances.models[0];
             $scope.instance = instance;
-            $scope.build = instance.build;
             $rootScope.safeApply();
           })
           .resolve(function (err, instances, cb) {
@@ -89,78 +94,32 @@ function term(
           .go();
       }
 
-      async.series([
-        fetchUser,
-        fetchInstance
-      ], function () {
-        initializeTerminal();
-      });
-
-      function initializeTerminal() {
-      }
-    }
-  };
-}
-
-
-/*
-      $scope.$watch('params.running()', function (running) {
-        if (!running) {
-          return;
-        }
-        // Numbers chosen erring on the side of padding, will be updated with more accurate numbers later
-        var params = $scope.params;
-        var streams, termStream, clientEvents;
-
-        
-        function createSubstreams(reconnect) {
-          if (reconnect) {
-            terminal.removeAllListeners('data');
-            termStream.removeAllListeners('data');
-          }
-          // Initalize link to server
-          streams = primus.createTermStreams(params);
-          termStream = streams.termStream;
-          clientEvents = streams.eventStream;
-
-          // Client enters data into the system, which registers as data event on terminal
-          // terminal then writes that to termStream, sending the data to the server
-          // The server then responds (tab-complete, command output, etc)
-          // The *response data* is what's eventually written to terminal.
-          terminal.on('data', termStream.write.bind(termStream));
-          termStream.on('data', terminal.write.bind(terminal));
-        }
-
-        createSubstreams();
-
-        function regainedMessage() {
-          createSubstreams(true);
+      function subscribeToSubstream(container) {
+        if (termStream) {
           terminal.writeln('');
           terminal.writeln('Connection regained.  Thank you for your patience');
-        }
-        primus.on('reconnect', regainedMessage);
-
-        function offlineMessage() {
-          terminal.writeln('');
-          terminal.writeln('******************************');
-          terminal.writeln('* LOST CONNECTION - retrying *');
-          terminal.writeln('******************************');
-        }
-
-
-        $scope.$on('$destroy', function () {
-          termStream.end();
-          terminal.destroy();
           terminal.removeAllListeners('data');
           termStream.removeAllListeners('data');
-          terminal.removeAllListeners('end');
-          termStream.removeAllListeners('end');
-          primus.off('reconnect', regainedMessage);
-          primus.off('offline', offlineMessage);
-          jQuery($window).off('resize', dResizeTerm);
-        });
+          terminal.reset();
+        }
+        // Initalize link to server
+        streams      = primus.createTermStreams(container);
+        termStream   = streams.termStream;
+        eventsStream = streams.eventStream;
+
+        // Client enters data into the system, which registers as data event on terminal
+        // terminal then writes that to termStream, sending the data to the server
+        // The server then responds (tab-complete, command output, etc)
+        // The *response data* is what's eventually written to terminal.
+        terminal.on('data', termStream.write.bind(termStream));
+        termStream.on('data', terminal.write.bind(terminal));
+      }
+
+      $scope.$watch('instance.containers.models[0].running()', function (n) {
+        if (!n) return;
+        subscribeToSubstream($scope.instance.containers.models[0]);
       });
+
     }
   };
 }
-*/
