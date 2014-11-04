9bcf3b5e4a7349df21c0bf771ca30d7bd958cc40
Casey Flynn <cflynn.us@gmail.com>
2014-11-04 11:03:32 -0800

    readding...

---
+++ client/services/serviceHelperSetupTerminal.js
@@ -0,0 +1,54 @@
+require('app')
+  .factory('helperSetupTerminal', helperSetupTerminal);
+/**
+ * @ngInject
+ */
+function helperSetupTerminal(
+  configTerminalOpts,
+  debounce,
+  jQuery,
+  termjs,
+  $window
+) {
+  return function ($scope, elem, opts) {
+    var CHAR_HEIGHT = 20;
+    var config = angular.extend({}, configTerminalOpts);
+    config = angular.extend(config, (opts || {}));
+    config.rows = Math.floor(elem[0].clientHeight / CHAR_HEIGHT);
+    var terminal = new termjs(config);
+    terminal.open(elem[0]);
+
+    // Terminal sizing
+    var $termElem = jQuery(terminal.element);
+
+    function resizeTerm() {
+      // Tab not selected
+      if ($termElem.width() === 100) {
+        return;
+      }
+      var termLineEl = $termElem.find('div')[0];
+      if (!termLineEl) {
+        return;
+      }
+      var tBox = termLineEl.getBoundingClientRect();
+      var charWidth = tBox.width / termLineEl.textContent.length;
+      var x = Math.floor($termElem.width() / charWidth);
+      if (x < 80) {
+        x = 80;
+      }
+      var y = Math.floor($termElem.height() / CHAR_HEIGHT);
+      terminal.resize(x, y);
+    }
+
+    var dResizeTerm = debounce(resizeTerm, 300);
+    dResizeTerm();
+
+    jQuery($window).on('resize', dResizeTerm);
+    $scope.$on('$destroy', function () {
+      jQuery($window).off('resize', dResizeTerm);
+      terminal.destroy();
+    });
+
+    return terminal;
+  };
+}
