157fd6c7d803642f5eb68df4ab193c59bb3f9f9f
Nathan219 <Nathan219@gmail.com>
2014-11-13 14:25:51 -0800

    Adding 100% unit test coverage for Env Vars Cleaning up env vars directive

--- client/directives/envVars/directiveEnvVars.js
+++ client/directives/envVars/directiveEnvVars.js
@@ -4,12 +4,7 @@ require('app')
  * @ngInject
  */
 function envVars(
-  async,
-  keypather,
-  QueryAssist,
-  $rootScope,
-  $stateParams,
-  user
+  keypather
 ) {
   return {
     restrict: 'E',
@@ -24,26 +19,29 @@ function envVars(
 
       $scope.environmentalVars = '';
 
+      // Watch the current model for envs
       var unreg = $scope.$watch('currentModel.env', function (env) {
         if (!Array.isArray(env)) {
           return;
         }
-        // houston, this is tranquility base.
-        // we have asynchronously fetched the instance
-        // wtf am I doing?
+        // If we have some, add them to the screen
         $scope.environmentalVars = env.reduce(function (environmentalVars, env) {
           return environmentalVars + env + '\n';
         }, '');
-
-        $scope.validity = {
-          valid: true,
-          errors: []
-        };
       });
 
+      // Add the validity checker
+      $scope.validity = {
+        valid: true,
+        errors: []
+      };
+      // When the envs on the screen change
       $scope.$watch('environmentalVars', function (newEnv, oldEnv) {
+        // Since the user has inputed text, we don't need to listen to the current model anymore
         unreg();
+        // If the envs haven't changed, (also takes care of first null/null occurrence)
         if (newEnv === oldEnv) return;
+        // Save them to the state model
         keypather.set($scope, 'stateModel.env', newEnv.split('\n').filter(function (v) {
           return v.length;
         }));
---
+++ test/unit/directives/directiveEnvVars.unit.js
@@ -0,0 +1,176 @@
+describe('directiveEnvVars'.bold.underline.blue, function() {
+  var element;
+  var $scope;
+  var $rootScope;
+
+  function initState (addToScope) {
+    angular.mock.module('app');
+    angular.mock.inject(function($compile, _$rootScope_, $timeout){
+      $rootScope = _$rootScope_;
+      $scope = $rootScope.$new();
+
+      $rootScope.safeApply = function(cb) {
+        $timeout(function() {
+          $scope.$digest();
+        });
+      };
+
+      var tpl = '<env-vars ' +
+        'items="items" current-model="currentModel" state-model="stateModel"' +
+        '></env-vars>';
+
+      Object.keys(addToScope).forEach(function (key) {
+        $scope[key] = addToScope[key];
+      });
+
+      element = $compile(tpl)($scope);
+      $scope.$digest();
+    });
+  }
+
+  /** Things that we need to test:
+   *  3 use cases
+   *    1 Setup         -> no currentModel
+   *    2 Instance      -> no stateModel
+   *    3 InstanceEdit  -> both
+   *
+   *  Test features
+   *    1/2/3 Nothing on currentModel -> environmentalVars = blank
+   *    2/3   Envs on currentModel -> = environmentalVars
+   *
+   *    3     Envs on currentModel, write envs on environmentalVars -> stateModel should be updated
+   *    1/3   Write envs on environmentalVars -> stateModel should be updated
+   *    1/3   Delete some envs -> stateModel should be updated
+   *    1/3   Delete ALL envs -> stateModel should be updated
+   */
+
+  /**
+   * Borderline useless helper function, just for funzies
+   * @param envs env array
+   * @returns {{env: *}}
+   */
+  function createEnvModel(envs) {
+    return {
+      env: envs
+    };
+  }
+
+  it('Should not display anything on the page with no current model', function () {
+    initState({stateModel: {}});
+    $scope.$digest();
+
+    var environmentalVars = element.isolateScope().environmentalVars;
+    expect(environmentalVars).to.equal('');
+    expect($scope.stateModel.env).to.equal(undefined);
+  });
+  it('Should not display anything on the page with no input', function () {
+    initState({currentModel: createEnvModel([])});
+    $scope.$digest();
+
+    var environmentalVars = element.isolateScope().environmentalVars;
+    expect(environmentalVars).to.equal('');
+  });
+
+  it('Should not display or save anything on the page with no input', function () {
+    initState({
+      currentModel: createEnvModel([]),
+      stateModel: {}
+    });
+    $scope.$digest();
+
+    var environmentalVars = element.isolateScope().environmentalVars;
+    expect(environmentalVars).to.equal('');
+    expect($scope.stateModel.env).to.equal(undefined);
+  });
+
+  it('Should display envs from current model, no stateModel', function () {
+    var envs = ['a=b', 'x=y', 'dasdasd=asfa'];
+    initState({
+      currentModel: createEnvModel(envs)
+    });
+    $scope.$digest();
+
+    var environmentalVars = element.isolateScope().environmentalVars;
+    expect(environmentalVars).to.equal(envs.join('\n') + '\n');
+  });
+
+  it('Should display envs from current model', function () {
+    var envs = ['a=b', 'x=y', 'dasdasd=asfa'];
+    initState({
+      currentModel: createEnvModel(envs),
+      stateModel: {}
+    });
+    $scope.$digest();
+
+    var environmentalVars = element.isolateScope().environmentalVars;
+    expect(environmentalVars).to.equal(envs.join('\n') + '\n');
+    expect($scope.stateModel.env).to.equal(undefined);
+  });
+
+  it('should add new envs to stateModel', function() {
+    initState({
+      stateModel: {}
+    });
+    var addedEnvs = ['lms=asd', 'db=awe'];
+    element.isolateScope().environmentalVars += addedEnvs.join('\n');
+    $scope.$digest();
+
+    var environmentalVars = element.isolateScope().environmentalVars;
+    expect(environmentalVars).to.equal(addedEnvs.join('\n'));
+    expect($scope.stateModel.env.length).to.equal(addedEnvs.length);
+    addedEnvs.forEach(function (env, index) {
+      expect($scope.stateModel.env[index]).to.equal(env);
+    });
+  });
+
+  it('should add current and new envs to stateModel', function() {
+    var envs = ['a=b', 'x=y', 'dasdasd=asfa'];
+    initState({
+      currentModel: createEnvModel(envs),
+      stateModel: {}
+    });
+    var addedEnvs = ['lms=asd', 'db=awe'];
+    element.isolateScope().environmentalVars += addedEnvs.join('\n');
+    $scope.$digest();
+    var expectedEnvs = envs.concat(addedEnvs);
+
+    var environmentalVars = element.isolateScope().environmentalVars;
+    expect(environmentalVars).to.equal(expectedEnvs.join('\n'));
+    expect($scope.stateModel.env.length).to.equal(expectedEnvs.length);
+    expectedEnvs.forEach(function (env, index) {
+      expect($scope.stateModel.env[index]).to.equal(env);
+    });
+  });
+
+  it('should remove some envs, and set them in the stateModel', function() {
+    var envs = ['a=b', 'x=y', 'dasdasd=asfa'];
+    initState({
+      currentModel: createEnvModel(envs),
+      stateModel: {}
+    });
+    var addedEnvs = ['a=b', 'x=y'];
+    element.isolateScope().environmentalVars = addedEnvs.join('\n');
+    $scope.$digest();
+
+    var environmentalVars = element.isolateScope().environmentalVars;
+    expect(environmentalVars).to.equal(addedEnvs.join('\n'));
+    expect($scope.stateModel.env.length).to.equal(addedEnvs.length);
+    addedEnvs.forEach(function (env, index) {
+      expect($scope.stateModel.env[index]).to.equal(env);
+    });
+  });
+
+  it('should remove all envs, and clear them in the stateModel', function() {
+    var envs = ['a=b', 'x=y', 'dasdasd=asfa'];
+    initState({
+      currentModel: createEnvModel(envs),
+      stateModel: {}
+    });
+    element.isolateScope().environmentalVars = '';
+    $scope.$digest();
+
+    var environmentalVars = element.isolateScope().environmentalVars;
+    expect(environmentalVars).to.equal('');
+    expect($scope.stateModel.env.length).to.equal(0);
+  });
+});
