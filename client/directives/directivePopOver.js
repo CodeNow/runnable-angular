cc22e264737fdcac59985bfefdaa1fa19168b468
SomeKittens <rkoutnik@gmail.com>
2014-12-01 14:17:39 -0800

    add popovers as children of body

--- client/directives/directivePopOver.js
+++ client/directives/directivePopOver.js
@@ -4,18 +4,44 @@ require('app')
  * popOver Directive
  * @ngInject
  */
-function popOver() {
+function popOver(
+  $templateCache,
+  $compile,
+  jQuery
+) {
   return {
     restrict: 'E',
-    templateUrl: function ($element, attrs) {
-      return attrs.template;
-    },
     replace: true,
     scope: {
       data: '=',
       actions: '='
     },
     link: function ($scope, element, attrs) {
+      var $ = jQuery;
+
+      var template = $templateCache.get(attrs.template);
+
+      var options;
+      try {
+        options = JSON.parse(attrs.popoverOptions);
+      } catch (e) {
+        options = {};
+      }
+      options.left = (typeof options.left !== 'undefined') ? options.left : 0;
+      options.top = (typeof options.top !== 'undefined') ? options.top : 0;
+      options.class = (typeof options.class !== 'undefined') ? options.class : false;
+
+      var gParent = $(element.parent().parent());
+
+      var popEl = $compile(template)($scope);
+
+      popEl.css({
+        top: (gParent.offset().top + options.top) + 'px',
+        left: (gParent.offset().left + options.left) + 'px'
+      });
+
+      $('body').append(popEl);
+
       element.on('click', function (event) {
         event.stopPropagation();
       });
