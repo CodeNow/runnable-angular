36db13bc89ccef2c71b9a61e97019fea035d2ec7
SomeKittens <rkoutnik@gmail.com>
2014-12-10 18:15:26 -0800

    do not display modal on 401 err

--- client/services/serviceHandleErr.js
+++ client/services/serviceHandleErr.js
@@ -2,12 +2,15 @@ require('app')
   .factory('errs', errs);
 
 function errs (
-  $rootScope
+  $rootScope,
+  keypather
 ) {
   var errors = [];
   return {
     handler: function (err) {
       if (err) {
+        // 401 does not need to be displayed to user
+        if (keypather.get(err, 'data.statusCode') === 401) { return; }
         errors.push(err);
         $rootScope.safeApply();
       }
