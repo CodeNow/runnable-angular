9db2035751b6ec291e1ef921f41d62e17282cc8e
SomeKittens <rkoutnik@gmail.com>
2014-12-12 11:27:09 -0800

    no redirect on 401 if page allows anon

--- client/services/serviceFetchUser.js
+++ client/services/serviceFetchUser.js
@@ -12,7 +12,8 @@ function fetchUser (
       return cb(null, currentUser);
     }
     var tempUser = user.fetchUser('me', function (err) {
-      if (err && keypather.get(err, 'data.statusCode') === 401) {
+      if (err && keypather.get(err, 'data.statusCode') === 401 &&
+          !$state.current.data.anon) {
         $state.go('home');
         return cb(err);
       }
