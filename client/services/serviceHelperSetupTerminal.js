b55f9d4a4ffda73aa14e1ffbcd8c97aad36d15bd
Nathan219 <Nathan219@gmail.com>
2014-12-11 19:44:46 -0800

    Terminal width is now variable

--- client/services/serviceHelperSetupTerminal.js
+++ client/services/serviceHelperSetupTerminal.js
@@ -11,10 +11,11 @@ function helperSetupTerminal(
   $window
 ) {
   return function ($scope, elem, opts) {
-    var CHAR_HEIGHT = 20;
+    var CHAR_SIZE = getTerminalCharacterSize(elem);
     var config = angular.extend({}, configTerminalOpts);
     config = angular.extend(config, (opts || {}));
-    config.rows = Math.floor(elem[0].clientHeight / CHAR_HEIGHT);
+    config.rows = Math.floor(elem[0].clientHeight / CHAR_SIZE.height);
+    config.cols = Math.floor(elem[0].clientWidth / CHAR_SIZE.width);
     var terminal = new termjs(config);
     terminal.open(elem[0]);
 
@@ -27,17 +28,16 @@ function helperSetupTerminal(
       if ($termElem.width() === 100) {
         return;
       }
+      var CHAR_SIZE = getTerminalCharacterSize(elem);
       var termLineEl = $termElem.find('div')[0];
       if (!termLineEl) {
         return;
       }
-      var tBox = termLineEl.getBoundingClientRect();
-      var charWidth = tBox.width / termLineEl.textContent.length;
-      var x = Math.floor($termElem.width() / charWidth);
+      var x = Math.floor($termElem.width() / CHAR_SIZE.width);
       if (x < configTerminalOpts.cols) {
         x = configTerminalOpts.cols;
       }
-      var y = Math.floor($termElem.height() / CHAR_HEIGHT);
+      var y = Math.floor($termElem.height() / CHAR_SIZE.height);
       if (!(oldX === x && oldY === y)) {
         oldX = x;
         oldY = y;
@@ -55,5 +55,13 @@ function helperSetupTerminal(
     });
 
     return terminal;
+  };
+}
+
+function getTerminalCharacterSize(elem) {
+  var temp = elem[0].querySelectorAll('.js-char-width')[0];
+  return {
+    width: temp ? temp.offsetWidth : 9,
+    height: temp ? temp.offsetHeight : 19
   };
 }
