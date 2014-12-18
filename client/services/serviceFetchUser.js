c7ab4f99b542b0052e4e6ffbc36d32de5dd15d45
runnabro <tli415@gmail.com>
2014-12-18 15:08:49 -0800

    keypather randall

--- client/services/serviceFetchUser.js
+++ client/services/serviceFetchUser.js
@@ -13,7 +13,7 @@ function fetchUser (
     }
     var tempUser = user.fetchUser('me', function (err) {
       if (err && keypather.get(err, 'data.statusCode') === 401 &&
-          !$state.current.data.anon) {
+          !keypather.get($state, 'current.data.anon')) {
         $state.go('home');
         return cb(err);
       }
