c91d6d900d682f24f4aea3786e4a3ba594b39ca7
runnabro <tli415@gmail.com>
2015-01-09 12:11:43 -0800

    setup modal template files

---
+++ client/directives/modals/modalGettingStarted/directiveGettingStarted.js
@@ -0,0 +1,24 @@
+require('app')
+  .directive('modalGettingStarted', modalGettingStarted);
+/**
+ * directive modalGettingStarted
+ * @ngInject
+ */
+function modalGettingStarted(
+  $localStorage,
+  keypather
+) {
+  return {
+    restrict: 'E',
+    templateUrl: 'viewModalGettingStarted',
+    scope: {
+      data: '=',
+      currentModel: '=',
+      stateModel: '=',
+      actions: '=',
+      defaultActions: '='
+    },
+    link: function ($scope, element, attrs) {
+    }
+  };
+}
