673c666862d6e9b787b626661529c6a3177c6a11
Nathan219 <Nathan219@gmail.com>
2015-01-29 14:36:31 -0800

    just to test

--- client/services/serviceFetchCoachMarkData.js
+++ client/services/serviceFetchCoachMarkData.js
@@ -9,25 +9,21 @@ function fetchCoachMarkData(
   errs
 ) {
   return function (coachMarkKey, cb) {
-    return cb();
-    //fetchUser(function (err, user) {
-    //  errs.handler(err);
-    //  // If the coach mark has been shown, just return null
-    //  // if it hasn't, return an object with a save function on it
-    //  var data = keypather.get(
-    //    user,
-    //    'attrs.userOptions.uiState.shownCoachMarks.' + coachMarkKey
-    //  ) ? null : {
-    //    save: function () {
-    //      var userOptions = {};
-    //      userOptions['userOptions.uiState.shownCoachMarks.' + coachMarkKey] = true;
-    //      // Make user update call here
-    //      user.update(userOptions, function (err) {
-    //        errs.handler(err);
-    //      });
-    //    }
-    //  };
-    //  cb(data);
-    //});
+    fetchUser(function (err, user) {
+      errs.handler(err);
+      // If the coach mark has been shown, just return null
+      // if it hasn't, return an object with a save function on it
+      var data = {
+        save: function () {
+          var userOptions = {};
+          userOptions['userOptions.uiState.shownCoachMarks.' + coachMarkKey] = true;
+          // Make user update call here
+          user.update(userOptions, function (err) {
+            errs.handler(err);
+          });
+        }
+      };
+      cb(data);
+    });
   };
 }
