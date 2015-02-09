7fae767e4ac79ff008c55ac5379409c378ab2c17
Nathan219 <Nathan219@gmail.com>
2015-02-09 13:22:12 -0800

    Fixing Tests

--- test/unit/controllers/controllerInstanceHome.unit.js
+++ test/unit/controllers/controllerInstanceHome.unit.js
@@ -71,6 +71,11 @@ describe('ControllerInstanceHome'.bold.underline.blue, function () {
     };
     angular.mock.module('app', function ($provide) {
       $provide.factory('fetchInstances', mockFetch.fetch);
+      $provide.value('favico', {
+        reset : sinon.spy(),
+        setImage: sinon.spy(),
+        setInstanceState: sinon.spy()
+      });
       $provide.value('$stateParams', ctx.stateParams);
       $provide.value('$localStorage', localStorageData || {});
     });
