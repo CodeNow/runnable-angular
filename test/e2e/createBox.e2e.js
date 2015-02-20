7c5eee35b930426312f4836af76d9eaa00e4eec5
SomeKittens <rkoutnik@gmail.com>
2015-02-19 16:06:29 -0800

    wip

--- test/e2e/createBox.e2e.js
+++ test/e2e/createBox.e2e.js
@@ -9,53 +9,92 @@ var util = require('./helpers/util');
 
 var SetupPage = require('./pages/SetupPage');
 var InstancePage = require('./pages/InstancePage');
+var GettingStarted = require('./modals/GettingStarted');
+
 var instances = ['Test-0', 'Test-1'];
 var ENV_VARS = [null, 'a=b\nbasd=asasdasdasd'];
 
 describe('project creation workflow', function () {
+  it('runs throug the GS modal', function() {
+    // Getting started modal should be open by default
+    var gettingStarted = new GettingStarted();
+
+    gettingStarted.filter('node-hello');
+
+    gettingStarted.selectRepo(0);
+
+    gettingStarted.clickButton('Next Step');
+
+    gettingStarted.clickButton('Build Server');
+
+    util.waitForUrl(InstancePage.urlRegex);
+  });
+
+  it('loads a building instance', function() {
+    var instance = new InstancePage('node-hello-world');
+
+    instance.get();
+
+    util.waitForUrl(InstancePage.urlRegex);
+
+    browser.wait(function () {
+      return util.hasClass(instance.statusIcon, 'running');
+    });
+
+    instance.activePanel.setActiveTab('Box Logs');
+    browser.wait(instance.activePanel.aceLoaded.bind(instance.activePanel));
+
+    expect(instance.activePanel.getContents()).toMatch('sleep 123456789');
+
+  });
+
+
+  /*
   instances.forEach(function (instanceName, index) {
     describe('with' + ((ENV_VARS[index]) ? ' ' : 'out ') + 'Envs', function () {
       it('should direct the user to the setup page', function () {
-        var setup = new SetupPage();
-        setup.get();
-        util.waitForUrl(SetupPage.urlRegex);
 
-        setup.setBoxName(instanceName);
+        // Setup page is deprecated
+        // var setup = new SetupPage();
+        // setup.get();
+        // util.waitForUrl(SetupPage.urlRegex);
 
-        setup.repoList.openAddDropdown();
+        // setup.setBoxName(instanceName);
 
-        setup.repoList.searchRepos('node-hello-world', 1);
+        // setup.repoList.openAddDropdown();
 
-        setup.repoList.selectRepo(0);
+        // setup.repoList.searchRepos('node-hello-world', 1);
 
-        setup.selectTemplate('Blank');
+        // setup.repoList.selectRepo(0);
 
-        browser.wait(function () {
-          return setup.activePanel.aceLoaded();
-        });
-        browser.wait(function () {
-          return setup.blankTemplateLoaded();
-        });
+        // setup.selectTemplate('Blank');
 
-        setup.activePanel.writeToFile('\nFROM dockerfile/nodejs\nCMD sleep 123456789\n');
+        // browser.wait(function () {
+        //   return setup.activePanel.aceLoaded();
+        // });
+        // browser.wait(function () {
+        //   return setup.blankTemplateLoaded();
+        // });
 
-        browser.wait(function () {
-          return setup.dockerfileValidates();
-        });
-        browser.wait(function () {
-          return setup.activePanel.isClean();
-        });
+        // setup.activePanel.writeToFile('\nFROM dockerfile/nodejs\nCMD sleep 123456789\n');
 
-        if (ENV_VARS[index]) {
-          // Now enter some envs
-          setup.activePanel.openTab('Env Vars');
-          browser.wait(setup.activePanel.aceLoaded.bind(setup.activePanel));
+        // browser.wait(function () {
+        //   return setup.dockerfileValidates();
+        // });
+        // browser.wait(function () {
+        //   return setup.activePanel.isClean();
+        // });
 
-          setup.activePanel.writeToFile(ENV_VARS[index]);
+        // if (ENV_VARS[index]) {
+        //   // Now enter some envs
+        //   setup.activePanel.openTab('Env Vars');
+        //   browser.wait(setup.activePanel.aceLoaded.bind(setup.activePanel));
 
-          browser.wait(setup.activePanel.isClean.bind(setup.activePanel));
-        }
-        setup.createBox();
+        //   setup.activePanel.writeToFile(ENV_VARS[index]);
+
+        //   browser.wait(setup.activePanel.isClean.bind(setup.activePanel));
+        // }
+        // setup.createBox();
 
         util.waitForUrl(InstancePage.urlRegex);
       });
@@ -84,5 +123,5 @@ describe('project creation workflow', function () {
         }
       });
     });
-  });
+  });*/
 });
--- test/e2e/modals/GettingStarted.js
+++ test/e2e/modals/GettingStarted.js
@@ -7,6 +7,8 @@ function GettingStarted () {
 
   this.repoList = util.createGetterAll(by.repeater('repo in githubRepos.models | repos:data.repoFilter | orderBy: \'-attrs.updated_at\''), this.modalElem);// '))
 
+  this.search = util.createGetter(by.model('data.repoFilter'));
+
   this.isShowing = function() {
     return this.modalElem.get().isDisplayed();
   };
@@ -17,6 +19,12 @@ function GettingStarted () {
 
   this.clickButton = function(text) {
     this.modalElem.get().element(by.buttonText(text)).click();
+  };
+
+  this.filter = function(text) {
+    var searchBox = this.search.get();
+    searchBox.click();
+    searchBox.sendKeys(text);
   };
 }
 
