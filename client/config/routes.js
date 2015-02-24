d267028fa60754958685a43d88f87d30393888a3
Anton Podviaznikov <podviaznikov@gmail.com>
2015-02-23 17:31:27 -0800

    change url route

--- client/config/routes.js
+++ client/config/routes.js
@@ -38,7 +38,7 @@ module.exports = [
   }, {
     state: 'serverSelection',
     abstract: false,
-    url: '^/:userName/serverSelection/:repo/:branch/:message/:commit',
+    url: '^/:userName/serverSelection/:repo',
     templateUrl: 'viewServerSelection',
     controller: 'ControllerServerSelection',
     data: {
