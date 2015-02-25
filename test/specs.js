9d4a339108458613e49385d2c9cf188b568240eb
Nathan219 <Nathan219@gmail.com>
2015-02-25 15:13:54 -0800

    Now we can properly use saucelabs

--- test/specs.js
+++ test/specs.js
@@ -2,6 +2,7 @@
 
 module.exports = [
   'e2e/helpers/login.js',
+  'e2e/changeUser.e2e.js',
 
   // FOR USER
   'e2e/createBox.e2e.js',
@@ -16,11 +17,10 @@ module.exports = [
   //'e2e/forkBox.e2e.js',
   'e2e/deleteBox.e2e.js',
 
-
-  // FOR ORG
-  'e2e/changeUser.e2e.js',
-  'e2e/createBox.e2e.js',
-  'e2e/watchBuildLogs.e2e.js',
-  'e2e/changeCommit.e2e.js',
-  'e2e/deleteBox.e2e.js'
+  //
+  //// FOR ORG
+  //'e2e/createBox.e2e.js',
+  //'e2e/watchBuildLogs.e2e.js',
+  //'e2e/changeCommit.e2e.js',
+  //'e2e/deleteBox.e2e.js'
 ];
