0caab2102ad0cda4a87fecd60ac2828abe095b36
SomeKittens <rkoutnik@gmail.com>
2015-02-24 17:28:07 -0800

    extract specs into shared file

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var browsers = require('./browsers');
+var specs = require('./specs');
 
 exports.config = {
   // The address of a running selenium server.
@@ -9,19 +10,7 @@ exports.config = {
   allScriptsTimeout: 20000,
 
   // Order is important
-  specs: [
-    'e2e/helpers/login.js',
-    'e2e/createBox.e2e.js',
-    //'e2e/editDockerfile.e2e.js',
-    // 'e2e/readLogs.e2e.js',
-    // 'e2e/changeCommit.e2e.js',
-    //'e2e/addRepo.e2e.js',
-    // 'e2e/changeCommit2Reps.e2e.js',
-    //'e2e/deleteRepo.e2e.js',
-    //'e2e/renameBox.e2e.js',
-    //'e2e/forkBox.e2e.js',
-    'e2e/deleteBox.e2e.js'
-  ],
+  specs: specs,
 
   onPrepare: function () {
     browser.driver.manage().window().maximize();
--- test/sauce.conf.js
+++ test/sauce.conf.js
@@ -1,22 +1,11 @@
 'use strict';
 
 var browsers = require('./browsers');
+var specs = require('./specs');
 
 var config = {
   // Order is important
-  specs: [
-    'e2e/helpers/login.js',
-    'e2e/createBox.e2e.js',
-    // 'e2e/editDockerfile.e2e.js',
-    // 'e2e/readLogs.e2e.js',
-    // 'e2e/changeCommit.e2e.js',
-    // 'e2e/addRepo.e2e.js',
-    // 'e2e/changeCommit2Reps.e2e.js',
-    // 'e2e/deleteRepo.e2e.js',
-    // 'e2e/renameBox.e2e.js',
-    // 'e2e/forkBox.e2e.js',
-    'e2e/deleteBox.e2e.js'
-  ],
+  specs: specs,
 
   onPrepare: function () {
     browser.driver.manage().window().maximize();
