632f4a7f8b1e7b8ea9d0476c7c4d276b60183abb
Myztiq <kahn@runnable.com>
2015-03-16 12:00:54 -0700

    Removed popover-file-explorer-menu directive (It wasn't in use) and tested popover-file-explorer-folder-menu.

--- test/unit/directives/popovers/directivePopoverFileExplorerItemMenu.unit.js
+++ test/unit/directives/popovers/directivePopoverFileExplorerItemMenu.unit.js
@@ -9,26 +9,20 @@ var
   $scope;
 var $elScope;
 
-describe('directivePopoverFileExplorerFolderMenu'.bold.underline.blue, function() {
+describe('directivePopoverFileExplorerItemMenu'.bold.underline.blue, function() {
   var ctx, errs;
 
   describe('Functionality', function() {
     var element;
-    var createFsMock;
+
+    errs = {
+      handler: sinon.spy()
+    };
 
     function injectSetupCompile () {
       ctx = {};
-      createFsMock = sinon.spy();
-      createFsMock.foo = Math.random();
-
-      errs = {
-        handler: sinon.spy()
-      };
       angular.mock.module('app');
       angular.mock.module(function($provide) {
-        $provide.factory('helperCreateFS', function(){
-          return createFsMock;
-        });
         $provide.value('errs', errs);
       });
 
@@ -43,16 +37,26 @@ describe('directivePopoverFileExplorerFolderMenu'.bold.underline.blue, function(
       });
 
       // Emitting contextmenu messes with our scope, we need to set things AFTER it's been added to the page.
-      $scope.readOnly = false;
-      $scope.dir = {
-        attrs: {
-          name: '/foo'
+      $scope.openItems = {
+        add: sinon.stub()
+      };
+      $scope.actions = {
+        fetchDirFiles: sinon.stub()
+      };
+      $scope.fs = {
+        attrs:{
+          name: 'Test'
         },
-        destroy: sinon.spy(),
-        rename: sinon.spy()
+        state:{
+          open: true,
+          renaming: false
+        },
+        destroy: sinon.stub().callsArg(0),
+        rename: sinon.stub().callsArg(1)
       };
 
-      ctx.template = '<div popover-file-explorer-folder-menu><input class="tree-input"></div>';
+
+      ctx.template = '<div popover-file-explorer-item-menu><input class="tree-input"></div>';
       ctx.element = $compile(ctx.template)($scope);
       $elScope = ctx.element.isolateScope();
 
@@ -76,52 +80,44 @@ describe('directivePopoverFileExplorerFolderMenu'.bold.underline.blue, function(
       injectSetupCompile();
     });
 
-    it('should show new file', function() {
-      var newFileElement = element.querySelector('.popover-list-item:nth-child(1)');
-      expect(newFileElement.innerText).to.equal('New File')
+
+    it('should show open', function() {
+      var renameElement = element.querySelector('.popover-list-item:nth-child(1)');
+      expect(renameElement.innerText).to.equal('Open');
     });
 
-    it('should trigger the new file action when clicked on new file', function() {
-      var newFileElement = element.querySelector('.popover-list-item:nth-child(1)');
-      window.helpers.click(newFileElement);
-      expect(createFsMock.lastCall.args[1].isDir).to.equal(false);
+    it('should trigger the open action when clicked on open', function() {
+      var openElement = element.querySelector('.popover-list-item:nth-child(1)');
+      window.helpers.click(openElement);
+      expect($scope.openItems.add.calledOnce).to.equal(true)
     });
 
-    it('should show new folder', function() {
-      var newFolderElement = element.querySelector('.popover-list-item:nth-child(2)');
-      expect(newFolderElement.innerText).to.equal('New Folder')
-    });
-
-    it('should trigger the new folder action when clicked on new folder', function() {
-      var newFolderElement = element.querySelector('.popover-list-item:nth-child(2)');
-      window.helpers.click(newFolderElement);
-      expect(createFsMock.lastCall.args[1].isDir).to.equal(true);
-    });
 
     it('should show rename', function() {
-      var renameElement = element.querySelector('.popover-list-item:nth-child(4)');
+      var renameElement = element.querySelector('.popover-list-item:nth-child(3)');
       expect(renameElement.innerText).to.equal('Rename');
     });
 
     it('should trigger the rename action when clicked on rename and should handle close rename', function() {
-      var renameElement = element.querySelector('.popover-list-item:nth-child(4)');
+      var renameElement = element.querySelector('.popover-list-item:nth-child(3)');
       window.helpers.click(renameElement);
-      expect($scope.dirItemData.editFolderName).to.equal(true);
-      $scope.dirItemData.actions.closeFolderNameInput();
-      $scope.dir.attrs.name = "Foo";
-      expect($scope.dir.rename.calledOnce).to.equal(true);
-      expect($scope.dirItemData.editFolderName).to.equal(false);
+      expect($scope.fs.state.renaming).to.equal(true);
+      $scope.fileItemData.actions.closeFileNameInput();
+      $scope.fs.attrs.name = 'Test 1234';
+      expect($scope.fs.rename.calledOnce, 'fs.rename called').to.equal(true);
+      expect($scope.fs.state.renaming, 'renaming got set to false').to.equal(false);
     });
 
     it('should show delete', function() {
-      var deleteElement = element.querySelector('.popover-list-item:nth-child(5)');
+      var deleteElement = element.querySelector('.popover-list-item:nth-child(4)');
       expect(deleteElement.innerText).to.equal('Delete')
     });
 
     it('should trigger the delete action when clicked on delete', function() {
-      var deleteElement = element.querySelector('.popover-list-item:nth-child(5)');
+      var deleteElement = element.querySelector('.popover-list-item:nth-child(4)');
       window.helpers.click(deleteElement);
-      expect($scope.dir.destroy.calledOnce).to.equal(true)
+      expect($scope.fs.destroy.calledOnce).to.equal(true);
+      expect($scope.actions.fetchDirFiles.calledOnce, 'fetch dir files called').to.equal(true);
     });
 
     it('should close when the document is clicked', function(){
@@ -132,10 +128,6 @@ describe('directivePopoverFileExplorerFolderMenu'.bold.underline.blue, function(
     it('should close when another file modal is shown', function(){
       $scope.$emit('file-modal-open');
       expect($document[0].querySelector('.context-menu')).to.equal.null;
-    });
-
-    it('should not show if the tree is read only', function() {
-      expect(element).to.equal.null;
     });
 
     it('should cleanup it\'s event handlers on destroy', function() {
