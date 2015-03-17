2d3e3473360b1ff9bf8cb2b47363c27d8a30fe24
Myztiq <kahn@runnable.com>
2015-03-17 13:23:56 -0700

    Fixed formatting.

--- test/unit/services/serviceFetch.unit.js
+++ test/unit/services/serviceFetch.unit.js
@@ -1,12 +1,12 @@
 describe('serviceFetch'.bold.underline.blue, function () {
+  'use strict';
 
 
-  describe('factory pFetchUser', function(){
-    var $state,
-      user,
-      $rootScope,
-      pFetchUser
-      ;
+  describe('factory pFetchUser', function () {
+    var $state;
+    var user;
+    var $rootScope;
+    var pFetchUser;
 
     beforeEach(function () {
       user = {
@@ -27,12 +27,12 @@ describe('serviceFetch'.bold.underline.blue, function () {
       });
     });
 
-    it('should call the fetchUser method of the user service', function(done){
+    it('should call the fetchUser method of the user service', function (done) {
       var myUser = {
-        name: "Ryan Kahn"
+        name: 'Ryan Kahn'
       };
       user.fetchUser = sinon.stub().callsArgWith(1, null, myUser);
-      pFetchUser().then(function(foundUser){
+      pFetchUser().then(function (foundUser) {
         expect(user.fetchUser.calledOnce, 'fetchUser called').to.equal(true);
         expect(foundUser, 'Returned user').to.equal(myUser);
         done();
@@ -40,7 +40,7 @@ describe('serviceFetch'.bold.underline.blue, function () {
       $rootScope.$apply();
     });
 
-    it('should redirect on a 401 error', function(done){
+    it('should redirect on a 401 error', function (done) {
       $state.go = sinon.stub();
       var err = {
         data: {
@@ -48,7 +48,7 @@ describe('serviceFetch'.bold.underline.blue, function () {
         }
       };
       user.fetchUser = sinon.stub().callsArgWith(1, err);
-      pFetchUser().catch(function(myErr){
+      pFetchUser().catch(function (myErr) {
         expect(myErr, 'Returned err').to.equal(err);
         expect($state.go.calledWith('home'), 'Called go home on the state').to.equal(true);
         done();
@@ -57,19 +57,17 @@ describe('serviceFetch'.bold.underline.blue, function () {
     });
   });
 
-  describe('factory fetchInstances', function(){
-    var user,
-      $rootScope,
-      fetchInstances,
-      $stateParams,
-      $q,
-      userStream,
-      keypather,
-      $state,
-      errs
-    ;
+  describe('factory fetchInstances', function () {
+    var user;
+    var $rootScope;
+    var fetchInstances;
+    var $stateParams;
+    var userStream;
+    var keypather;
+    var $state;
+    var errs;
 
-    var setupFetchInstances = function(pFetchUserFactory){
+    var setupFetchInstances = function (pFetchUserFactory) {
       errs = {
         handler: sinon.spy()
       };
@@ -77,8 +75,8 @@ describe('serviceFetch'.bold.underline.blue, function () {
         on: sinon.spy()
       };
       var primus = {
-        createUserStream: function(){
-          return userStream
+        createUserStream: function () {
+          return userStream;
         }
       };
       angular.mock.module('app');
@@ -91,40 +89,38 @@ describe('serviceFetch'.bold.underline.blue, function () {
         _$rootScope_,
         _fetchInstances_,
         _$stateParams_,
-        _$q_,
         _keypather_,
         _$state_
       ) {
         $rootScope = _$rootScope_;
         fetchInstances = _fetchInstances_;
         $stateParams = _$stateParams_;
-        $q = _$q_;
         keypather = _keypather_;
         $state = _$state_;
       });
     };
 
 
-    it('should handle when the user is not signed in', function(){
+    it('should handle when the user is not signed in', function (done) {
       var err = {
         data: {
           statusCode: 401
         }
       };
-      setupFetchInstances(function($q){
+      setupFetchInstances(function ($q) {
         return sinon.stub().returns($q.reject(err));
       });
 
-      fetchInstances().catch(function(userError){
-        expect(userError).to.equal(err)
-      }).catch(function(e){
-        done(e)
+      fetchInstances().catch(function (userError) {
+        expect(userError).to.equal(err);
+      }).catch(function (e) {
+        done(e);
       });
       $rootScope.$apply();
     });
 
-    it('should call fetch instances on the user object', function(done){
-      var user = {
+    it('should call fetch instances on the user object', function (done) {
+      user = {
         models: [
           {
             name: 'MyModel'
@@ -132,24 +128,24 @@ describe('serviceFetch'.bold.underline.blue, function () {
         ],
         fetchInstances: sinon.stub().callsArg(1)
       };
-      setupFetchInstances(function($q){
+      setupFetchInstances(function ($q) {
         return sinon.stub().returns($q.when(user));
       });
 
-      $stateParams.userName = "Myztiq";
+      $stateParams.userName = 'Myztiq';
 
-      fetchInstances({name: 'MyModel'}).then(function(instance){
+      fetchInstances({name: 'MyModel'}).then(function (instance) {
         expect(instance.name).to.equal('MyModel');
         expect(user.fetchInstances.calledOnce).to.equal(true);
-        done()
-      }).catch(function(e){
-        done(e)
+        done();
+      }).catch(function (e) {
+        done(e);
       });
       $rootScope.$apply();
     });
 
-    it('should call fetch instances on the user object and return an array when the search is not for a specific named instance', function(done){
-      var user = {
+    it('should call fetch instances on the user object and return an array when the search is not for a specific named instance', function (done) {
+      user = {
         models: [
           {
             name: 'MyModel'
@@ -157,24 +153,23 @@ describe('serviceFetch'.bold.underline.blue, function () {
         ],
         fetchInstances: sinon.stub().callsArg(1)
       };
-      setupFetchInstances(function($q){
+      setupFetchInstances(function ($q) {
         return sinon.stub().returns($q.when(user));
       });
 
-      $stateParams.userName = "Myztiq";
+      $stateParams.userName = 'Myztiq';
 
-      fetchInstances().then(function(instance){
+      fetchInstances().then(function (instance) {
         expect(instance.models[0].name).to.equal('MyModel');
-        done()
-      }).catch(function(e){
-        done(e)
+        done();
+      }).catch(function (e) {
+        done(e);
       });
       $rootScope.$apply();
     });
 
-    describe('userStream events', function(){
-      var instances, user;
-      beforeEach(function(done){
+    describe('userStream events', function () {
+      beforeEach(function (done) {
         user = {
           models: [
             {
@@ -186,41 +181,40 @@ describe('serviceFetch'.bold.underline.blue, function () {
           remove: sinon.spy(),
           fetchInstances: sinon.stub().callsArg(1)
         };
-        setupFetchInstances(function($q){
+        setupFetchInstances(function ($q) {
           return sinon.stub().returns($q.when(user));
         });
 
-        $stateParams.userName = "Myztiq";
+        $stateParams.userName = 'Myztiq';
 
-        keypather.set($rootScope, 'dataApp.data.activeAccount.oauthId', function(){
+        keypather.set($rootScope, 'dataApp.data.activeAccount.oauthId', function () {
           return 1234;
         });
 
         $rootScope.$apply();
 
-        fetchInstances().then(function(_instances){
-          setTimeout(function(){
-            instances = _instances;
-            done()
+        fetchInstances().then(function () {
+          setTimeout(function () {
+            done();
           }, 0);
-        }).catch(function(e){
-          done(e)
+        }).catch(function (e) {
+          done(e);
         });
         $rootScope.$apply();
       });
 
-      it('should register to data events', function(){
+      it('should register to data events', function () {
         expect(userStream.on.calledWith('data')).to.equal(true);
       });
 
-      it('should listen to deploy message', function(){
+      it('should listen to deploy message', function () {
         userStream.on.withArgs('data').lastCall.args[1]({
           event: 'ROOM_MESSAGE',
-          data:{
+          data: {
             action: 'deploy',
             data: {
               name: 'New Instance',
-              owner:{
+              owner: {
                 github: 1234
               }
             }
@@ -232,14 +226,14 @@ describe('serviceFetch'.bold.underline.blue, function () {
         expect(errs.handler.called).to.equal(false);
       });
 
-      it('should listen to post event', function(){
+      it('should listen to post event', function () {
         userStream.on.withArgs('data').lastCall.args[1]({
           event: 'ROOM_MESSAGE',
-          data:{
+          data: {
             action: 'post',
             data: {
               name: 'New Instance',
-              owner:{
+              owner: {
                 github: 1234
               }
             }
@@ -251,7 +245,7 @@ describe('serviceFetch'.bold.underline.blue, function () {
         expect(errs.handler.called).to.equal(false);
       });
 
-      it('should listen to delete events', function(){
+      it('should listen to delete events', function () {
         $state.go = sinon.stub();
         $stateParams.instanceName = 'DELETE ME!';
         var myInstance = {
@@ -262,11 +256,11 @@ describe('serviceFetch'.bold.underline.blue, function () {
         user.find = sinon.stub().returns(myInstance);
         userStream.on.withArgs('data').lastCall.args[1]({
           event: 'ROOM_MESSAGE',
-          data:{
+          data: {
             action: 'delete',
             data: {
               name: 'New Instance',
-              owner:{
+              owner: {
                 github: 1234
               }
             }
@@ -282,36 +276,32 @@ describe('serviceFetch'.bold.underline.blue, function () {
     });
   });
 
-  describe('factory fetchBuild', function(){
-    var $state,
-      $rootScope,
-      fetchBuild,
-      user
-      ;
+  describe('factory fetchBuild', function () {
+    var $rootScope;
+    var fetchBuild;
+    var user;
 
     beforeEach(function () {
       angular.mock.module('app');
       angular.mock.module(function ($provide) {
-        $provide.factory('pFetchUser', function($q){
+        $provide.factory('pFetchUser', function ($q) {
           user = {
             fetchBuild: sinon.stub().callsArg(1)
           };
-          return sinon.stub().returns($q.when(user))
+          return sinon.stub().returns($q.when(user));
         });
       });
       angular.mock.inject(function (
-        _$state_,
         _$rootScope_,
         _fetchBuild_
       ) {
-        $state = _$state_;
         $rootScope = _$rootScope_;
         fetchBuild = _fetchBuild_;
       });
     });
 
-    it('should call the fetchBuild method of the user', function(done){
-      fetchBuild(123).then(function(fetchedBuild){
+    it('should call the fetchBuild method of the user', function (done) {
+      fetchBuild(123).then(function (fetchedBuild) {
         expect(user.fetchBuild.calledOnce).to.equal(true);
         expect(user.fetchBuild.calledWith(123)).to.equal(true);
         expect(fetchedBuild).to.equal(user);
@@ -322,17 +312,15 @@ describe('serviceFetch'.bold.underline.blue, function () {
 
   });
 
-  describe('factory fetchOwnerRepos', function(){
-    var $state,
-      $rootScope,
-      fetchOwnerRepos,
-      user
-    ;
+  describe('factory fetchOwnerRepos', function () {
+    var $rootScope;
+    var fetchOwnerRepos;
+    var user;
 
     beforeEach(function () {
       angular.mock.module('app');
       angular.mock.module(function ($provide) {
-        $provide.factory('pFetchUser', function($q){
+        $provide.factory('pFetchUser', function ($q) {
           user = {
             oauthName: sinon.stub().returns('Myztiq'),
             fetchGithubRepos: sinon.stub().callsArgWith(1),
@@ -347,18 +335,16 @@ describe('serviceFetch'.bold.underline.blue, function () {
         });
       });
       angular.mock.inject(function (
-        _$state_,
         _$rootScope_,
         _fetchOwnerRepos_
       ) {
-        $state = _$state_;
         $rootScope = _$rootScope_;
         fetchOwnerRepos = _fetchOwnerRepos_;
       });
     });
 
-    it('should call the fetchOwnerRepos method of the user', function(done){
-      fetchOwnerRepos('Myztiq').then(function(){
+    it('should call the fetchOwnerRepos method of the user', function (done) {
+      fetchOwnerRepos('Myztiq').then(function () {
         expect(user.fetchGithubRepos.calledOnce).to.equal(true);
         expect(user.newGithubRepos.calledOnce).to.equal(true);
         expect(user.newGithubRepos.calledWith(user.models)).to.equal(true);
@@ -369,36 +355,32 @@ describe('serviceFetch'.bold.underline.blue, function () {
   });
 
 
-  describe('factory fetchContexts', function(){
-    var $state,
-      $rootScope,
-      fetchContexts,
-      user
-      ;
+  describe('factory fetchContexts', function () {
+    var $rootScope;
+    var fetchContexts;
+    var user;
 
     beforeEach(function () {
       angular.mock.module('app');
       angular.mock.module(function ($provide) {
-        $provide.factory('pFetchUser', function($q){
+        $provide.factory('pFetchUser', function ($q) {
           user = {
             fetchContexts: sinon.stub().callsArgWith(1)
           };
-          return sinon.stub().returns($q.when(user))
+          return sinon.stub().returns($q.when(user));
         });
       });
       angular.mock.inject(function (
-        _$state_,
         _$rootScope_,
         _fetchContexts_
       ) {
-        $state = _$state_;
         $rootScope = _$rootScope_;
         fetchContexts = _fetchContexts_;
       });
     });
 
-    it('should call the fetchContexts method of the user', function(done){
-      fetchContexts().then(function(fetchedContexts){
+    it('should call the fetchContexts method of the user', function (done) {
+      fetchContexts().then(function (fetchedContexts) {
         expect(user.fetchContexts.calledOnce).to.equal(true);
         expect(fetchedContexts).to.equal(user);
         done();
