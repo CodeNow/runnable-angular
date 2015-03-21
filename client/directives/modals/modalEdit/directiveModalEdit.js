21ad39c40c8de11c9db0d58de9caffa532ae6b49
runnabro <tli415@gmail.com>
2015-03-20 19:15:41 -0700

    merge

--- client/directives/modals/modalEdit/directiveModalEdit.js
+++ client/directives/modals/modalEdit/directiveModalEdit.js
@@ -8,42 +8,80 @@ require('app')
  */
 function modalEdit(
   $localStorage,
+  configUserContentDomain,
+  errs,
   keypather,
-  configUserContentDomain
+  OpenItems,
+  promisify
 ) {
   return {
     restrict: 'A',
     templateUrl: 'viewModalEdit',
     scope: {
       data: '=',
-      currentModel: '=',
-      stateModel: '=',
       actions: '=',
       defaultActions: '='
     },
     link: function ($scope, element, attrs) {
+      $scope.openItems = new OpenItems();
       // Add thing
       $scope.validation = {};
       $scope.tempModel = {};
       $scope.configUserContentDomain = configUserContentDomain;
 
+      $scope.getAllErrorsCount = function () {
+        var envErrors = keypather.get($scope, 'data.instance.validation.envs.errors.length') || 0;
+        var dockerFileErrors = keypather.get($scope, 'dockerfile.validation.errors.length') || 0;
+        return envErrors + dockerFileErrors;
+      };
+
+      if ($scope.data.instance) {
+        $scope.data.instance.validation = {
+          envs: {}
+        };
+      }
       $scope.popoverExposeInstruction = {
         data: {
           show: false
         },
         actions: {}
       };
+      $scope.popoverLinkServers = {
+        data: {
+          show: false
+        },
+        actions: {}
+      };
+
+      function setDefaultTabs() {
+        var rootDir = keypather.get($scope, 'build.contextVersions.models[0].rootDir');
+        if (!rootDir) { throw new Error('rootDir not found'); }
+        return promisify(rootDir.contents, 'fetch')()
+          .then(function () {
+            var file = rootDir.contents.models.find(function (file) {
+              return (file.attrs.name === 'Dockerfile');
+            });
+            if (file) {
+              $scope.dockerfile = file;
+              $scope.openItems.add(file);
+            }
+          });
+      }
 
       $scope.pasteLinkedInstance = function (text) {
         $scope.$broadcast('eventPasteLinkedInstance', text);
       };
-      $scope.data.hideGuideHelpEnvModal =
-          keypather.get($localStorage, 'guides.hideGuideHelpEnvModal') || false;
 
-      $scope.onChangeHideGuideEnv = function () {
-        $scope.data.hideGuideHelpEnvModal = true;
-        keypather.set($localStorage, 'guides.hideGuideHelpEnvModal', true);
-      };
+      function resetBuild() {
+        return promisify($scope.data.instance.build, 'deepCopy')()
+          .then(function (build) {
+            $scope.build = build;
+            return promisify(build.contextVersions.models[0], 'fetch')();
+          });
+      }
+      resetBuild()
+        .then(setDefaultTabs)
+        .catch(errs.handler);
     }
   };
 }
