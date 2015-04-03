4242b6bd0520318d2cf037653fafddd9a88bbd45
Myztiq <kahn@runnable.com>
2015-04-03 12:45:21 -0700

    Added better coverage for directiveActivePanel.

--- test/unit/directives/directiveActivePanel.unit.js
+++ test/unit/directives/directiveActivePanel.unit.js
@@ -2,14 +2,22 @@
 
 describe('directiveActivePanel'.bold.underline.blue, function () {
   var ctx;
+  var mockUpdateInstanceWithNewBuild;
+  var $rootScope;
+  var $q;
 
   var $scope,
       $elScope;
 
   var apiMocks = require('../apiMocks/index');
-  function initState(attrs) {
+  function initState(attrs, scopeOptions) {
+    scopeOptions = scopeOptions || {};
+    attrs = attrs || {};
+
     angular.mock.module('app');
     angular.mock.module(function($provide) {
+
+      $provide.value('updateInstanceWithNewBuild', mockUpdateInstanceWithNewBuild);
 
       $provide.factory('envVarsDirective', function () {
         return {
@@ -51,7 +59,13 @@ describe('directiveActivePanel'.bold.underline.blue, function () {
       });
     });
 
-    angular.mock.inject(function($compile, $rootScope) {
+    angular.mock.inject(function(
+      $compile,
+      _$rootScope_,
+      _$q_
+    ) {
+      $rootScope = _$rootScope_;
+      $q = _$q_;
       $scope = $rootScope.$new();
 
       $scope.openItems = {};
@@ -74,11 +88,17 @@ describe('directiveActivePanel'.bold.underline.blue, function () {
         'instance': 'instance',
         'build': 'build',
         'validation': 'validation',
-        'state-model': 'stateModel'
+        'state-model': 'stateModel',
+        'is-edit-modal': 'isEditModal'
       };
       Object.keys(attrs).forEach(function (key) {
         theseAttrs[key] = attrs[key];
       });
+
+      Object.keys(scopeOptions).forEach(function (key) {
+        $scope[key] = scopeOptions[key];
+      });
+
       ctx.template = directiveTemplate.attribute('active-panel', theseAttrs);
       ctx.element = $compile(ctx.template)($scope);
       $scope.$digest();
@@ -118,6 +138,178 @@ describe('directiveActivePanel'.bold.underline.blue, function () {
       term: true
     });
     expect($elScope.useAutoUpdate).to.be.false;
+  });
+
+  it('should hide build failure only when on the edit modal', function () {
+    initState({}, {
+      isEditModal: true,
+      instance: {
+        attrs: apiMocks.instances.building,
+        build: {
+          attrs: apiMocks.builds.failed
+        }
+      },
+      openItems: {
+        activeHistory: {
+          models: [
+            {
+              state: {
+                active: true,
+                type: 'LogView'
+              }
+            },
+            {
+              state: {
+                active: false,
+                type: 'BuildStream'
+              }
+            }
+          ]
+        }
+      }
+    });
+    var foo = $elScope.showBuildFailurePrompt();
+    expect(foo).to.equal(false);
+  });
+
+  it('should show build failure only when on the edit modal', function () {
+    initState({}, {
+      isEditModal: false,
+      instance: {
+        attrs: apiMocks.instances.building,
+        build: {
+          attrs: apiMocks.builds.failed
+        }
+      },
+      openItems: {
+        activeHistory: {
+          models: [
+            {
+              state: {
+                active: false,
+                type: 'LogView'
+              }
+            },
+            {
+              state: {
+                active: true,
+                type: 'BuildStream'
+              }
+            }
+          ]
+        }
+      }
+    });
+    expect($elScope.showBuildFailurePrompt()).to.equal(true);
+  });
+
+  it('should only show build failure on the build stream page', function () {
+    initState({}, {
+      isEditModal: false,
+      instance: {
+        attrs: apiMocks.instances.building,
+        build: {
+          attrs: apiMocks.builds.failed
+        }
+      },
+      openItems: {
+        activeHistory: {
+          models: [
+            {
+              state: {
+                active: false,
+                type: 'BuildStream'
+              }
+            },
+            {
+              state: {
+                active: true,
+                type: 'LogView'
+              }
+            }
+          ]
+        }
+      }
+    });
+    expect($elScope.showBuildFailurePrompt()).to.equal(false);
+  });
+
+  it('should have a build without cache action', function () {
+    mockUpdateInstanceWithNewBuild = sinon.stub();
+    var deepCopy = sinon.stub().callsArg(0);
+    initState({}, {
+      isEditModal: false,
+      instance: {
+        attrs: apiMocks.instances.building,
+        build: {
+          attrs: apiMocks.builds.failed,
+          deepCopy: deepCopy
+        }
+      },
+      openItems: {
+        activeHistory: {
+          models: [
+            {
+              state: {
+                active: true,
+                type: 'LogView'
+              }
+            },
+            {
+              state: {
+                active: false,
+                type: 'BuildStream'
+              }
+            }
+          ]
+        }
+      }
+    });
+    var deferred = $q.defer();
+    mockUpdateInstanceWithNewBuild.returns(deferred.promise);
+
+    $elScope.actions.buildWithoutCache();
+    expect($rootScope.dataApp.data.loading, 'Loading on root scope').to.equal(true);
+    expect(deepCopy.calledOnce, 'deep copy called').to.equal(true);
+    $elScope.$digest();
+    expect(mockUpdateInstanceWithNewBuild.calledOnce, 'Update Instance called').to.equal(true);
+
+    deferred.resolve();
+    $elScope.$digest();
+
+    expect($rootScope.dataApp.data.loading, 'Loading on root scope').to.equal(false);
+  });
+
+  it('should have a hide build failure action', function () {
+    initState({}, {
+      isEditModal: false,
+      instance: {
+        attrs: apiMocks.instances.building,
+        build: {
+          attrs: apiMocks.builds.failed
+        }
+      },
+      openItems: {
+        activeHistory: {
+          models: [
+            {
+              state: {
+                active: false
+              }
+            },
+            {
+              state: {
+                active: true,
+                type: 'BuildStream'
+              }
+            }
+          ]
+        }
+      }
+    });
+    expect($elScope.showBuildFailurePrompt()).to.equal(true);
+    $elScope.actions.hideBuildFailurePrompt();
+    expect($elScope.showBuildFailurePrompt()).to.equal(false);
   });
 
 });
