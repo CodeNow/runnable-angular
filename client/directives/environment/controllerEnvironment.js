5fa97a21eaaba66361a1104395a1f18a2014208a
Nathan219 <Nathan219@gmail.com>
2015-04-09 14:15:31 -0700

    Populating environmentOrgSelect with real data

--- client/directives/environment/controllerEnvironment.js
+++ client/directives/environment/controllerEnvironment.js
@@ -25,26 +25,15 @@ function ControllerEnvironment(
   $window
 ) {
   favico.reset();
-  var dataSetup = $scope.dataSetup = {
-    data: {
-      instanceOpts: {},
-      unsavedAcvs: []
-    },
-    actions: {}
-  };
-  $scope.state = {
-    step: 1
-  };
-
   $scope.data = {
     dataModalEditServer: {
       portTagOptions: {
         breakCodes: [
-           13, // return
-           32, // space
-           44, // comma (opera)
-           188 // comma (mozilla)
-         ],
+          13, // return
+          32, // space
+          44, // comma (opera)
+          188 // comma (mozilla)
+        ],
         texts: {
           'inputPlaceHolder': 'Add ports here',
           maxInputLength: 5,
@@ -54,49 +43,12 @@ function ControllerEnvironment(
       }
     }
   };
-
-  var data = dataSetup.data;
-  pageName.setTitle('Create Server');
-
-  dataSetup.actions.olarkShrink = function() {
-    if (angular.isFunction($window.olark)) {
-      $window.olark('api.box.shrink');
+  $scope.actions = {
+    selectAccount: function (account) {
+      $scope.data.activeAccount = account;
     }
   };
 
-  $scope.$watch('dataSetup.data.build.contextVersions.models[0].source', function(n, p) {
-    if (n && dataSetup.data.showVideo) {
-      dataSetup.data.showVideoFixed = true;
-    }
-    if (n && !p && data.showVideo) {
-      // first time user has selected a seed dockerfile, minimize olark if video is playing
-      //
-      dataSetup.actions.olarkShrink();
-    }
-  });
-
-  data.openItems = new OpenItems();
-  data.showExplorer = false;
-  data.loading = false;
-
-  fetchBuild($stateParams.buildId)
-    .then(function(build) {
-      if (keypather.get(build, 'attrs.started')) {
-        // this build has been built.
-        // redirect to new?
-        $state.go('instance.new', {
-          userName: $stateParams.userName
-        });
-        $log.error('build already built');
-      } else {
-        data.build = build;
-      }
-    });
-
-  fetchInstances()
-    .then(function (instances) {
-      data.instances = instances;
-    });
 
   $scope.$on('$destroy', function () {
   });
---
+++ client/directives/environment/modals/modalRepositorySelect/directiveModalRepositorySelector.js
@@ -0,0 +1,120 @@
+'use strict';
+
+require('app')
+  .directive('gsRepoSelector', gsRepoSelector);
+/**
+ * @ngInject
+ */
+function gsRepoSelector(
+  errs,
+  fetchOwnerRepos,
+  fetchStackAnalysis,
+  hasKeypaths,
+  keypather,
+  promisify,
+  $q,
+  $log
+) {
+  return {
+    restrict: 'A',
+    templateUrl: 'viewModalRepoSelector',
+    scope: {
+      actions: '=',
+      data: '=',
+      state: '='
+    },
+    link: function ($scope, elem, attrs) {
+      function fetchStackData(repo) {
+        return fetchStackAnalysis(repo).then(function (data) {
+          if (!data.languageFramework) {
+            $scope.state.stack = $scope.data.stacks[0];
+            $log.warn('No language detected');
+            return;
+          }
+          $scope.state.stack = $scope.data.stacks.find(hasKeypaths({
+            'key': data.languageFramework.toLowerCase()
+          })) || $scope.data.stacks[0];
+          setStackSelectedVersion($scope.state.stack, data.version);
+          if (data.serviceDependencies && data.serviceDependencies.length) {
+            return $q(function (resolve) {
+              var unwatch = $scope.$watch('data.allDependencies', function (allDeps) {
+                if (allDeps) {
+                  unwatch();
+                  var includedDeps = data.serviceDependencies.map(
+                    function (dep) {
+                      return allDeps.models.find(
+                        hasKeypaths({'attrs.name.toLowerCase()': dep.toLowerCase()})
+                      ) || false;
+                    }
+                  );
+                  includedDeps.forEach(function (dep) {
+                    if (dep) {
+                      $scope.actions.addDependency(dep);
+                    }
+                  });
+                  resolve();
+                }
+              });
+            });
+          }
+        });
+      }
+      $scope.selectRepo = function (repo) {
+        if ($scope.state.repoSelected) { return; }
+        $scope.state.repoSelected = true;
+        repo.spin = true;
+        $scope.state.ports = null;
+        $scope.state.startCommand = null;
+        $scope.state.extraEnvs = null;
+        $scope.state.selectedRepo = repo;
+        promisify(repo.branches, 'fetch')(
+        ).then(function getActiveBranch() {
+          $scope.state.activeBranch =
+              repo.branches.models.find(hasKeypaths({'attrs.name': 'master'}));
+          if (!$scope.state.activeBranch) {
+            return $q.reject(new Error('No branches found'));
+          }
+        }).then(function () {
+          return fetchStackData(repo.attrs.full_name);
+        }).then(function () {
+          $scope.actions.nextStep(2);
+        }).catch(function (err) {
+          $scope.state.repoSelected = false;
+          errs.handler(err);
+        }).finally(function () {
+          delete repo.spin;
+        });
+      };
+
+      $scope.$watch('data.activeAccount', function (n) {
+        if (n) {
+          $scope.loading = true;
+          $scope.githubRepos = null;
+          fetchOwnerRepos(
+            n.oauthName()
+          ).then(function (repoList) {
+            // Actually check the value on the scope since it isn't cached
+            if (keypather.get(repoList, 'ownerUsername') === $scope.data.activeAccount.oauthName()) {
+              $scope.githubRepos = repoList;
+            }
+          }).catch(
+            errs.handler
+          ).finally(function () {
+            $scope.loading = false;
+          });
+        }
+      });
+
+      function setStackSelectedVersion(stack, versions) {
+        if (versions[stack.key]) {
+          stack.selectedVersion = versions[stack.key];
+        }
+        if (stack.dependencies) {
+          stack.dependencies.forEach(function (childStack) {
+            setStackSelectedVersion(childStack, versions);
+          });
+        }
+      }
+    }
+  };
+}
