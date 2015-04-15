3555f75d4ee238255b7959cfa7adee9a6e4eee89
Myztiq <kahn@runnable.com>
2015-04-15 14:48:19 -0700

    Got fancy-select up and running like we want for the moment.

--- client/directives/fancySelect/directiveFancySelect.js
+++ client/directives/fancySelect/directiveFancySelect.js
@@ -6,35 +6,97 @@ require('app')
  * @ngInject
  */
 function fancySelect(
-  $document
+  $document,
+  $timeout
 ) {
   return {
     restrict: 'E',
     templateUrl: 'viewFancySelect',
     transclude: true,
+    scope: {
+      value: '=',
+      placeholder: '@'
+    },
     link: function ($scope, element, attrs, controller, transcludeFn) {
       var transcludedContent;
       var transclusionScope;
 
-      var list =  element.find('ul');
-      //$scope.isOpen = false;
-      //
-      //$scope.actions = {
-      //  toggleSelect: function () {
-      //    console.log('Toggle Select');
-      //    $scope.open = true;
-      //
-      //  },
-      //  updateSelect: function (evt) {
-      //    console.log('Updating select', evt);
-      //  }
-      //};
+      $scope.placeholder = $scope.placeholder || 'Select an item';
 
+      var list =  element.find('ul');
+      $scope.isOpen = false;
+
+      var positionDropdown = false;
+
+      function openDropdown() {
+        if (positionDropdown){
+          return;
+        }
+        $scope.isOpen = true;
+        positionDropdown = true;
+        $document.find('body').append(list);
+      }
+
+      function closeDropdown() {
+        $scope.isOpen = false;
+        $timeout(function () {
+          element.append(list);
+          positionDropdown = false;
+        }, 200);
+      }
+
+      $scope.getDropdownStyles = function () {
+        if (!positionDropdown) {
+          return;
+        }
+        var button = element.find('button')[0];
+        var boundingRect = button.getBoundingClientRect();
+
+        return {
+          top: boundingRect.top + button.offsetHeight + 'px',
+          left: boundingRect.left + 'px'
+        };
+      };
+
+
+      $scope.actions = {
+        toggleSelect: function () {
+          if ($scope.isOpen) {
+            closeDropdown();
+          } else {
+            openDropdown();
+          }
+        },
+        clickedOption: function (clickedOption) {
+          $scope.value = clickedOption.value;
+          closeDropdown();
+        }
+      };
+
+      var options = [];
+
+      $scope.registerOption = function (option) {
+        options.push(option);
+      };
 
       transcludeFn($scope, function(clone, innerScope ){
         list.append(clone);
         transcludedContent = clone;
         transclusionScope = innerScope;
+      });
+
+      $scope.$watch('value', function (newValue) {
+        var matchedOption = options.find(function (option) {
+          return option.value === newValue;
+        });
+
+        if (matchedOption) {
+          options.forEach(function (option) {
+            option.selected = false;
+          });
+          matchedOption.selected = true;
+          angular.element(element[0].querySelector('.display')).html(matchedOption.element.html());
+        }
       });
     }
   };
