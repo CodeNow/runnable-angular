a3fac0ec14c9d6e859524eeae6c146ce91fda1bf
Myztiq <kahn@runnable.com>
2015-04-22 18:01:48 -0700

    dataApp->data and removed unused import, cleaned up controllerOrgSelect

--- client/controllers/controllerOrgSelect.js
+++ client/controllers/controllerOrgSelect.js
@@ -10,18 +10,13 @@ require('app')
  */
 function ControllerOrgSelect(
   $scope,
-  $state,
-  keypather
+  $state
 ) {
   $scope.actions = {
     selectAccount: function (account) {
-      var username = keypather.get(account, 'attrs.accounts.github.username');
-      if (!username) {
-        username = keypather.get(account, 'attrs.login');
-      }
       $scope.$emit('close-modal');
       $state.go('instance.home', {
-        userName: username
+        userName: account.oauthName()
       });
     }
   };
