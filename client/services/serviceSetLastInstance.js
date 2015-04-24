7d70ac6eaeabf5629c3149c13e85f3e8e3d96379
SomeKittens <rkoutnik@gmail.com>
2015-04-24 15:51:57 -0700

    removed some console logs

--- client/services/serviceSetLastInstance.js
+++ client/services/serviceSetLastInstance.js
@@ -9,7 +9,6 @@ function setLastInstance(
   errs
 ) {
   return function (instanceName) {
-    console.log('Setting last instance', instanceName);
     return pFetchUser().then(function (user){
       return promisify(user, 'update')({
         'userOptions.uiState.previousLocation.instance': instanceName
