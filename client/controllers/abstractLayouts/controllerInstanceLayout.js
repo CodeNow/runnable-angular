615e646a9ed8de7d8925fbe5b07447bb3e0974c7
SomeKittens <rkoutnik@gmail.com>
2015-04-29 14:38:20 -0700

    fix controllerInstanceLayout tests

--- client/controllers/abstractLayouts/controllerInstanceLayout.js
+++ client/controllers/abstractLayouts/controllerInstanceLayout.js
@@ -34,27 +34,10 @@ function ControllerInstanceLayout(
     resolveInstanceFetch(n);
   });
 
-  keypather.set($scope, 'dataApp.actions.setToggled', function (teamMember) {
-    if (!keypather.get($scope, 'dataApp.data.instanceGroups.teamMembers.length')) {
-      return;
-    }
-    if (teamMember && teamMember.toggled) {
-      teamMember.toggled = false;
-      return;
-    }
-    $scope.dataApp.data.instanceGroups.teamMembers.forEach(function (tm) {
-      tm.toggled = false;
-    });
-    if (teamMember) {
-      teamMember.toggled = true;
-    }
-  });
-
   function resolveInstanceFetch(username) {
     if (!username) { return; }
     keypather.set($rootScope, 'dataApp.state.loadingInstances', true);
-    keypather.set($rootScope, 'dataApp.data.instances', null);
-    keypather.set($rootScope, 'dataApp.data.instanceGroups', null);
+    keypather.set($rootScope, 'dataApp.data.instancesByPod', null);
 
     fetchInstancesByPod(username)
     .then(function (instancesByPod) {
