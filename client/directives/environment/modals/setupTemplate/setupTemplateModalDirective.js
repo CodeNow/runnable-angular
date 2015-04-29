d901c5b47672e6e6de0bc49ff004e431a91ea12d
Nathan219 <Nathan219@gmail.com>
2015-04-28 20:10:39 -0700

    Creating directive for template list Moving files Term.js min height now 10 lines Descriptions now fetched from server for template list

---
+++ client/directives/environment/modals/setupTemplate/setupTemplateModalDirective.js
@@ -0,0 +1,63 @@
+'use strict';
+
+require('app')
+  .directive('setupTemplateModal', setupTemplateModal);
+/**
+ * @ngInject
+ */
+function setupTemplateModal(
+  $rootScope,
+  copySourceInstance,
+  getNewForkName,
+  keypather,
+  pFetchUser,
+  promisify
+) {
+  return {
+    restrict: 'A',
+    templateUrl: 'setupTemplateModalView',
+    scope: {
+      actions: '=',
+      data: '=',
+      defaultActions: '='
+    },
+    link: function ($scope) {
+      $scope.$watch('data.allDependencies', function (n) {
+        if (n) {
+          n.models.forEach(function (templateInstance) {
+            if (!templateInstance.description) {
+              return promisify(templateInstance.build.contexts.models[0], 'fetch')()
+                .then(function (context) {
+                  templateInstance.description =
+                      keypather.get(context, 'attrs.description');
+                });
+            }
+          });
+        }
+      });
+
+      $scope.addServerFromTemplate = function(sourceInstance) {
+        var serverName = getNewForkName(sourceInstance, $scope.data.instances, true);
+
+        var serverModel = {
+          opts: {
+            name: serverName,
+            masterPod: true
+          }
+        };
+        return $scope.actions.createAndBuild(
+          copySourceInstance(
+            $rootScope.dataApp.data.activeAccount,
+            sourceInstance,
+            serverName
+          )
+            .then(function (build) {
+              serverModel.build = build;
+              return serverModel;
+            }),
+          serverName
+        );
+      };
+    }
+  };
+}
