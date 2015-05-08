caa6298c255e3f0e842e2ae7ad7fa74122394d5f
Myztiq <kahn@runnable.com>
2015-05-08 13:40:21 -0700

    Initial commit with new loading service.

--- client/controllers/setup/controllerNew.js
+++ client/controllers/setup/controllerNew.js
@@ -9,14 +9,15 @@ function ControllerNew(
   errs,
   createNewBuild,
   $scope,
-  $state
+  $state,
+  loading
 ) {
-  $scope.dataApp.data.loading = true;
+  loading('main', true);
   var unwatch = $scope.$watch('dataApp.data.activeAccount', function (account) {
     if (account) {
       unwatch();
       createNewBuild(account).then(function (build) {
-        $scope.dataApp.data.loading = false;
+        loading('main', false);
         $state.go('instance.setup', {
           userName: $state.params.userName,
           buildId: build.id()
