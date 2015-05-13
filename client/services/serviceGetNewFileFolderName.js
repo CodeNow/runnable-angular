cf03b9c7225147e08efd3aaaa2c152fb77586146
SomeKittens <rkoutnik@gmail.com>
2015-05-13 15:39:31 -0700

    much shorter solution in getNewFileFolderName

--- client/services/serviceGetNewFileFolderName.js
+++ client/services/serviceGetNewFileFolderName.js
@@ -7,36 +7,15 @@ require('app')
  */
 function getNewFileFolderName() {
   return function (dir) {
-    var genericFileName = 'newfile';
-    var reg = new RegExp(genericFileName + '( [0-9]+)?');
+    var newFileName = 'newfile';
+    var count = 0;
+    var filenames = dir.contents.models.map(function (model) { return model.attrs.name; });
 
-    var nextNum = null;
-
-    var exitedEarly = dir.contents.models
-      .reduce(function(arr, model) {
-        if (reg.test(model.attrs.name)) {
-          arr.push(model.attrs.name);
-        }
-        return arr;
-      }, [])
-      .sort()
-      .some(function(val, index) {
-        nextNum = index;
-        if (!index) {
-          if (val !== genericFileName) {
-            nextNum = null;
-            return true;
-          }
-          return false;
-        }
-        if (val !== genericFileName + ' ' + (index - 1)) {
-          return true;
-        }
-      });
-
-    if (nextNum === null) {
-      return genericFileName;
+    while (filenames.indexOf(newFileName) > -1){
+      newFileName = 'newfile ' + count;
+      count++;
     }
-    return genericFileName + ' ' + (exitedEarly ? nextNum - 1 : nextNum);
+
+    return newFileName;
   };
 }
