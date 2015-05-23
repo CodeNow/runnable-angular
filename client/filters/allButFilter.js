9902973cda34a0b342e720340413bc486f4df327
Nathan219 <Nathan219@gmail.com>
2015-05-22 18:40:15 -0700

    add the check for nullness for reposToFilter

--- client/filters/allButFilter.js
+++ client/filters/allButFilter.js
@@ -6,7 +6,7 @@ require('app')
 // Filters out all repos that don't match
 function allBut(hasKeypaths) {
   return function (inputData, reposToFilter) {
-    if (!inputData || !inputData.length) { return []; }
+    if (!inputData || !inputData.length || !reposToFilter) { return []; }
     return inputData.filter(function (datum) {
       return !reposToFilter.find(hasKeypaths({
         name: datum.attrs.name
