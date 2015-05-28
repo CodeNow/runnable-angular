69f6766f81ba515373094a3d70850fb6188b4a1c
Nathan219 <Nathan219@gmail.com>
2015-05-27 19:47:44 -0700

    These are changes that block the user from ignoring files too fast Keeps everything in sync Blocks input when processing

--- client/directives/environment/modals/forms/formTranslation/diffDisplay/diffDisplayDirective.js
+++ client/directives/environment/modals/forms/formTranslation/diffDisplay/diffDisplayDirective.js
@@ -2,8 +2,6 @@
 
 require('app')
   .directive('diffDisplay', function fileEditor(
-    createTransformRule,
-    keypather
   ) {
     return {
       restrict: 'A',
@@ -15,26 +13,6 @@ require('app')
         state: '='
       },
       link: function ($scope, element, attrs) {
-        $scope.ignore = {
-          toggleIgnoreFile: function (fileDiff) {
-            if (fileDiff.ignoring) { return; }
-            var acv = keypather.get($scope, 'state.contextVersion.getMainAppCodeVersion()');
-            fileDiff.ignoring = true;
-            if (acv) {
-              var newArray;
-              if ($scope.actions.checkFileIgnored(fileDiff)) {
-                newArray = acv.attrs.transformRules.exclude;
-                newArray.splice(newArray.indexOf(fileDiff.from), 1);
-              } else {
-                newArray = acv.attrs.transformRules.exclude.concat(fileDiff.from);
-              }
-              createTransformRule(acv, newArray)
-                .finally(function () {
-                  fileDiff.ignoring = false;
-                });
-            }
-          }
-        };
       }
     };
   });
