15e53ab850fbdd0bb1ad71f5e1e9f86410c2bb7b
Casey Flynn <cflynn.us@gmail.com>
2015-05-28 13:30:30 -0700

    also allow underscore

--- client/services/serviceValidateEnvVars.js
+++ client/services/serviceValidateEnvVars.js
@@ -27,7 +27,7 @@ function validateEnvVars() {
         return;
       }
       // Check for syntactic validity
-      if (!/^([A-z]+[A-z0-9]*)=.*$/.test(line)) {
+      if (!/^([A-z]+[A-z0-9_]*)=.*$/.test(line)) {
         response.valid = false;
         response.errors.push(index);
         return;
