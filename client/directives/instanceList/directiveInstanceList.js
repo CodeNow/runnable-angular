0d09febf9344e4408610df0e71b014ee46736fff
Nathan219 <Nathan219@gmail.com>
2015-05-29 17:41:11 -0700

    test

--- client/directives/instanceList/directiveInstanceList.js
+++ client/directives/instanceList/directiveInstanceList.js
@@ -43,6 +43,7 @@ function instanceList(
           });
         }
       });
+      //
 
       $scope.getInstanceClasses = getInstanceClasses;
       $scope.getInstanceAltTitle = getInstanceAltTitle;
