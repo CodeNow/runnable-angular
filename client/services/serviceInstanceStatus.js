c67590ab8fdb153d13174cc79704f0c55ba77d0c
Casey Flynn <cflynn.us@gmail.com>
2015-06-03 15:26:06 -0700

    new state for deployed but not yet started

--- client/services/serviceInstanceStatus.js
+++ client/services/serviceInstanceStatus.js
@@ -16,6 +16,7 @@ unknown
 
  */
 function instanceStatus(keypather) {
+  var jesusBirthday = new Date('0001-01-01T00:00:00Z').valueOf();
   return function (instance) {
     var container = keypather.get(instance, 'containers.models[0]');
     var build = keypather.get(instance, 'build');
@@ -23,6 +24,11 @@ function instanceStatus(keypather) {
       if (!container.running()) {
         if (keypather.get(container, 'attrs.inspect.State.ExitCode') === -1) {
           return 'stopped';
+        }
+        if (keypather.get(container, 'attrs.inspect.State.ExitCode') === 0 &&
+            new Date(keypather.get(container, 'attrs.inspect.State.StartedAt')).valueOf() === jesusBirthday) {
+          // container has been deployed but not yet started
+          return 'building';
         }
         return 'crashed';
       }
