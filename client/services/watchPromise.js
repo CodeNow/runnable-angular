582a7694996fc1536907def99f42fbf4a4974c37
Nathan219 <Nathan219@gmail.com>
2015-06-05 16:54:56 -0700

    lower unwatch

--- client/services/watchPromise.js
+++ client/services/watchPromise.js
@@ -4,11 +4,10 @@ require('app')
   .factory('watchOncePromise', function watchOncePromise(
     $q
   ) {
-    var unWatch;
     return function ($scope, watchMe, returnWhen) {
       var returnWhenUndefined = returnWhen === undefined;
       return $q(function (resolve) {
-        unWatch = $scope.$watch(watchMe, function watchWhen(n) {
+        var unWatch = $scope.$watch(watchMe, function watchWhen(n) {
           // Convert both values to bools before we check.
           // Unless we're looking for an undefined value
           var normalized = returnWhenUndefined ? !!n : n;
