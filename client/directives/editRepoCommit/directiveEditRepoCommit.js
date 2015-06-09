f5c3b7aa7d06f8e8b7a01943739161ceab8a007c
Myztiq <kahn@runnable.com>
2015-06-08 17:50:56 -0700

    Cleanup directive repo list

--- client/directives/editRepoCommit/directiveEditRepoCommit.js
+++ client/directives/editRepoCommit/directiveEditRepoCommit.js
@@ -6,98 +6,37 @@ require('app')
  * @ngInject
  */
 function editRepoCommit(
-  fetchCommitData,
-  errs
+  fetchCommitData
 ) {
   return {
     restrict: 'A',
     templateUrl: 'viewEditRepoCommit',
     scope: {
-      model: '='
+      acv: '= model'
     },
-    link: function ($scope, elem, attrs) {
-      // emits (broadcast up DOM tree) event to be
-      // intercepted by directive-runnable-repo-list
-      // when ACV selected commit changes
-      function emitACVChange(opts) {
-        $scope.$emit('acv-change', opts);
-      }
+    link: function ($scope) {
+      $scope.activeCommit = fetchCommitData.activeCommit($scope.acv);
 
-      $scope.activeBranch = null;
-      $scope.activeCommit = null;
-      $scope.commitsBehind = null;
-
-      // use watchers to share branch/commit between
-      // this scope and popover + keep sync
-      $scope.$watch('activeBranch', function (n) {
-        if (n) { $scope.popoverRepositoryToggle.data.activeBranch = n; }
-      });
-      $scope.$watch('activeCommit', function (n) {
-        if (n) { $scope.popoverRepositoryToggle.data.activeCommit = n; }
-      });
-
-      $scope.popoverRepositoryToggle = {
-        data: {},
-        actions: {}
-      };
-
-      $scope.popoverRepositoryToggle.actions.selectBranch = function (activeBranch) {
-        $scope.activeBranch = activeBranch;
-        // Silence errors on bad branch fetch
-        // We don't want a 404 modal when we can't find the branch
-        fetchCommitData.branchCommits(activeBranch);
-      };
-
-      $scope.popoverRepositoryToggle.actions.selectCommit = function (commitSha) {
-        $scope.$broadcast('close-popovers');
-        $scope.model.unsavedAcv.branch = $scope.activeBranch.attrs.name;
-        $scope.model.unsavedAcv.commit = commitSha;
-        $scope.activeCommit = fetchCommitData.activeCommit($scope.model.acv, commitSha);
-        emitACVChange(
-          {
-            acv: $scope.model.acv,
-            updateOpts: $scope.model.unsavedAcv
-          }
-        );
-      };
-      // controls appearance of
-      // gear-menu popover
-      // to fast-forward/delete
-      $scope.showPendingClassWhenSelectedOutdatedCommit = true;
-
-      // reset filter when opening popover
-      $scope.$watch('popoverRepositoryToggle.data.show', function (n, p) {
-        // reset branch if selected commit does
-        // not belong to selected branch
-        // on popoverRepositoryToggle close
-        if (n === false && p === true) {
-          // was open, is now closed
-          $scope.activeBranch = fetchCommitData.activeBranch($scope.model.acv, $scope.model.unsavedAcv.branch);
-        } else if (!n) { return; }
-      });
-
-      $scope.popoverRepoActions = {
-        data: {},
-        actions: {}
-      };
-      $scope.popoverRepositoryToggle.actions.deleteRepo = function () {
-        $scope.model.acv.destroy(errs.handler);
-      };
-
-      $scope.$watch('model', function (n) {
-        if (n && n.acv) {
-          $scope.activeBranch = fetchCommitData.activeBranch($scope.model.acv);
-          $scope.activeCommit = fetchCommitData.activeCommit($scope.model.acv);
-
-          $scope.popoverRepoActions.data.acv = $scope.model.acv;
-          $scope.popoverRepoActions.data.unsavedAcv = $scope.model.unsavedAcv;
-          $scope.popoverRepositoryToggle.data.acv = $scope.model.acv;
-          $scope.popoverRepositoryToggle.data.unsavedAcv = $scope.model.unsavedAcv;
-          // Silence errors on bad branch fetch
-          // We don't want a 404 modal when we can't find the branch
-          fetchCommitData.branchCommits($scope.activeBranch);
+      $scope.actions = {
+        toggleEditCommits: function () {
+          var branch = fetchCommitData.activeBranch($scope.acv);
+          fetchCommitData.branchCommits(branch);
+          $scope.popoverRepositoryToggle.data.branch = branch;
+          $scope.popoverRepositoryToggle.data.commit = $scope.activeCommit;
         }
-      });
+      };
+      $scope.popoverRepositoryToggle = {
+        data: {
+          acv: $scope.acv
+        },
+        actions: {
+          selectCommit: function (commitSha) {
+            $scope.acv.commit = commitSha;
+            $scope.$emit('change-commit', commitSha);
+            $scope.$broadcast('close-popovers');
+          }
+        }
+      };
     }
   };
 }
