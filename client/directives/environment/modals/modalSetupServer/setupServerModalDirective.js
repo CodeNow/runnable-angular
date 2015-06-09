7f01854bf1eea01a75d2740148c2f7db9e1bf27f
Nathan219 <Nathan219@gmail.com>
2015-06-08 17:49:22 -0700

    Should be at $scope.state.acv

--- client/directives/environment/modals/modalSetupServer/setupServerModalDirective.js
+++ client/directives/environment/modals/modalSetupServer/setupServerModalDirective.js
@@ -95,8 +95,8 @@ function setupServerModal(
             );
           })
           .then(function () {
-            if ($scope.acv.attrs.branch !== $scope.state.branch.attrs.name) {
-              return promisify($scope.acv, 'update')({
+            if ($scope.state.acv.attrs.branch !== $scope.state.branch.attrs.name) {
+              return promisify($scope.state.acv, 'update')({
                 repo: $scope.state.repo.attrs.full_name,
                 branch: $scope.state.branch.attrs.name,
                 commit: $scope.state.branch.attrs.commit.sha
