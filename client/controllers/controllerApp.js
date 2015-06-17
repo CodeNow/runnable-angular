2182401db815da8799519ec8e7cd4b993d96aa8f
Nathan219 <Nathan219@gmail.com>
2015-06-16 17:24:49 -0700

    Adding 404 redirect logic

--- client/controllers/controllerApp.js
+++ client/controllers/controllerApp.js
@@ -106,6 +106,9 @@ function ControllerApp(
     eventTracking.update();
     loading('main', false);
   });
+  $scope.$on('$stateChangeError', function () {
+    $state.go('404');
+  });
 
   $scope.$watch(function () {
     return errs.errors.length;
