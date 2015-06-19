09bb00dc9994a6dba585d874ad9c03643a3b35eb
Myztiq <kahn@runnable.com>
2015-06-18 17:36:24 -0700

    Updated to return an empty obj instead of contextVersions

--- test/unit/directives/directiveDnsManager.unit.js
+++ test/unit/directives/directiveDnsManager.unit.js
@@ -45,9 +45,7 @@ describe('directiveDnsManager'.bold.underline.blue, function() {
         noStore: true
       });
 
-      instance.contextVersion.getMainAppCodeVersion = function () {
-        return instance.attrs.contextVersions[0];
-      };
+      instance.contextVersion.getMainAppCodeVersion = sinon.stub().returns({});
 
       childInstances.models[0].attrs.contextVersion = {
         context: instance.attrs.contextVersion.context
