6638baaa9603692687563a849412b34e3217e700
Nathan219 <Nathan219@gmail.com>
2015-06-23 00:52:49 -0700

    Making the edit update path a lot easier to read More unit tests

--- test/unit/environment/editServerModalDirective.unit.js
+++ test/unit/environment/editServerModalDirective.unit.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('editServerModalDirective'.bold.underline.blue, function () {
+describe.only('editServerModalDirective'.bold.underline.blue, function () {
   var ctx;
   var $timeout;
   var $scope;
@@ -170,10 +170,20 @@ describe('editServerModalDirective'.bold.underline.blue, function () {
       runnable,
       apiMocks.contextVersions.running
     );
+    ctx.contextVersion.appCodeVersions.models = [
+      {
+        attrs: apiMocks.appCodeVersions.bitcoinAppCodeVersion
+      }
+    ];
     ctx.newContextVersion = apiClientMockFactory.contextVersion(
       runnable,
       apiMocks.contextVersions.setup
     );
+    ctx.newContextVersion.appCodeVersions.models = [
+      {
+        attrs: apiMocks.appCodeVersions.bitcoinAppCodeVersion
+      }
+    ];
     sinon.stub(ctx.contextVersion, 'deepCopy', function (cb) {
       $rootScope.$evalAsync(function () {
         cb(null, ctx.newContextVersion);
@@ -190,6 +200,7 @@ describe('editServerModalDirective'.bold.underline.blue, function () {
       return ctx.newContextVersion;
     });
     sinon.stub(ctx.newContextVersion, 'fetchFile', function (opts, cb) {
+      console.log('****');
       $rootScope.$evalAsync(function () {
         cb(null, ctx.dockerfile);
       });
@@ -310,7 +321,46 @@ describe('editServerModalDirective'.bold.underline.blue, function () {
         expect($elScope.state.ports).to.be.ok;
         $scope.$digest();
         $scope.$digest();
-        sinon.assert.calledOnce(ctx.newContextVersion.fetchFile);
+        sinon.assert.notCalled(ctx.newContextVersion.fetchFile);
+        $scope.$digest();
+        sinon.assert.notCalled(ctx.fetchDockerfileFromSourceMock.getFetchSpy());
+        sinon.assert.notCalled(ctx.populateDockerfile.getFetchSpy());
+        $scope.$digest();
+        sinon.assert.calledOnce(ctx.build.build);
+        $scope.$digest();
+        expect($elScope.state.opts.build).to.be.ok;
+        $scope.$digest();
+        sinon.assert.calledOnce(ctx.helpCards.refreshActiveCard);
+        $scope.$digest();
+        sinon.assert.calledOnce($scope.defaultActions.close);
+
+        sinon.assert.calledOnce(ctx.instance.update);
+        sinon.assert.notCalled(ctx.instance.redeploy);
+      });
+      it('should create dockerfile when ports change', function () {
+        var alertSpy = sinon.spy();
+        var closePopoverSpy = sinon.spy();
+        $rootScope.$on('close-popovers', closePopoverSpy);
+        $rootScope.$on('alert', function (event, opts) {
+          expect(opts).to.be.deep.equal({
+            type: 'success',
+            text: 'Container updated successfully.'
+          });
+        });
+        ctx.loadingPromiseFinishedValue = 2;
+
+        keypather.set($elScope, 'portTagOptions.tags.tags', {0: '123'});
+        $elScope.state.opts.env = ['asdasd', 'sadfsdfasdfasdf'];
+        $elScope.getUpdatePromise();
+        $scope.$digest();
+        sinon.assert.called(closePopoverSpy);
+        sinon.assert.called(ctx.loadingPromiseMock.finished);
+        expect($elScope.building).to.be.true;
+        expect($elScope.state.ports).to.be.ok;
+        $scope.$digest();
+        $scope.$digest();
+        $scope.$digest();
+        //sinon.assert.calledOnce(ctx.newContextVersion.fetchFile);
         ctx.fetchDockerfileFromSourceMock.triggerPromise({attrs: 'dockerfile'});
         $scope.$digest();
         sinon.assert.calledOnce(ctx.fetchDockerfileFromSourceMock.getFetchSpy());
