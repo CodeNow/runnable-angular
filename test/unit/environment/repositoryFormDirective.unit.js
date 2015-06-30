3945b000f57079a136c5c896764e439c09a1a270
Myztiq <kahn@runnable.com>
2015-06-30 16:53:50 -0700

    Fixing javascript.

--- test/unit/environment/repositoryFormDirective.unit.js
+++ test/unit/environment/repositoryFormDirective.unit.js
@@ -125,7 +125,7 @@ describe('repositoryFormDirective'.bold.underline.blue, function () {
       $elScope = ctx.element.isolateScope();
       $scope.$digest();
       expect($elScope.data).to.deep.equal({
-        cacheCommand = false
+        cacheCommand: false
       });
       expect($elScope.state).to.equal(scope.state);
       expect($elScope.startCommandCanDisable).to.be.true;
