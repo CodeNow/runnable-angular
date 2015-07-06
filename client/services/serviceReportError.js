bdefcd8a74e857bbbaee5eca076172a7b3e472b6
Myztiq <kahn@runnable.com>
2015-07-06 13:03:29 -0700

    Added tracking data to rollbar error reporting.

--- client/services/serviceReportError.js
+++ client/services/serviceReportError.js
@@ -3,12 +3,28 @@
 require('app')
   .factory('reportError', reportError);
 
-function reportError() {
+function reportError(
+  keypather,
+  $rootScope
+) {
   return function (err, options) {
     if (window.NREUM) {
       window.NREUM.noticeError(err, options);
     }
     if (window.Rollbar) {
+
+      var user = {};
+      user.email = keypather.get($rootScope, 'dataApp.data.user.attrs.email');
+      user.username = keypather.get($rootScope, 'dataApp.data.user.attrs.accounts.github.username');
+      user.id = keypather.get($rootScope, 'dataApp.data.user.attrs.id');
+
+      window.Rollbar.configure({
+        payload: {
+          person: user
+        }
+      });
+
+
       window.Rollbar.error(err, options);
     }
   };
