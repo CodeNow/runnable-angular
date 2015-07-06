0754d013486a4f7f5daf754e522c0cd4f5a3042e
Myztiq <kahn@runnable.com>
2015-07-06 16:56:51 -0700

    Added logic to redirect ot login if we have bad-credentials.

--- client/services/serviceHandleErr.js
+++ client/services/serviceHandleErr.js
@@ -8,7 +8,9 @@ function errs (
   keypather,
   hasKeypaths,
   $log,
-  reportError
+  reportError,
+  configAPIHost,
+  $window
 ) {
   // codes that do not need to be displayed to user
   var noDisplayCodes = [401, 403];
@@ -16,6 +18,11 @@ function errs (
   return {
     handler: function (err) {
       if (err) {
+        if (err.message === 'Bad credentials') {
+          window.location = configAPIHost + '/auth/github?redirect=' + $window.location.protocol + '//' + $window.location.host + '/?auth';
+          return;
+        }
+
         if (~noDisplayCodes.indexOf(keypather.get(err, 'data.statusCode'))) { return; }
         if (!errors.find(hasKeypaths({ 'message': err.message }))) {
           errors.push(err);
