10be8fdcd3c5247f153fa223d4903f43414c0484
runnabro <tli415@gmail.com>
2015-07-09 14:38:01 -0700

    update grunt-sass options
    - add source maps to sass
    - add source maps to autoprefixer
    - compress output

--- Gruntfile.js
+++ Gruntfile.js
@@ -58,7 +58,8 @@ module.exports = function(grunt) {
     autoprefixer: {
       dist: {
         options: {
-          browsers: ['last 2 versions']
+          browsers: ['last 2 versions'],
+          map: true
         },
         files: {
           'client/build/css/index.css' : 'client/build/css/index.css',
@@ -70,7 +71,7 @@ module.exports = function(grunt) {
     sass: {
       compile: {
         options: {
-          style: 'compressed'
+          outputStyle: 'compressed'
         },
         files: {
           'client/build/css/error.css': sassError,
@@ -80,8 +81,8 @@ module.exports = function(grunt) {
       },
       dev: {
         options: {
-          lineNumbers: true,
-          style: 'expanded'
+          outputStyle: 'nested',
+          sourceMap: true
         },
         files: {
           'client/build/css/error.css': sassError,
