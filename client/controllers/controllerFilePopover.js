4d4546a7ceb81d94e94c3f5e4beed33968171b5e
Myztiq <kahn@runnable.com>
2015-07-23 16:34:35 -0700

    WIP

---
+++ client/controllers/controllerFilePopover.js
@@ -0,0 +1,95 @@
+'use strict';
+
+require('app')
+  .controller('ControllerFilePopover', ControllerFilePopover);
+/**
+ * @ngInject
+ */
+function ControllerFilePopover(
+  $q,
+  uploadFile,
+  configAPIHost,
+  errs,
+  promisify,
+  $scope,
+  loadingPromises,
+  helperCreateFSpromise
+) {
+  var self = this;
+  this.dir = $scope.dir;
+  this.loadingPromisesTarget = $scope.loadingPromisesTarget || 'generic';
+
+  this.actions = {
+    createFile: function () {
+      loadingPromises.add(self.loadingPromisesTarget, helperCreateFSpromise($scope.dir, {
+        isDir: false
+      }))
+        .catch(errs.handler);
+      $scope.$broadcast('close-popovers');
+    },
+    createFolder: function () {
+      loadingPromises.add(self.loadingPromisesTarget, helperCreateFSpromise($scope.dir, {
+        isDir: true
+      }))
+        .catch(errs.handler);
+      $scope.$broadcast('close-popovers');
+    },
+    renameFolder: function () {
+      $scope.editFolderName = true;
+      $scope.actions.focusInputElement();
+      $scope.$broadcast('close-popovers');
+    },
+    deleteFolder: function () {
+      loadingPromises.add(self.loadingPromisesTarget, promisify($scope.dir, 'destroy')())
+        .catch(errs.handler);
+      $scope.$broadcast('close-popovers');
+    },
+    uploadFiles: function (files) {
+      if (files && files.length) {
+        $scope.$broadcast('close-popovers');
+
+        var uploadURL = configAPIHost + '/' + $scope.fileModel.urlPath + '/' + $scope.fileModel.id() + '/files';
+        var fileUploadPromises = files.map(function (file) {
+          var myFile = {
+            attrs: {
+              name: file.name,
+              isDir: false
+            },
+            state: {
+              uploading: true,
+              progress: 0
+            }
+          };
+
+          $scope.dir.contents.models.push(myFile);
+          return uploadFile(file, uploadURL)
+            .progress(function (evt) {
+              myFile.state.progress = parseInt(100.0 * evt.loaded / evt.total, 10);
+            })
+            .then(function () {
+              myFile.state.progress = 100;
+            })
+            .catch(function (err) {
+              var fileIndex = $scope.dir.contents.models.indexOf(myFile);
+              $scope.dir.contents.models.splice(fileIndex, 1);
+              errs.handler(err);
+            })
+            .then(function () {
+              return myFile;
+            });
+
+        });
+
+        $q.all(fileUploadPromises).then(function (uploads) {
+          uploads.forEach(function (myFile) {
+            var fileIndex = $scope.dir.contents.models.indexOf(myFile);
+            if (fileIndex !== -1) {
+              $scope.dir.contents.models.splice(fileIndex, 1);
+            }
+          });
+          $scope.actions.fetchDirFiles();
+        });
+      }
+    }
+  };
+}
--- client/directives/fileTreeDir/directiveFileTreeDir.js
+++ client/directives/fileTreeDir/directiveFileTreeDir.js
@@ -9,12 +9,9 @@ require('app')
 function fileTreeDir(
   $rootScope,
   keypather,
-  uploadFile,
   errs,
   $q,
   promisify,
-  helperCreateFSpromise,
-  configAPIHost,
   fetchCommitData,
   cardInfoTypes,
   loadingPromises
@@ -35,7 +32,7 @@ function fileTreeDir(
       loadingPromisesTarget: '='
     },
     templateUrl: 'viewFileTreeDir',
-    link: function ($scope, element, attrs) {
+    link: function ($scope, element) {
 
       var actions = $scope.actions = {};
       $scope.data = {};
@@ -104,6 +101,11 @@ function fileTreeDir(
           return;
         }
         file.rename(newValue, errs.handler);
+      };
+
+      $scope.actions.focusInputElement = function () {
+        inputElement.focus();
+        inputElement.select();
       };
 
       actions.handleClickOnFileInput = function (event, file) {
@@ -195,80 +197,6 @@ function fileTreeDir(
           top: -16,
           left: 10,
           mouse: true
-        },
-        actions: {
-          createFile: function () {
-            loadingPromises.add($scope.loadingPromisesTarget, helperCreateFSpromise($scope.dir, {
-              isDir: false
-            }))
-              .catch(errs.handler);
-            $scope.$broadcast('close-popovers');
-          },
-          createFolder: function () {
-            loadingPromises.add($scope.loadingPromisesTarget, helperCreateFSpromise($scope.dir, {
-              isDir: true
-            }))
-              .catch(errs.handler);
-            $scope.$broadcast('close-popovers');
-          },
-          renameFolder: function () {
-            $scope.editFolderName = true;
-            inputElement.focus();
-            inputElement.select();
-            $scope.$broadcast('close-popovers');
-          },
-          deleteFolder: function () {
-            loadingPromises.add($scope.loadingPromisesTarget, promisify($scope.dir, 'destroy')())
-              .catch(errs.handler);
-            $scope.$broadcast('close-popovers');
-          },
-          uploadFiles: function (files) {
-            if (files && files.length) {
-              $scope.$broadcast('close-popovers');
-
-              var uploadURL = configAPIHost + '/' + $scope.fileModel.urlPath + '/' + $scope.fileModel.id() + '/files';
-              var fileUploadPromises = files.map(function (file) {
-                var myFile = {
-                  attrs: {
-                    name: file.name,
-                    isDir: false
-                  },
-                  state: {
-                    uploading: true,
-                    progress: 0
-                  }
-                };
-
-                $scope.dir.contents.models.push(myFile);
-                return uploadFile(file, uploadURL)
-                  .progress(function (evt) {
-                    myFile.state.progress = parseInt(100.0 * evt.loaded / evt.total, 10);
-                  })
-                  .then(function () {
-                    myFile.state.progress = 100;
-                  })
-                  .catch(function (err) {
-                    var fileIndex = $scope.dir.contents.models.indexOf(myFile);
-                    $scope.dir.contents.models.splice(fileIndex, 1);
-                    errs.handler(err);
-                  })
-                  .then(function () {
-                    return myFile;
-                  });
-
-              });
-
-              $q.all(fileUploadPromises).then(function (uploads) {
-                uploads.forEach(function (myFile) {
-                  var fileIndex = $scope.dir.contents.models.indexOf(myFile);
-                  if (fileIndex !== -1) {
-                    $scope.dir.contents.models.splice(fileIndex, 1);
-                  }
-                });
-                $scope.actions.fetchDirFiles();
-              });
-            }
-          }
         }
       };
 
