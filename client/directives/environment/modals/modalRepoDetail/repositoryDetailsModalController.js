8ab09dd56d22c11654159b060e5d0917e5367a00
Nathan219 <Nathan219@gmail.com>
2015-08-12 11:58:04 -0700

    Pulling the controller out into its own file

---
+++ client/directives/environment/modals/modalRepoDetail/repositoryDetailsModalController.js
@@ -0,0 +1,31 @@
+'use strict';
+
+require('app')
+  .controller('RepositoryDetailsModalController', RepositoryDetailsModalController);
+/**
+ * controller RepositoryDetailsModalController
+ * @ngInject
+ */
+function RepositoryDetailsModalController(
+  fetchCommitData,
+  loading,
+  updateInstanceWithNewAcvData
+) {
+  var RDMC = this;
+  this.data = {
+    repo: this.appCodeVersion.githubRepo,
+    acv: this.appCodeVersion,
+    branch: fetchCommitData.activeBranch(this.appCodeVersion),
+    commit: fetchCommitData.activeCommit(this.appCodeVersion),
+    instance: this.instance
+  };
+  this.updateInstance = function () {
+    loading('main', true);
+    RDMC.defaultActions.close(function () {
+      updateInstanceWithNewAcvData(RDMC.instance, RDMC.appCodeVersion, RDMC.data)
+        .finally(function () {
+          loading('main', false);
+        });
+    });
+  };
+}
--- client/directives/environment/modals/modalRepoDetail/repositoryDetailsModalDirective.js
+++ client/directives/environment/modals/modalRepoDetail/repositoryDetailsModalDirective.js
@@ -7,32 +7,11 @@ require('app')
  * @ngInject
  */
 function repositoryDetailsModal(
-  fetchCommitData,
-  loading,
-  updateInstanceWithNewAcvData
 ) {
   return {
     restrict: 'A',
     templateUrl: 'repositoryDetailsModalView',
-    controller: function () {
-      var RDMC = this;
-      this.data = {
-        repo: this.appCodeVersion.githubRepo,
-        acv: this.appCodeVersion,
-        branch: fetchCommitData.activeBranch(this.appCodeVersion),
-        commit: fetchCommitData.activeCommit(this.appCodeVersion),
-        instance: this.instance
-      };
-      this.updateInstance = function () {
-        loading('main', true);
-        RDMC.defaultActions.close(function () {
-          updateInstanceWithNewAcvData(RDMC.instance, RDMC.appCodeVersion, RDMC.data)
-            .finally(function () {
-              loading('main', false);
-            });
-        });
-      };
-    },
+    controller: 'RepositoryDetailsModalController',
     controllerAs: 'RDMC',
     bindToController: true,
     scope: {
