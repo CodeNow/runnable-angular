f771d901dcffff1f8e4412cc65aa9e9c760a2646
Myztiq <kahn@runnable.com>
2015-08-14 15:03:10 -0700

    Added pooling so we can run up to X tests at a time. Right now it's 6.

--- test/ghost-inspector.js
+++ test/ghost-inspector.js
@@ -1,3 +1,18 @@
+var Kinvey = require('kinvey');
+var kinveyConfig = {
+  appKey    : 'kid_-1IVLi7tNg',
+  appSecret : '90556f04389143758834ad75a800687f',
+  masterSecret: 'cec3e35ff5b6457c9ed0b2e58f059ff5'
+};
+var initialized = Kinvey.init({
+  appKey: kinveyConfig.appKey,
+  appSecret: kinveyConfig.appSecret
+});
+
+var kinveyUser = initialized.then(function () {
+ return Kinvey.User.login(kinveyConfig.appKey, kinveyConfig.masterSecret);
+});
+
 require('array.prototype.find');
 var config = require('./ghost.conf.js');
 var Promise = require("bluebird");
@@ -22,6 +37,15 @@ function getSuiteId(suiteName){
     });
 }
 
+
+function getTestUser(){
+  return kinveyUser
+    .then(function () {
+      return Kinvey.execute('checkoutUser');
+    });
+}
+
+
 function handleTestResults (testResults) {
   var testList = testResults[0];
   var testPassing = testResults[1];
@@ -32,51 +56,78 @@ function handleTestResults (testResults) {
   console.log(testList.length + ' Test(s) Passed!');
 }
 
-var testPromise = Promise.resolve();
-console.log('Setting up tests!');
-config.suites.forEach(function (suiteName) {
-  testPromise = testPromise
+
+var userPromise = getTestUser();
+
+userPromise.then(function (user) {
+  console.log('Running tests with user:  ' + user.username);
+
+  var testOptions = {
+    githubUser: user.username
+  };
+
+  var testPromise = Promise.resolve();
+  console.log('Setting up tests!');
+  config.suites.forEach(function (suiteName) {
+    testPromise = testPromise
+      .then(function () {
+        return getSuiteId(suiteName);
+      })
+      .then(function (suiteId) {
+        console.log('Running suite: ' +  suiteName);
+        return GhostInspector.executeSuiteAsync(suiteId, testOptions);
+      })
+      .then(handleTestResults);
+  });
+
+
+  function teardownTests(){
+    console.log('Start Teardown');
+    return getSuiteId(config.teardown)
+      .then(function (suiteId) {
+        return GhostInspector.executeSuiteAsync(suiteId, testOptions);
+      })
+      .then(handleTestResults)
+      .catch(function (err) {
+        console.error('Error tearing down tests!!!!');
+        console.error(err);
+        throw(err);
+      })
+  }
+
+  testPromise
     .then(function () {
-      return getSuiteId(suiteName);
+      return teardownTests()
+        .then(function () {
+          console.log('All Tests Finished!');
+          stopTests(user);
+        })
+        .then(function () {
+          process.exit(0);
+        })
+        .catch(function () {
+          process.exit(1);
+        })
     })
-    .then(function (suiteId) {
-      console.log('Running suite: ' +  suiteName);
-      return GhostInspector.executeSuiteAsync(suiteId);
-    })
-    .then(handleTestResults);
+    .catch(function (e) {
+      console.error(e);
+      teardownTests()
+        .finally(function () {
+          stopTests(user)
+            .then(function () {
+              process.exit(1);
+            });
+        })
+    });
 });
 
 
-function teardownTests(){
-  console.log('Start Teardown');
-  return getSuiteId(config.teardown)
-    .then(function (suiteId) {
-      return GhostInspector.executeSuiteAsync(suiteId);
-    })
-    .then(handleTestResults)
-    .catch(function (err) {
-      console.error('Error tearing down tests!!!!');
-      console.error(err);
-      throw(err);
-    })
+function stopTests(user){
+  console.log('Unlocking user: ' + user.username);
+  return Kinvey.DataStore.save('github-accounts', user);
 }
 
-testPromise
-  .then(function () {
-    return teardownTests()
-      .then(function () {
-        console.log('All Tests Finished!');
-        process.exit(0);
-      })
-      .catch(function () {
-        process.exit(1);
-      })
-  })
-  .catch(function (e) {
-    console.error(e);
-    teardownTests()
-      .finally(function () {
-        process.exit(1);
-      })
-  });
 
+//test4lyfe
+//RunnableTest1
+//githubuser@runnable.com
