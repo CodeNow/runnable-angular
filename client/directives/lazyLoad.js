c0136baa7580db92628f2f44302f2e8cc74f8db3
Myztiq <kahn@runnable.com>
2015-09-18 16:33:30 -0700

    Worked around an issue w/ Lazy Loader and replacing directives and commented in lazy loader the scenario.

--- client/directives/lazyLoad.js
+++ client/directives/lazyLoad.js
@@ -16,6 +16,7 @@ function lazyLoad(
       }
       element.html('<div class="spinner-wrapper spinner-backdrop spinner-md in"><svg height="30" viewbox="0 0 32 32" width="30" class="spinner spinner-md"><circle cx="16" cy="16" fill="none" r="15" stroke-dasharray="60, 12" stroke-linecap="round" stroke-width="2" class="path"></circle></svg></div>');
       $ocLazyLoad.load(key).then(function() {
+        // WARNING: This won't work well if you have stacking directives and replace:true.
         $compile(angular.element(element))($scope);
       });
     }
