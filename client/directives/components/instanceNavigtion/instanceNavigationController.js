7a7551f0322be6e826129f3bc9061d646cd76ce4
Myztiq <kahn@runnable.com>
2015-09-30 18:58:42 -0700

    Initial steps at adding instanceNavigation directive.

---
+++ client/directives/components/instanceNavigtion/instanceNavigationController.js
@@ -0,0 +1,12 @@
+'use strict';
+
+require('app').controller('InstanceNavigationController', InstanceNavigationController);
+
+function InstanceNavigationController(
+) {
+  var INC = this;
+
+  console.log('INC!');
+}
+
+
