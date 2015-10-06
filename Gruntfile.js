9d930b1158a40dd508017d862dc2d18d3f930575
Myztiq <kahn@runnable.com>
2015-10-06 16:01:29 -0700

    Updated thresholds. Sneaky Sneaky.

--- Gruntfile.js
+++ Gruntfile.js
@@ -266,14 +266,10 @@ module.exports = function(grunt) {
       default: {
         options: {
           thresholds: {
-            //statements: 62.03,
-            //branches: 42.00,
-            //functions: 52.03,
-            //lines: 62.3
-            statements: 25,
-            branches: 25,
-            functions: 25,
-            lines: 25
+            statements   : 71,
+            branches     : 53,
+            functions    : 66,
+            lines        : 71
           },
           dir: 'coverage',
           root: 'test'
