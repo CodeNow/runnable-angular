e6345ddd666f44a3e7665b9e531dd0fd1fba1abd
Myztiq <kahn@runnable.com>
2015-10-06 13:55:16 -0700

    Hooked up support for creating isolations!

--- client/directives/components/isolationConfiguration/isolationConfigurationModalController.js
+++ client/directives/components/isolationConfiguration/isolationConfigurationModalController.js
@@ -5,16 +5,44 @@ require('app').controller('IsolationConfigurationModalController', IsolationConf
 function IsolationConfigurationModalController(
   instance,
   close,
-  fetchInstancesByPod
+  fetchInstancesByPod,
+  loading,
+  createIsolation,
+  errs
 ) {
   var ICMC = this;
   ICMC.instance = instance;
   ICMC.close = close;
+  loading.reset('createIsolation');
 
   ICMC.createIsolation = function () {
-    console.log('Create isolation!');
-    close();
+    var isolatedChildren = [];
+    Object.keys(ICMC.instanceBranchMapping).forEach((function (key) {
+      isolatedChildren.push(ICMC.instanceBranchMapping[key]);
+    }));
+
+    isolatedChildren = isolatedChildren.map(function (instance) {
+      return {
+        branch: instance.getBranchName(),
+        repo: instance.getRepoName(),
+        org: instance.attrs.owner.username
+      };
+    });
+
+    ICMC.nonRepoInstances.forEach(function (instance) {
+      isolatedChildren.push(instance.id());
+    });
+
+    loading('createIsolation', true);
+    createIsolation( ICMC.instance, isolatedChildren)
+      .catch(errs.handler)
+      .finally(function () {
+        loading('createIsolation', false);
+        close();
+      });
   };
+
+  ICMC.instanceBranchMapping = {};
 
 
   fetchInstancesByPod()
@@ -23,9 +51,15 @@ function IsolationConfigurationModalController(
         return instance !== ICMC.instance;
       });
 
-      ICMC.repoInstances = instances.filter(function (instance) {
-        return instance.getRepoName();
+      ICMC.repoInstances = instances
+        .filter(function (instance) {
+          return instance.getRepoName();
+        });
+
+      ICMC.repoInstances.forEach(function (instance) {
+        ICMC.instanceBranchMapping[instance.attrs.contextVersion.id] = instance;
       });
+
       ICMC.nonRepoInstances = instances.filter(function (instance) {
         return !instance.getRepoName();
       });
