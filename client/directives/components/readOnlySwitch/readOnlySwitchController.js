624de24058de98d0a6b0e1555cebfabfd907bba1
thejsj <jorge.silva@thejsj.com>
2015-10-16 18:20:36 -0700

    Adding ability to switch between simple mode and advanced mode

--- client/directives/components/readOnlySwitch/readOnlySwitchController.js
+++ client/directives/components/readOnlySwitch/readOnlySwitchController.js
@@ -34,59 +34,64 @@ function ReadOnlySwitchController(
 
   // Getter/setter
   this.readOnly = function (newAdvancedMode) {
-    if (newAdvancedMode !== undefined) {
-      if (newAdvancedMode === true) {
-        ROSC.state.advanced = newAdvancedMode;
-        return $q.when(true)
-          .then(function () {
-            // If there is not instance, we need to copy this context version and
-            // keep a reference to the original CV
-            if (!ROSC.state.instance) {
-              // Save copy of simple mode CV to state
-              ROSC.state.allContextVersions = {
-                'simple': ROSC.state.contextVersion
-              };
-              return promisify(ROSC.state.contextVersion, 'deepCopy')()
-                .then(function (contextVersion) {
-                  ROSC.state.contextVersion = contextVersion;
-                  // Set advanced CV
-                  ROSC.state.allContextVersions.advanced = ROSC.state.contextVersion;
-                  ROSC.state.acv = contextVersion.getMainAppCodeVersion();
-                  ROSC.state.repo = keypather.get(contextVersion, 'getMainAppCodeVersion().githubRepo');
-                  return promisify(contextVersion, 'fetch')();
-                });
-            }
-            return ROSC.state.contextVersion;
-          })
-          .then(function () {
-            if (ROSC.state.instance && !ROSC.state.instance.attrs.lastBuiltSimpleContextVersion) {
-              // Grab off of the instance, since it's the original one, and hasn't been modified
-              ROSC.state.instance.attrs.lastBuiltSimpleContextVersion = {
-                id: ROSC.state.instance.contextVersion.attrs.id,
-                created: ROSC.state.instance.contextVersion.attrs.created
-              };
-            }
-            if (ROSC.state.promises) {
-              return ROSC.state.promises.contextVersion
-                .then(function (contextVersion) {
-                  return loadingPromises.add(ROSC.loadingPromisesTarget,
-                    promisify(contextVersion, 'update')({
-                      advanced: newAdvancedMode
-                    })
-                    .then(function () {
-                      return promisify(contextVersion, 'fetch')()
-                      .then(function (_contextVersion) {
-                        return _contextVersion;
-                      });
-                    }));
-                })
-                .catch(function (err) {
-                  errs.handler(err);
-                  ROSC.state.advanced = !newAdvancedMode;
-                });
-            }
-            return true;
-          });
+    if (newAdvancedMode === true) {
+      ROSC.state.advanced = newAdvancedMode;
+      return $q.when(true)
+        .then(function () {
+          // If there is not instance, we need to copy this context version and
+          // keep a reference to the original CV
+          if (!ROSC.state.instance) {
+            // Save copy of simple mode CV to state
+            ROSC.state.allContextVersions = {
+              'simple': ROSC.state.contextVersion
+            };
+            return promisify(ROSC.state.contextVersion, 'deepCopy')()
+              .then(function (contextVersion) {
+                ROSC.state.contextVersion = contextVersion;
+                // Set advanced CV
+                ROSC.state.allContextVersions.advanced = contextVersion;
+                ROSC.state.acv = contextVersion.getMainAppCodeVersion();
+                ROSC.state.repo = keypather.get(contextVersion, 'getMainAppCodeVersion().githubRepo');
+                return promisify(contextVersion, 'fetch')();
+              });
+          }
+          return ROSC.state.contextVersion;
+        })
+        .then(function () {
+          if (ROSC.state.instance && !ROSC.state.instance.attrs.lastBuiltSimpleContextVersion) {
+            // Grab off of the instance, since it's the original one, and hasn't been modified
+            ROSC.state.instance.attrs.lastBuiltSimpleContextVersion = {
+              id: ROSC.state.instance.contextVersion.attrs.id,
+              created: ROSC.state.instance.contextVersion.attrs.created
+            };
+          }
+          if (ROSC.state.promises) {
+            return ROSC.state.promises.contextVersion
+              .then(function (contextVersion) {
+                return loadingPromises.add(ROSC.loadingPromisesTarget,
+                  promisify(contextVersion, 'update')({
+                    advanced: newAdvancedMode
+                  })
+                  .then(function () {
+                    return promisify(contextVersion, 'fetch')();
+                  }));
+              })
+              .catch(function (err) {
+                errs.handler(err);
+                ROSC.state.advanced = !newAdvancedMode;
+              });
+          }
+          return true;
+        });
+    }
+    if (newAdvancedMode === false) {
+      if (!ROSC.state.instance) {
+        var contextVersion = ROSC.state.allContextVersions.simple;
+        ROSC.state.contextVersion = contextVersion;
+        ROSC.state.acv = contextVersion.getMainAppCodeVersion();
+        ROSC.state.repo = keypather.get(contextVersion, 'getMainAppCodeVersion().githubRepo');
+        ROSC.state.advanced = false;
+        return false;
       }
       // If switching from advanced to basic
       return ROSC.state.promises.contextVersion
@@ -104,13 +109,15 @@ function ReadOnlySwitchController(
                 if (confirmed) {
                   performRollback(contextVersion);
                 } else {
-                  ROSC.state.advanced = true;
+                  ROSC.state.advanced = false;
+                  return false;
                 }
               });
             });
         })
-        .catch(errs.handler);
+      .catch(errs.handler);
     }
+    return ROSC.state.advanced;
   };
 }
 
