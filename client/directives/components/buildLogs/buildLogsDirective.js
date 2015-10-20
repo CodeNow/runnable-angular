f38cd838d0badd6e2c671e3c8502b574a3cb2342
Myztiq <kahn@runnable.com>
2015-10-19 17:27:27 -0700

    Fixed auto-scroll helper.

--- client/directives/components/buildLogs/buildLogsDirective.js
+++ client/directives/components/buildLogs/buildLogsDirective.js
@@ -73,7 +73,7 @@ function buildLogs(
       $scope.$watch(function() {
         return {
           buildLogs: $scope.BLC.buildLogs.length,
-          childLogs: keypather.get($scope.BLC.buildLogs[$scope.BLC.buildLogs.length-1], 'content.length')
+          childLogs: keypather.get($scope.BLC.buildLogs[$scope.BLC.buildLogs.length-1], 'unprocessedContent.length')
         };
       }, function () {
         scrollHelper();
