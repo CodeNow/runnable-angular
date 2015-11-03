55c4b77861b1158830526a75a831cbb8ea340e04
Nathan219 <Nathan219@gmail.com>
2015-11-03 13:57:56 -0800

    Starting to change how the popoverTrigger works

--- client/directives/popovers/popoverHoverTriggerDirective.js
+++ client/directives/popovers/popoverHoverTriggerDirective.js
@@ -22,6 +22,42 @@ function popOverHoverTrigger(
         return Math.tan((firstPoint.pageY - secondPoint.pageY) / (firstPoint.pageX - secondPoint.pageX));
       }
 
+      function getCenterPoint(start, finish) {
+        return start + (finish - start);
+      }
+
+      /**
+       *  slopeInvert: Standard slope is y/x, so when we use the y coordinate instead of the x,
+       *  we need to flip it
+       * @returns {{boundary: *, slope}}
+       */
+      function getBoundaryValues() {
+        var boundingRect = POC.popoverElement[0].getBoundingClientRect();
+        if (POC.popoverElement.hasClass('bottom')) {
+          return {
+            boundary: getCenterPoint(boundingRect.top, boundingRect.bottom),
+            slopeInvert: true
+          };
+        }
+      }
+      function getValues() {
+        if (!POC.popoverElement) {
+          return;
+        }
+        var boundingRect = POC.popoverElement[0].getBoundingClientRect();
+        if (POC.popoverElement.hasClass('bottom')) {
+          return [{pageX: boundingRect.left, pageY: boundingRect.top}, {
+            pageX: boundingRect.right,
+            pageY: boundingRect.top
+          }];
+        } else if (POC.popoverElement.hasClass('top')) {
+          return [{pageX: boundingRect.left, pageY: boundingRect.bottom}, {
+            pageX: boundingRect.right,
+            pageY: boundingRect.bottom
+          }];
+        }
+      }
+
       function getPointsFromPopover() {
         if (!POC.popoverElement) {
           return;
@@ -87,28 +123,25 @@ function popOverHoverTrigger(
 
       function onActivateHover(e) {
         initialMouseEvent = e;
-        //var unWatch = $scope.$watch(function () {
-        //  return POC.popoverElement;
-        //}, function (popoverElement) {
-          if (POC.popoverElement) {
-            //unWatch();
-            element.on('mouseleave', onElementMouseLeave);
-            POC.popoverElement.on('mouseleave', cleanUp);
-          }
-        //});
+        element.on('mouseleave', onElementMouseLeave);
+        POC.popoverElement.on('mouseleave', cleanUp);
       }
 
       function onMouseOver(e) {
+        if (POC.isPopoverActive()) {
+          // If the element is already there, don't do anything
+          return;
+        }
         popoverAngle1 = null;
         popoverAngle2 = null;
         POC.openPopover(e);
         onActivateHover(e);
       }
 
-      element.on('mouseover', onMouseOver);
+      element.on('mouseenter', onMouseOver);
       $scope.$on('$destroy', function () {
         cleanUp();
-        element.off('mouseover', onMouseOver);
+        element.off('mouseenter', onMouseOver);
       });
     }
   };
