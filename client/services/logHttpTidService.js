42d7ab0dd216eef357a90bc2ce8f02458ed61d5a
Myztiq <kahn@runnable.com>
2015-11-02 16:45:27 -0800

    Updated to use keypather and $window.

--- client/services/logHttpTidService.js
+++ client/services/logHttpTidService.js
@@ -3,10 +3,20 @@
 require('app')
   .factory('logHttpTid', logHttpTid);
 
-function logHttpTid(configAPIHost) {
+function logHttpTid(
+  configAPIHost,
+  $window,
+  keypather
+) {
   function logResponse(response) {
-    if (window.trackJs && response.config.url.indexOf(configAPIHost) === 0) {
-      window.trackJs.console.info(response.config.method + ' ' + response.config.url + ' - ' + response.status + ' tid: ' + response.headers()['x-runnable-tid']);
+    if ($window.trackJs && (keypather(response, 'config.url') || '').indexOf(configAPIHost) === 0) {
+      $window.trackJs.console.info(keypather.get(response, 'config.method') +
+        ' ' +
+        keypather.get(response, 'config.url') +
+        ' - ' +
+        response.status +
+        ' tid: ' +
+        (keypather.get(response, 'headers()') || {})['x-runnable-tid']);
     }
     return response;
   }
