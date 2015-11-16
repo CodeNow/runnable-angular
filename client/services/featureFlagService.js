98acf1d6049e1952c203443d628306edc07bde0f
runnabro <tli415@gmail.com>
2015-11-16 14:44:12 -0800

    add new rebuild flow feature flag

--- client/services/featureFlagService.js
+++ client/services/featureFlagService.js
@@ -25,6 +25,7 @@ function featureFlags(
     navListFilter: false,
     newVerificationFlow: true,
     newVerificationFlowStackSelector: true,
+    rebuildFlow: false, // updated rebuild flow
     renameContainer: false,
     saveToolbar: false,
     teamManagement: false, // changes text from org to team in account menu
