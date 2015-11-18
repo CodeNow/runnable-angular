d6d4dd2ca8f1b1b5fa88d8983c4789070a91f879
Nathan219 <Nathan219@gmail.com>
2015-11-18 15:16:49 -0800

    Creating inviteAdminModalController Adding method for creating the inviteAdmin modal where necessary

---
+++ client/directives/modals/inviteAdminModal/inviteAdminModalController.js
@@ -0,0 +1,46 @@
+'use strict';
+
+require('app')
+  .controller('InviteAdminModalController', InviteAdminModalController);
+
+function InviteAdminModalController(
+  $scope,
+  $state,
+  $timeout,
+  errs,
+  fetchGitHubAdminsByRepo,
+  close,
+  instance,
+  isFromAutoDeploy
+) {
+  var IAMC = this;
+  IAMC.close = close;
+  IAMC.isFromAutoDeploy = isFromAutoDeploy || false;
+  IAMC.DEFAULT_MESSAGE = 'Join my Sandbox on Runnable, where we can run the code in CodeNow\'s repositories on demand.\n\nI need your admin permissions to enable some features. Thanks!';
+
+  IAMC.repoName = instance.getRepoName();
+  var username = $state.params.userName;
+  fetchGitHubAdminsByRepo(username, IAMC.repoName)
+    .then(function (admins) {
+      IAMC.admins = admins;
+    })
+    .catch(function (err) {
+      errs.handler(err);
+      close();
+    });
+
+  IAMC.sendEmail = function (user) {
+    IAMC.sending = true;
+    $timeout(angular.noop, 2000)
+      .then(function () {
+        IAMC.sending = false;
+        IAMC.activeItem = null;
+        user.emailSent = true;
+      });
+  };
+
+  IAMC.selectUser = function (user) {
+    user.emailMessage = IAMC.DEFAULT_MESSAGE;
+    IAMC.activeItem = user.login;
+  };
+}
