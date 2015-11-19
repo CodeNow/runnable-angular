373008ad2c4eec0b7e859cacf222b0fe66fa3e8b
runnabro <tli415@gmail.com>
2015-11-19 13:55:30 -0800

    fix buttons for feature flags

--- client/directives/environment/modals/modalSetupServer/setupConfirmButtonDirective.js
+++ client/directives/environment/modals/modalSetupServer/setupConfirmButtonDirective.js
@@ -1,7 +1,7 @@
 'use strict';
 
 require('app')
-  .directive('setupConfirmButton', function setupConfirmButton() {
+  .directive('setupConfirmButton', function setupConfirmButton($rootScope) {
     return {
       restrict: 'A',
       link: function ($scope, elem, attrs) {
@@ -9,17 +9,30 @@ require('app')
         $scope.$on('updateStep', updateStepHandler);
         updateStepHandler(null, 1);
 
+        var step3Copy;
+        var step4Copy;
+        var step4ClosesModal;
+        if ($rootScope.featureFlags.rebuildFlow) {
+          step3Copy = 'Save & Build';
+          step4Copy = 'Save & Build'
+          step4ClosesModal = false;
+        } else {
+          step3Copy = 'Start Build';
+          step4Copy = 'Create Container'
+          step4ClosesModal = true;
+        }
+
         function updateStepHandler (event, newStep) {
           $scope.step = newStep;
           if ($scope.step < 3) {
             $scope.buttonText = 'Next';
             $scope.buttonClosesModal = false;
           } else if ($scope.step === 3) {
-            $scope.buttonText = 'Start Build';
+            $scope.buttonText = step3Copy;
             $scope.buttonClosesModal = false;
           } else if ($scope.step >= 4) {
-            $scope.buttonText = 'Create Container';
-            $scope.buttonClosesModal = true;
+            $scope.buttonText = step4Copy;
+            $scope.buttonClosesModal = step4ClosesModal;
           }
         }
 
