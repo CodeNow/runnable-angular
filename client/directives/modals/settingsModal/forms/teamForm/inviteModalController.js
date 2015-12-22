1084be2f36271f4105af10e6c279c1c2b18d467a
thejsj <jorge.silva@thejsj.com>
2015-12-22 11:14:53 -0800

    Change inviteModalController to use service

--- client/directives/modals/settingsModal/forms/teamForm/inviteModalController.js
+++ client/directives/modals/settingsModal/forms/teamForm/inviteModalController.js
@@ -11,6 +11,7 @@ function InviteModalController(
   $q,
   fetchUser,
   fetchGithubOrgId,
+  inviteGithubUserToRunnable,
   promisify,
   errs,
   teamName,
@@ -30,36 +31,22 @@ function InviteModalController(
     IMC.sendingInviteUserId = user.id;
     IMC.sendingInvitation = true;
     IMC.setActiveUserId(null);
-    return $q.all({
-      user: fetchUser(),
-      githubOrgId: fetchGithubOrgId(teamName)
-    })
-    .then(function (response) {
-      IMC.invitesSent += 1;
-      return promisify(response.user, 'createTeammateInvitation')({
-        organization: {
-          github: response.githubOrgId
-        },
-        recipient: {
-          email: user.inviteEmail,
-          github: user.id
-        }
+    return inviteGithubUserToRunnable(user.id, user.inviteEmail, teamName)
+      .then(function (invitationModel) {
+        IMC.invitesSent += 1;
+        user.inviteSent = true;
+        // Append invitation to user
+        user.userInvitation = invitationModel;
+        $rootScope.$broadcast('newInvitedAdded', user);
+        IMC.sendingInvitation = false;
+        IMC.sendingInviteUserId = null;
+        return invitationModel;
+      })
+      .catch(function (err) {
+        errs.handler(err);
+        IMC.sendingInvitation = false;
+        IMC.sendingInviteUserId = null;
       });
-    })
-    .then(function (invitationModel) {
-      user.inviteSent = true;
-      // Append invitation to user
-      user.userInvitation = invitationModel;
-      $rootScope.$broadcast('newInvitedAdded', user);
-      IMC.sendingInvitation = false;
-      IMC.sendingInviteUserId = null;
-      return invitationModel;
-    })
-    .catch(function (err) {
-      errs.handler(err);
-      IMC.sendingInvitation = false;
-      IMC.sendingInviteUserId = null;
-    });
   };
 
   IMC.setActiveUserId = function (userId) {
