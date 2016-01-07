1cfe7e2c6dbac980c427a15edaeb49dc8f3ff835
Myztiq <kahn@runnable.com>
2016-01-07 11:15:01 -0800

    Made form validation it's own method because ng-pattern is busted.

--- client/directives/environment/modals/forms/whitelistForm/whitelistFormDirective.js
+++ client/directives/environment/modals/forms/whitelistForm/whitelistFormDirective.js
@@ -17,6 +17,17 @@ function ipWhitelistDirective() {
       $scope.state = {
         isFocused: false
       };
+
+      var verificationPattern = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+      $scope.isFormValid = function () {
+        if (!verificationPattern.test($scope.WFC.fromAddress)) {
+          return false;
+        }
+        if ($scope.WFC.isRange && !verificationPattern.test($scope.WFC.toAddress)) {
+          return false;
+        }
+        return true;
+      };
     }
   };
 }
