dbb0c89276d6da5d011a67a19340c882749b647c
Nathan219 <Nathan219@gmail.com>
2016-01-26 16:54:48 -0800

    make the build logs handler able to take an array

--- client/services/streamingLogService.js
+++ client/services/streamingLogService.js
@@ -26,103 +26,109 @@ function streamingLog(
     var streaming = false;
     var rawLogs = [];
     var processedRawLogs = null;
-    function handleStreamData(data) {
-      if (['docker', 'log'].includes(data.type)) {
-        var stepRegex = /^Step [0-9]+ : /;
-        if (stepRegex.test(data.content)) {
-          if (currentCommand) {
-            currentCommand.expanded = false;
-          }
-          currentCommand = {
-            unprocessedContent: [],
-            processedContent: [],
-            command: $sce.trustAsHtml(convert.toHtml(data.content.replace(stepRegex, ''))),
-            rawCommand: data.content.replace(stepRegex, ''),
-            imageId: data.imageId,
-            expanded: false,
-            time: new Date(data.timestamp || new Date()),
-            converter: new Convert({
-              stream: true,
-              escapeXML: true
-            }),
-            trustedContent: $sce.trustAsHtml(''),
-            hasContent: false,
-            getProcessedHtml: function () {
-              var self = this;
-              if (!self.unprocessedContent.length) {
+    function handleStreamData(dataArray) {
+      if (!Array.isArray(dataArray)) {
+        dataArray = [dataArray];
+      }
+      dataArray.forEach(function (data) {
+        if (['docker', 'log'].includes(data.type)) {
+          var stepRegex = /^Step [0-9]+ : /;
+          if (stepRegex.test(data.content)) {
+            if (currentCommand) {
+              currentCommand.expanded = false;
+            }
+            currentCommand = {
+              unprocessedContent: [],
+              processedContent: [],
+              command: $sce.trustAsHtml(convert.toHtml(data.content.replace(stepRegex, ''))),
+              rawCommand: data.content.replace(stepRegex, ''),
+              imageId: data.imageId,
+              expanded: false,
+              time: new Date(data.timestamp || new Date()),
+              converter: new Convert({
+                stream: true,
+                escapeXML: true
+              }),
+              trustedContent: $sce.trustAsHtml(''),
+              hasContent: false,
+              getProcessedHtml: function () {
+                var self = this;
+                if (!self.unprocessedContent.length) {
+                  return self.trustedContent;
+                }
+                var joinedContent = self.unprocessedContent.join('');
+                var lastLineIsFinished = joinedContent.slice(-2) === '\n';
+                var lines = joinedContent.split('\n');
+                self.lastProcessedLine = null;
+                lines.forEach(function (line, index) {
+                  if (line.length > 1000) {
+                    line = line.substr(0, 1000) + ' - Line Truncated because its too long.';
+                  }
+                  // If we can still expect new logs, and the last line isn't finished and it's the last line let's temporarily process it.
+                  if (streaming && currentCommand === self && !lastLineIsFinished && index === (lines.length - 1)) {
+                    self.lastProcessedLine = self.converter.toHtml(line + '\n');
+                  } else {
+                    self.processedContent.push(self.converter.toHtml(line + '\n'));
+                  }
+                });
+                self.unprocessedContent = [];
+                if (self.lastProcessedLine) {
+                  self.unprocessedContent.push(lines[lines.length - 1]);
+                  self.trustedContent = $sce.trustAsHtml(self.processedContent.join('') + self.lastProcessedLine);
+                  self.lineCount = self.processedContent.length + 1;
+                } else if (lines.length) {
+                  self.trustedContent = $sce.trustAsHtml(self.processedContent.join(''));
+                  self.lineCount = self.processedContent.length;
+                }
                 return self.trustedContent;
               }
-              var joinedContent = self.unprocessedContent.join('');
-              var lastLineIsFinished = joinedContent.slice(-2) === '\n';
-              var lines = joinedContent.split('\n');
-              self.lastProcessedLine = null;
-              lines.forEach(function (line, index) {
-                if (line.length > 1000) {
-                  line = line.substr(0, 1000) + ' - Line Truncated because its too long.';
-                }
-                // If we can still expect new logs, and the last line isn't finished and it's the last line let's temporarily process it.
-                if (streaming && currentCommand === self && !lastLineIsFinished && index === (lines.length - 1)) {
-                  self.lastProcessedLine = self.converter.toHtml(line + '\n');
-                } else {
-                  self.processedContent.push(self.converter.toHtml(line + '\n'));
-                }
-              });
-              self.unprocessedContent = [];
-              if (self.lastProcessedLine) {
-                self.unprocessedContent.push(lines[lines.length - 1]);
-                self.trustedContent = $sce.trustAsHtml(self.processedContent.join('') + self.lastProcessedLine);
-                self.lineCount = self.processedContent.length + 1;
-              } else if (lines.length) {
-                self.trustedContent = $sce.trustAsHtml(self.processedContent.join(''));
-                self.lineCount = self.processedContent.length;
+            };
+            var previous = streamLogs[streamLogs.length - 1];
+            if (previous) {
+              previous.expanded = false;
+            }
+            streamLogs.push(currentCommand);
+          } else if (currentCommand) {
+            var ignoreRegex = [
+              /^Runnable: Build completed successfully!/,
+              /^\s---> Running in [a-z0-9]{12}/,
+              /^Successfully built [a-z0-9]{12}/
+            ];
+
+            var ignore = ignoreRegex.some(function (regex) {
+              return regex.test(data.content);
+            });
+
+            if (!ignore) {
+              if (/^\s---> Using cache/.test(data.content)) {
+                currentCommand.cached = true;
+              } else if (/^\s---> [a-z0-9]{12}/.test(data.content)) {
+                currentCommand.imageId = /^\s---> ([a-z0-9]{12})/.exec(data.content)[1];
+              } else {
+                currentCommand.hasContent = true;
+                currentCommand.unprocessedContent.push(data.content);
               }
-              return self.trustedContent;
-            }
-          };
-          var previous = streamLogs[streamLogs.length - 1];
-          if (previous) {
-            previous.expanded = false;
-          }
-          streamLogs.push(currentCommand);
-        } else if (currentCommand) {
-          var ignoreRegex = [
-            /^Runnable: Build completed successfully!/,
-            /^\s---> Running in [a-z0-9]{12}/,
-            /^Successfully built [a-z0-9]{12}/
-          ];
-
-          var ignore = ignoreRegex.some(function (regex) {
-            return regex.test(data.content);
-          });
-
-          if (!ignore) {
-            if (/^\s---> Using cache/.test(data.content)) {
-              currentCommand.cached = true;
-            } else if (/^\s---> [a-z0-9]{12}/.test(data.content)) {
-              currentCommand.imageId = /^\s---> ([a-z0-9]{12})/.exec(data.content)[1];
-            } else {
-              currentCommand.hasContent = true;
-              currentCommand.unprocessedContent.push(data.content);
             }
           }
         }
-      }
-      if (data.content) {
-        rawLogs.push(data);
-        processedRawLogs = null;
-      }
-
-      if (data.timestamp) {
-        streamTimes.latest = new Date(data.timestamp);
-        clearInterval(timingInterval);
-        streamTimes.currentMachineTime = streamTimes.latest;
-        timingInterval = setInterval(function () {
-          streamTimes.currentMachineTime = new Date(streamTimes.currentMachineTime.getTime() + 1000);
-        }, 1000);
-        if (!streamTimes.start) {
-          streamTimes.start = new Date(data.timestamp);
+        if (data.content) {
+          rawLogs.push(data);
+          processedRawLogs = null;
         }
-      }
+
+        if (data.timestamp) {
+          streamTimes.latest = new Date(data.timestamp);
+          clearInterval(timingInterval);
+          streamTimes.currentMachineTime = streamTimes.latest;
+          timingInterval = setInterval(function () {
+            streamTimes.currentMachineTime = new Date(streamTimes.currentMachineTime.getTime() + 1000);
+          }, 1000);
+          if (!streamTimes.start) {
+            streamTimes.start = new Date(data.timestamp);
+          }
+        }
+      });
+
       refreshAngular();
     }
 
