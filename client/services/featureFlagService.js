d38b1d944a35e2c38f098d1a6b8a0bd0dce0e303
Myztiq <kahn@runnable.com>
2016-02-26 09:51:50 -0800

    Added isolationRepos feature flag.

--- client/services/featureFlagService.js
+++ client/services/featureFlagService.js
@@ -22,6 +22,7 @@ function featureFlags(
     internalDebugging: false,
     inviteFlows: false,
     isolationUI: true, // for isolation UI
+    isolationRepos: false, // for isolation UI
     multilineFnR: false,
     nameContainer: false,
     navListFilter: false,
