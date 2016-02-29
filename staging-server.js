daa9ab416ad93f47a6fd0dcbe741c6ccec385eeb
Myztiq <kahn@runnable.com>
2016-02-29 10:30:14 -0800

    Updated paths for staging-server.

--- staging-server.js
+++ staging-server.js
@@ -3,7 +3,7 @@
 console.log('To run this you need to make sure to install node-static, it does not belong in the package.json');
 
 var staticServer = require('node-static');
-var path = '/runnable-angular/dist';
+var path = './client/build';
 
 var fileServer = new staticServer.Server(path);
 
@@ -11,8 +11,9 @@ require('http').createServer(function (request, response) {
   request.addListener('end', function () {
     fileServer.serve(request, response, function (e, res) {
       if (e && (e.status === 404)) { // If the file wasn't found
-        fileServer.serveFile(path + '/index.html', 200, {}, request, response);
+        fileServer.serveFile('app.html', 200, {}, request, response);
       }
     });
   }).resume();
 }).listen(80);
+console.log('Server Started.');
