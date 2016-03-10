4e0b70b30b8197a9a9567f6a72b7ff56bc736e8d
Nathan Meyers <nathan@runnable.com>
2016-03-09 16:21:08 -0800

    Tony says no spacing around them

--- client/directives/activePanel/tabs/logs/directiveLogTerm.js
+++ client/directives/activePanel/tabs/logs/directiveLogTerm.js
@@ -44,9 +44,7 @@ function logTerm(
       function disconnected() {
         if (reconnecting) { return; }
         reconnecting = true;
-        terminal.writeln('');
         terminal.writeln('* Lost Connection — Retrying… *');
-        terminal.writeln('');
       }
       bind(primus, 'offline', disconnected);
       bind(primus, 'reconnect', disconnected);
@@ -56,9 +54,7 @@ function logTerm(
         if ($scope.clearTermOnReconnect) {
           terminal.reset();
         }
-        terminal.writeln('');
         terminal.writeln('* Connection Regained — Thanks for your patience! *');
-        terminal.writeln('');
         $timeout(function () {
           initializeStream(true);
         });
