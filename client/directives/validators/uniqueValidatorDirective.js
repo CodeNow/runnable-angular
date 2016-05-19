cafc5f11b898ebabb9a17424c93a8af16dddf0eb
thejsj <jorge.silva@thejsj.com>
2016-05-19 12:46:01 -0700

    Remove uniqueValidator check

--- client/directives/validators/uniqueValidatorDirective.js
+++ client/directives/validators/uniqueValidatorDirective.js
@@ -16,9 +16,6 @@ function uniqueValidator() {
           return true;
         }
         var lowerCaseViewValue = viewValue.toLowerCase();
-        if (!$scope.uniqueValidator) {
-          return true;
-        }
         return $scope.uniqueValidator.every(function (item) {
           return item.toLowerCase() !== lowerCaseViewValue;
         });
