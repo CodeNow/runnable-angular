65f5e7923eadf15c284c2d4f0cc112dc982b10e9
Myztiq <kahn@runnable.com>
2016-08-05 13:58:56 -0700

    Hooked up invoice display

---
+++ client/directives/modals/settingsModal/forms/billingForm/billingHistoryForm/billingHistoryFormDirective.js
@@ -0,0 +1,24 @@
+'use strict';
+
+require('app').directive('billingHistoryForm', billingHistoryForm);
+
+function billingHistoryForm(
+  fetchInvoices,
+  keypather,
+  moment
+) {
+  return {
+    restrict: 'A',
+    templateUrl: 'billingHistoryForm',
+    link: function ($scope, element) {
+      fetchInvoices()
+        .then(function (invoices) {
+          $scope.invoices = invoices;
+        });
+
+      $scope.getBillingDate = function (invoice) {
+        return moment(invoice.period_end * 1000).format('M/D/YYYY');
+      };
+    }
+  };
+}
