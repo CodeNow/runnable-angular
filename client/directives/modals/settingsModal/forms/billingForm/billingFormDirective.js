bb5f0c2a419f7d5399cbae6459e07690284fdca8
Myztiq <kahn@runnable.com>
2016-08-16 14:26:41 -0700

    Fixed tests.

--- client/directives/modals/settingsModal/forms/billingForm/billingFormDirective.js
+++ client/directives/modals/settingsModal/forms/billingForm/billingFormDirective.js
@@ -9,19 +9,14 @@ function billingForm(
     restrict: 'A',
     templateUrl: 'billingForm',
     link: function ($scope, element) {
-      element.on('$destroy', function () {
+      $scope.$on('$destroy', function () {
         $scope.SEMC.showFooter = true;
       });
       $scope.$on('changed-animated-panel', function (event, panelName) {
-        if (panelName !== 'billingForm') {
-          $scope.SEMC.showFooter = false;
-        } else {
-          $scope.SEMC.showFooter = true;
-        }
+        $scope.SEMC.showFooter = panelName === 'billingForm';
+        console.log('new value', $scope.SEMC.showFooter);
       });
-
       $scope.$broadcast('go-to-panel', $scope.SEMC.subTab || 'billingForm', 'immediate');
-
       $scope.activeAccount = $rootScope.dataApp.data.activeAccount;
       $scope.actions = {
         save: function () {
--- test/unit/directives/modals/settingsModal/forms/billingForm/billingFormDirective.unit.js
+++ test/unit/directives/modals/settingsModal/forms/billingForm/billingFormDirective.unit.js
@@ -2,6 +2,7 @@
 'use strict';
 var $scope;
 var $rootScope;
+var element;
 
 describe('billingFormDirective'.bold.underline.blue, function () {
   var broadcastStub;
@@ -29,7 +30,7 @@ describe('billingFormDirective'.bold.underline.blue, function () {
       });
       $scope.save = sinon.stub();
       var tpl = directiveTemplate.attribute('billing-form');
-      var element = $compile(tpl)($scope);
+      element = $compile(tpl)($scope);
       $scope.$digest();
       $scope.$broadcast = broadcastStub;
       element.isolateScope();
@@ -38,6 +39,27 @@ describe('billingFormDirective'.bold.underline.blue, function () {
 
   it('should set active account on local scope', function () {
     expect($scope.activeAccount).to.equal($rootScope.dataApp.data.activeAccount);
+  });
+
+  describe('on animated panel change', function () {
+    it('should set footer if panel is billingForm', function () {
+      $scope.SEMC.showFooter = false;
+      $scope.$emit('changed-animated-panel', 'billingForm');
+      $scope.$digest();
+      expect($scope.SEMC.showFooter).to.equal(true);
+    });
+    it('should unset footer if panel is billingForm', function () {
+      $scope.$emit('changed-animated-panel', 'changePaymentForm');
+      $scope.$digest();
+      expect($scope.SEMC.showFooter).to.equal(false);
+    });
+  });
+
+  it('should set the footer to true when the scope gets destroyed', function () {
+    $scope.SEMC.showFooter = false;
+    $scope.$emit('$destroy');
+    $scope.$digest();
+    expect($scope.SEMC.showFooter).to.equal(true);
   });
 
   describe('actions', function () {
@@ -54,7 +76,6 @@ describe('billingFormDirective'.bold.underline.blue, function () {
         $scope.actions.cancel();
         sinon.assert.calledOnce(broadcastStub);
         sinon.assert.calledWith(broadcastStub, 'go-to-panel', 'billingForm', 'back');
-        expect($scope.SEMC.showFooter).to.equal(true);
       });
     });
   });
