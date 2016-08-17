a0609b5c9c319ad319567db42b9f2bf9e8c4f2a1
runnabro <tli415@gmail.com>
2016-08-17 13:03:37 -0700

    add exited early flag

--- client/services/featureFlagService.js
+++ client/services/featureFlagService.js
@@ -10,6 +10,7 @@ function featureFlags(
     aha: false,
     aha0: false, // step 1: create sandbox
     aha1: false, // step 2: working repo config
+    aha1ExitedEarly: false, // step 2: if the user left the flow before getting a running config
     aha2: false, // step 3: add branch
     aha3: false, // step 4: runnabot
     ahaSidebar: false, // toggle sidebar
