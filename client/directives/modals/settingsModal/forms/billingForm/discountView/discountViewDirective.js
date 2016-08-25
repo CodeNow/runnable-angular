7656acde48dacce0dd57d50c5d670f614b609d90
Myztiq <kahn@runnable.com>
2016-08-25 09:25:51 -0700

    Fixed display date.

--- client/directives/modals/settingsModal/forms/billingForm/discountView/discountViewDirective.js
+++ client/directives/modals/settingsModal/forms/billingForm/discountView/discountViewDirective.js
@@ -15,7 +15,7 @@ function discountView(
     link: function ($scope) {
       $scope.getDiscountText = function () {
         if ($scope.hasDuration) {
-          return $scope.discount.coupon.percentOff + '% off until ' + moment($scope.discount.end).format('MMM Mo, YYYY');
+          return $scope.discount.coupon.percentOff + '% off until ' + moment($scope.discount.end).format('MMM Do, YYYY');
         }
         return $scope.discount.coupon.percentOff + '% off for ' + $scope.discount.coupon.durationInMonths + ' months';
       };
