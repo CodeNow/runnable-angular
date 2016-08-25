9e8b5da6dca0e98e4f34bfc2d759b3db9273296a
thejsj <jorge.silva@thejsj.com>
2016-08-25 11:34:10 -0700

    Add showing card errors

--- client/services/billingService.js
+++ client/services/billingService.js
@@ -10,6 +10,19 @@ function savePaymentMethod(
   errs,
   keypather
 ) {
+
+  var coerce400ResponseError = function (resData) {
+    var message = keypather.get(resData, 'message');
+    if (message && message.match(/StripeCardError/)) {
+      var errorMessageParts = message.match(/(StripeCardError)(\:\W)(.*)/);
+      var newErrorMessage = errorMessageParts[3] || 'Unknown card error';
+      var newError = new Error(newErrorMessage);
+      newError.type = 'card_error';
+      throw newError;
+    }
+    return errs.handler(new Error(keypather.get(resData, 'error')));
+  };
+
   return function (stripeToken) {
     return $http({
       method: 'post',
@@ -21,12 +34,16 @@ function savePaymentMethod(
         stripeToken: stripeToken
       }
     })
+      /**
+       * Catch any errors related to HTTP request, allow certain validation
+       * errors to be thrown
+       */
+      .catch(errs.handler)
       .then(function (res) {
         if (res.status < 300) {
           return res.data;
         }
-        throw new Error(keypather.get(res, 'data.error'));
-      })
-      .catch(errs.handler);
+        return coerce400ResponseError(res.data);
+      });
   };
 }
