d723db8c0be1ab81d44be7fd6705713ec5c76f1c
Nathan219 <Nathan219@gmail.com>
2016-09-27 16:13:23 -0700

    adding console.logs

--- client/services/intercomService.js
+++ client/services/intercomService.js
@@ -7,25 +7,30 @@ function intercom(
   $window,
   $rootScope
 ) {
+  console.log('intercom attaching');
   $rootScope.$on('intercom::buildFailed', function () {
+    console.log('intercom::buildFailed');
     $window.Intercom(
       'showNewMessage',
       'Fudge! This thing won’t build my container. Can you fix it?'
     );
   });
   $rootScope.$on('intercom::confirmedSetup', function () {
+    console.log('intercom::confirmedSetup');
     $window.Intercom(
       'showNewMessage',
       'Hey! I need help setting up my application.'
     );
   });
   $rootScope.$on('intercom::migrating', function () {
+    console.log('intercom::migrating');
     $window.Intercom(
       'showNewMessage',
       'Help! I\'m having issues with migration.'
     );
   });
   $rootScope.$on('intercom::pawsd', function () {
+    console.log('intercom::pawsd');
     $window.Intercom(
       'showNewMessage',
       'Can you help me back into Runnable?'
