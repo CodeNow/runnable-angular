55018187c7e4c209d24e067edff3c3f1672e8d6a
tosih <sohail@runnable.com>
2016-09-27 16:51:13 -0700

    Fixed getAllBranches to get more pages.

--- client/controllers/controllerInstances.js
+++ client/controllers/controllerInstances.js
@@ -210,7 +210,29 @@ function ControllerInstances(
   this.getAllBranches = function (instance) {
     return promisify(currentOrg.github, 'fetchRepo')(instance.getRepoName())
       .then(function (repo) {
-        return promisify(repo, 'fetchBranches')();
+        var allBranches = [];
+
+        function fetchPage(page) {
+          return promisify(repo, 'fetchBranches')({
+            page: page
+          }).then(function (branchesCollection) {
+            allBranches = allBranches.concat(branchesCollection.models);
+            // recursive until result set returns fewer than
+            // 100 repos, indicating last paginated result
+            if (branchesCollection.models.length < 100) {
+              return allBranches;
+            }
+            return fetchPage(page + 1);
+          });
+        }
+        return fetchPage(1)
+          .then(function (branchArray) {
+            var branches = repo.newBranches(branchArray, {
+              noStore: true
+            });
+            repo.branches = branches;
+            return branches;
+          });
       });
   };
 
