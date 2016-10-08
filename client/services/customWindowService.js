7f9e9d0d662e78b274beb2d8a48944126d1fc4c8
Myztiq <kahn@runnable.com>
2016-10-07 17:42:13 -0700

    Fixed window positioning

--- client/services/customWindowService.js
+++ client/services/customWindowService.js
@@ -7,6 +7,7 @@ function customWindowService(
   keypather
 ) {
   return function (targetUrl, options) {
+    var topBar = window.outerHeight - window.innerHeight;
     var padding = 60;
     var width = window.innerWidth - padding - padding;
     var height = window.innerHeight - padding - padding - 50;
@@ -14,10 +15,8 @@ function customWindowService(
       width = options.width;
       height = options.height;
     }
-
-    var top = window.screenTop + height - (height / 2);
-    var left = window.screenLeft + width - (width / 2);
-
+    var top = window.screenTop + ((window.innerHeight + topBar) / 2) - (height / 2);
+    var left = window.screenLeft + (window.innerWidth / 2) - (width / 2);
     return window.open(targetUrl, 'page', 'toolbar=0,scrollbars=1,location=0,statusbar=0,menubar=0,resizable=0,width=' + width + ',height=' + height + ',left=' + left + ',top=' + top + ',titlebar=yes');
   };
 }
