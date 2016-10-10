4881f2b9137bf0e658b5524340c90238e75bec58
runnabro <tli415@gmail.com>
2016-10-10 14:37:08 -0700

    fix GitHub primer event

--- client/directives/githubAuth/githubAuthController.js
+++ client/directives/githubAuth/githubAuthController.js
@@ -5,7 +5,18 @@ require('app')
 
 function GithubAuthController(
   $rootScope,
+  $window,
+  eventTracking,
   keypather
 ) {
+  var GAC = this;
+
   keypather.set($rootScope, 'dataApp', {});
+
+  GAC.actions = {
+    confirmGitHub: function () {
+      eventTracking.trackPrimer();
+      $window.location = 'https://github.com/settings/connections/applications/d42d6634d4070c9d9bf9';
+    }
+  };
 }
