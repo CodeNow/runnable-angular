c7bdde41192d1e479cf62144192eca9b7f102c1a
Myztiq <kahn@runnable.com>
2016-10-12 13:50:50 -0700

    Added display name to segment

--- client/services/serviceEventTracking.js
+++ client/services/serviceEventTracking.js
@@ -204,12 +204,18 @@ function EventTracking(
       '$email': keypather.get(userJSON, 'email')
     });
 
+    var fullName = firstName + ' ' + lastName;
+    if (fullName === ' ') {
+      fullName = data.name;
+    }
+
     // Segment
     ETS.analytics.ready(function () {
       ETS.analytics.identify(data.name, {
         firstName: firstName,
         lastName: lastName,
         username: data.name,
+        displayName: fullName,
         email: keypather.get(userJSON, 'email'),
         createdAt: keypather.get(userJSON, 'created'),
         avatar: keypather.get(userJSON, 'gravatar')
