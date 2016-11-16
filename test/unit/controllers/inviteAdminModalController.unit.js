29f5d56895c829d3c5865dae922412fe6a8e7e58
runnabro <tli415@gmail.com>
2016-11-15 16:23:50 -0800

    fix tests

--- test/unit/controllers/inviteAdminModalController.unit.js
+++ test/unit/controllers/inviteAdminModalController.unit.js
@@ -10,7 +10,7 @@ var $elScope;
 var $controller;
 var $rootScope;
 var $templateCache;
-var DEFAULT_MESSAGE = 'Join me on Runnable, where we can run the code in CodeNow\'s repositories on demand.\n\nI need your admin permissions to enable some features. Thanks!';
+var DEFAULT_MESSAGE = 'Join me on Runnable, where we can run the code in CodeNow’s repositories on demand.\n\nI need your admin permissions to enable some features. Thanks!';
 
 describe('InviteAdminModalController'.bold.underline.blue, function () {
   var ctx;
