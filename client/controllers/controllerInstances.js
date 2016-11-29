d6b5443b6804f562e162b3b288b9314a85d2d780
thejsj <jorge.silva@thejsj.com>
2016-11-29 15:41:33 -0800

    Exposing vars from controller

--- client/controllers/controllerInstances.js
+++ client/controllers/controllerInstances.js
@@ -28,7 +28,8 @@ function ControllerInstances(
   user
 ) {
   var CIS = this;
-  var userName = $state.params.userName;
+  CIS.userName = $state.params.userName;
+  CIS.instanceName = $state.params.instanceName;
   CIS.isInGuide = ahaGuide.isInGuide;
   CIS.shouldShowDemoSelector = demoRepos.shouldShowDemoSelector;
   CIS.isAddingFirstBranch = ahaGuide.isAddingFirstBranch;
@@ -73,7 +74,7 @@ function ControllerInstances(
     .then(function (instancesByPod) {
 
       // If the state has already changed don'  t continue with old data. Let the new one execute.
-      if (userName !== $state.params.userName) {
+      if (CIS.userName !== $state.params.userName) {
         return;
       }
       CIS.instancesByPod = instancesByPod;
@@ -123,7 +124,7 @@ function ControllerInstances(
           });
       }
 
-      setLastOrg(userName);
+      setLastOrg(CIS.userName);
 
       var instanceName = keypather.get(targetInstance, 'attrs.name');
       if ($state.current.name !== 'base.instances.instance') {
@@ -146,14 +147,15 @@ function ControllerInstances(
 
   this.checkAndLoadInstance = function (instanceName) {
     if (instanceName) {
+      CIS.demoInstance = instanceName;
       return $state.go('base.instances.instance', {
         instanceName: instanceName,
-        userName: userName
+        userName: CIS.userName
       }, {location: 'replace'});
     }
     if (!featureFlags.flags.containersViewTemplateControls) {
       return $state.go('base.config', {
-        userName: userName
+        userName: CIS.userName
       }, {location: 'replace'});
     }
   };
