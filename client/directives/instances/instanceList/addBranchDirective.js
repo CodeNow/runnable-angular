3068fbddcd0e7cd8447d9a60df941b729214fd94
thejsj <jorge.silva@thejsj.com>
2016-11-30 14:26:47 -0500

    Removing functionality for when not a demo repo

--- client/directives/instances/instanceList/addBranchDirective.js
+++ client/directives/instances/instanceList/addBranchDirective.js
@@ -16,8 +16,7 @@ function addBranch(
     templateUrl: 'addBranchView',
     scope: {
       userName: '=',
-      instance: '=',
-      isDemoRepo: '='
+      instance: '='
     },
     link: function ($scope, element, attrs) {
       $scope.getBranchCloneCopyText = function () {
@@ -30,19 +29,6 @@ function addBranch(
         var repoOwner = completeRepoName[0];
         var repoName = completeRepoName[1];
         return github.createNewBranch(repoOwner, repoName, acv.attrs.commit, 'my-branch')
-          .catch(errs.handler);
-      };
-
-      $scope.forkFirstBranch = function () {
-        var acv = $scope.instance.contextVersion.getMainAppCodeVersion();
-        return fetchRepoBranches(acv.githubRepo)
-          .then(function (branches) {
-            // Gets first brnach alphabetically
-            var firstBranch = branches.models[0];
-            var branchName = firstBranch.attrs.name;
-            var sha = firstBranch.attrs.commit.sha;
-            return promisify($scope.instance, 'fork')(branchName, sha);
-          })
           .catch(errs.handler);
       };
     }
