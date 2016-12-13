c917a40f4a0f0553dd2d6f8f97768ffcb6c69a49
Henry Mollman <henry@runnable.com>
2016-12-13 13:49:20 -0800

    Removed unused functions

--- client/services/githubService.js
+++ client/services/githubService.js
@@ -56,55 +56,6 @@ function github(
       return makeGhRequest(ghRequest);
     },
 
-    createCommit: function (repoOwner, repoName, commitSha, treeSha) {
-      var ghRequest = {
-        method: 'post',
-        url: githubAPIUrl + '/repos/' + repoOwner + '/' + repoName + '/git/commits',
-        data: {
-          message: 'This commit was made by Runnable!',
-          tree: treeSha,
-          parents: [commitSha]
-        }
-      };
-      return makeGhRequest(ghRequest);
-    },
-
-    updateRef: function (repoOwner, repoName, branchName, commitSha) {
-      var ghRequest = {
-        method: 'patch',
-        url: githubAPIUrl + '/repos/' + repoOwner + '/' + repoName + '/git/refs/heads/' + branchName,
-        data: {
-          sha: commitSha
-        }
-      };
-      return makeGhRequest(ghRequest);
-    },
-
-    createNewTreeFromSha: function (repoOwner, repoName, sha) {
-      var ghRequest = {
-        method: 'post',
-        url: githubAPIUrl + '/repos/' + repoOwner + '/' + repoName + '/git/trees',
-        data: {
-          base_tree: sha,
-          tree: [{
-            path: 'test-file.txt',
-            mode: '100644',
-            type: 'blob',
-            content: 'File created by runnable!'
-          }]
-        }
-      };
-      return makeGhRequest(ghRequest);
-    },
-
-    getTreeForCommit: function (repoOwner, repoName, sha) {
-      var ghRequest = {
-        method: 'get',
-        url: githubAPIUrl + '/repos/' + repoOwner + '/' + repoName + '/git/commits/' + sha
-      };
-      return makeGhRequest(ghRequest);
-    },
-
     createPR: function (repoOwner, repoName, branchToMergeTo, branchToMergeFrom) {
       var ghRequest = {
         method: 'post',
