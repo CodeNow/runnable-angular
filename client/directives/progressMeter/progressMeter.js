ce5b03a7ab90375bc5559921a0b0a9e7cdcf650b
Henry Mollman <henry@runnable.com>
2017-01-09 13:57:27 -0800

    Dynamic tick marks

--- client/directives/progressMeter/progressMeter.js
+++ client/directives/progressMeter/progressMeter.js
@@ -16,7 +16,16 @@ function progressMeter(
       meterMax: '='
     },
     link: function (scope, elem, attrs) {
-      scope.ticks = new Array(scope.meterMax || 6);
+      scope.meterMax = scope.meterMax || 1;
+      scope.ticks = new Array(scope.meterMax);
+      scope.getClass = function () {
+        var step = 'meter-' + scope.step;
+        var meterMax = 'meter-max-' + scope.meterMax;
+        var classObj = {};
+        classObj[step] = true;
+        classObj[meterMax] = true;
+        return classObj;
+      };
       scope.$watch(scope.meterMax, function () {
         scope.ticks = new Array(scope.meterMax);
       });
