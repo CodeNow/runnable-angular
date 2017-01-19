301a23b77ac9af8f7be971aa3e3499a260ecbf69
Praful Rana <prafulrana@gmail.com>
2017-01-19 14:54:00 -0800

    Update chooseOrganizationModalController.js

--- client/directives/modals/modalChooseOrganization/chooseOrganizationModalController.js
+++ client/directives/modals/modalChooseOrganization/chooseOrganizationModalController.js
@@ -192,6 +192,9 @@ function ChooseOrganizationModalController(
 
   COMC.selectedOrgName = null;
   COMC.pollForDockCreated = function (whitelistedDock, selectedOrgName) {
+    eventTracking.spunUpInfrastructure();
+    if (selectedOrgName.toLowerCase() != org.oauthName().toLowerCase())
+      eventTracking.spunUpInfrastructureForOrg();
     loading('waitingForDockCreated', true);
     COMC.selectedOrgName = selectedOrgName;
     COMC.cancelPollingForDockCreated();
