85c4be6fcfb4ec524ca443059e7a4e260f12925b
Taylor Dolan <taylordolan@me.com>
2017-01-24 17:09:16 -0800

    new add team button + add team step

--- client/services/featureFlagService.js
+++ client/services/featureFlagService.js
@@ -16,6 +16,7 @@ function featureFlags(
     cardStatus: false,
     connections: false,
     contingencyPlan: false,
+    demoAddTeamStep: false,
     demoAutoAddBranch: true,
     demoMultiTier: true,
     dockerCompose: true,
