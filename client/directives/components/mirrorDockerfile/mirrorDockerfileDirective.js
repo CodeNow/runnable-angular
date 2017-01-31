2268ebb35a6ec4d3ae55bf9074451d1e745c62c9
Henry Mollman <henry@runnable.com>
2017-01-31 15:12:42 -0800

    Added support for docker compose files not in root

--- client/directives/components/mirrorDockerfile/mirrorDockerfileDirective.js
+++ client/directives/components/mirrorDockerfile/mirrorDockerfileDirective.js
@@ -24,8 +24,18 @@ function mirrorDockerfileDirective(
       loading.reset('mirrorDockerfile');
       $scope.dockerfile = {};
       $scope.viewState = {
-        showAddDockerfile: false
+        showAddDockerfile: false,
+        showAddDockerComposeFile: false
       };
+      $scope.toggleDockerfileDisplay = function (fileType) {
+        if (fileType === 'Docker Compose') {
+          $scope.viewState.showAddDockerfile = false;
+          $scope.viewState.showAddDockerComposeFile = true;
+        } else {
+          $scope.viewState.showAddDockerfile = true;
+          $scope.viewState.showAddDockerComposeFile = false;
+        }
+      }
       $scope.$watch('MDC.repo.attrs.name', function () {
         $scope.MDC.resetDockerfilePaths();
         loading('mirrorDockerfile', true);
