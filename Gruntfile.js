f1f3329639e28f6f8b65936b118d2c1c564f9011
thejsj <jorge.silva@thejsj.com>
2017-02-08 11:11:59 -0800

    Add region to AWS gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -354,6 +354,7 @@ module.exports = function(grunt) {
         accessKeyId: process.env.AWS_ACCESS_KEY,
         secretAccessKey: process.env.AWS_SECRET_KEY,
         bucket: process.env.AWS_BUCKET,
+        region: process.env.AWS_REGION,
         createBucket: true,
         enableWeb: true,
         gzip: true,
