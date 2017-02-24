df299afb79c6aab5cd43f2705d99f08115faab67
Taylor Dolan <taylordolan@me.com>
2017-02-24 15:51:55 -0800

    add feature flag for empty state

--- client/services/featureFlagService.js
+++ client/services/featureFlagService.js
@@ -18,7 +18,8 @@ function featureFlags(
     contingencyPlan: false,
     demoAutoAddBranch: true,
     demoOrg: true,
-    demoOrgSelectUpdate: false,
+    demoOrgSelectUpdate: false, // updated org select for reduced github auth flow
+    demoNoOrgs: false, // simulates no orgs in the org select upate
     demoPersistentAddTeam: false,
     demoUrlPolling: true,
     dockerCompose: true,
