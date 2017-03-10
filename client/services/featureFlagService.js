0135e9d90c608f49d6b437fc475a2cf93fbd2fe7
Taylor Dolan <taylordolan@me.com>
2017-03-09 17:41:43 -0800

    mock up compose nav

--- client/services/featureFlagService.js
+++ client/services/featureFlagService.js
@@ -14,6 +14,7 @@ function featureFlags(
     autoIsolationSetup: false,
     backup: false,
     cardStatus: false,
+    composeNav: false,
     connections: false,
     contingencyPlan: false,
     demoAutoAddBranch: true,
