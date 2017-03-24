3b8203023a4cfce0dba103ebdf96683d5cb1f993
Henry Mollman <henry@runnable.com>
2017-03-24 15:37:10 -0700

    Create new directive for compose file path

---
+++ client/directives/components/mirrorDockerfile/composeFilePath.js
@@ -0,0 +1,22 @@
+'use strict';
+
+require('app')
+  .directive('composeFilePath', composeFilePath);
+
+/**
+ * @ngInject
+ */
+function composeFilePath(
+  
+) {
+  return {
+    restrict: 'A',
+    templateUrl: 'composeFilePathView',
+    scope: {
+      pathEnabled: '=?',
+      type: '@',
+      branchName: '=',
+      fullRepo: '=',
+    }
+  };
+}
