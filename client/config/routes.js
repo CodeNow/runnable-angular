827ce01a812711c23169d773dd925658c51e5cc5
Myztiq <kahn@runnable.com>
2017-03-31 14:57:00 -0700

    Fix state change

--- client/config/routes.js
+++ client/config/routes.js
@@ -383,6 +383,8 @@ module.exports = [
       testInstance: function (
         $stateParams,
         $state,
+        $rootScope,
+        $timeout,
         fetchInstanceByName,
         fetchInstanceTestHistoryBySha
         ) {
@@ -402,6 +404,12 @@ module.exports = [
                     });
                   }
                   instance.containerHistory = history;
+                  var stopListening = $rootScope.$on('$stateChangeStart', function (event, toState) {
+                    stopListening();
+                    if (toState.state !== 'base.instances.instance-test-sha') {
+                      delete instance.containerHistory;
+                    }
+                  });
                   return instance;
                 });
             });
