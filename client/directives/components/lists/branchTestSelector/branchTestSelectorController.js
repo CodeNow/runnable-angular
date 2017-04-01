f409653d120123d2a29b6006cfd2c1fa5f7c924d
damienrunnable <damien@runnable.com>
2017-03-31 19:52:18 -0700

    SAN-6042 Still doesn't work but work so far.

--- client/directives/components/lists/branchTestSelector/branchTestSelectorController.js
+++ client/directives/components/lists/branchTestSelector/branchTestSelectorController.js
@@ -7,12 +7,50 @@ function BranchTestSelectorController(
   $scope,
   $rootScope,
   $state,
-  keypather
+  keypather,
+  loading,
+  promisify,
+  fetchCommitData,
+  updateInstanceWithNewBuild,
+  updateInstanceWithNewAcvData
+
 ) {
   var BTSC = this;
+  BTSC.appCodeVersion = BTSC.instance.contextVersion.getMainAppCodeVersion();
+  BTSC.branch = fetchCommitData.activeBranch(BTSC.appCodeVersion);
+
+  BTSC.hasCommitBeenUpdated = function () {
+    var newCommitSha = keypather.get(BTSC, 'commit.attrs.sha');
+    var oldCommitSha = keypather.get(BTSC, 'appCodeVersion.attrs.commit');
+    return newCommitSha && newCommitSha !== oldCommitSha;
+  };
+
+  BTSC.updateInstance = function () {
+    if (BTSC.hasCommitBeenUpdated()) {
+      loading('main', true);
+      // promisify(BTSC.instance.build, 'deepCopy')()
+      //   .then(function (build) {
+      //     return updateInstanceWithNewBuild(
+      //       BTSC.instance,
+      //       build,
+      //       true
+      //     );
+      //   })
+      // return updateInstanceWithNewAcvData(BTSC.instance, BTSC.appCodeVersion, {
+      //   repo: BTSC.appCodeVersion.githubRepo,
+      //   acv: BTSC.appCodeVersion,
+      //   branch: fetchCommitData.activeBranch(BTSC.appCodeVersion),
+      //   useLatest: BTSC.appCodeVersion.attrs.useLatest
+      // })
+      //   .finally(function () {
+      //     loading('main', false);
+      //   });
+    }
+  };
 
   BTSC.selectCommit = function (commit) {
     BTSC.commit = commit;
+    BTSC.updateInstance();
     $scope.$emit('test-commit::selected', commit);
     $rootScope.$broadcast('close-popovers');
   };
@@ -22,7 +60,7 @@ function BranchTestSelectorController(
     BTSC.commit = firstCommit;
     BTSC.selectCommit(firstCommit);
     $state.go('base.instances.instance-test-sha', {
-      instanceName: BTSC.instanceName,
+      instanceName: BTSC.instance.attrs.name,
       sha: firstCommit.attrs.sha
     });
     return;
