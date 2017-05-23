ba9fc7c8ee2bef07ad1f7de95b1d4bf27cf6c0d0
Taylor Dolan <taylordolan@me.com>
2017-05-23 15:23:16 -0700

    add feature flag for branch selector

--- client/services/featureFlagService.js
+++ client/services/featureFlagService.js
@@ -14,6 +14,7 @@ function featureFlags(
     autoIsolationSetup: false,
     backup: false,
     cardStatus: false,
+    composeDefaultBranch: false,
     composeEditing: false,
     composeInstance: true,
     composeNav: true,
