422e1459d509933d7c4617ade5f6e1ab29c49ab6
Taylor Dolan <taylordolan@me.com>
2017-06-19 11:16:45 -0700

    remove merge marks

--- client/services/featureFlagService.js
+++ client/services/featureFlagService.js
@@ -49,10 +49,7 @@ function featureFlags(
     inviteFlows: false,
     isPersonalAccount: false, // if account is a personal account
     kubernetes: false,
-<<<<<<< HEAD
-=======
     limitBranches: false,
->>>>>>> 30b18f7c887d2f6b554a04429ec89cfb84e6286d
     mixAndMatchBranches: false,
     multilineFnR: false,
     multilineStartCmd: false,
