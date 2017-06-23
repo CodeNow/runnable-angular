6574edb596082b7c2e62ffabb92f9e6b8b38f595
Nathan219 <Nathan219@gmail.com>
2017-06-22 17:13:34 -0700

    Add service to use the new createMultiCluster route

--- client/services/createNewCluster.js
+++ client/services/createNewCluster.js
@@ -2,6 +2,7 @@
 
 require('app')
   .factory('createNewCluster', createNewCluster)
+  .factory('createNewMultiClusters', createNewMultiClusters)
   .factory('redeployClusterMasterInstance', redeployClusterMasterInstance);
 
 function createNewCluster(
@@ -22,6 +23,28 @@ function createNewCluster(
     return $http({
       method: 'post',
       url: configAPIHost + '/docker-compose-cluster',
+      data: data
+    });
+  };
+}
+
+function createNewMultiClusters(
+  $http,
+  configAPIHost
+) {
+  return function (repo, branch, filePath, name, githubId, isTesting, testReporters) {
+    var data = {
+      repo: repo,
+      branch: branch,
+      filePath: filePath,
+      name: name,
+      githubId: githubId,
+      isTesting: isTesting,
+      testReporters: testReporters
+    };
+    return $http({
+      method: 'post',
+      url: configAPIHost + '/docker-compose-cluster/multi',
       data: data
     });
   };
