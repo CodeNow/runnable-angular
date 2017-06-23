04b02162a4191791c27fee0f1d36b14cfadc501f
Henry Mollman <henry@runnable.com>
2017-06-23 13:32:40 -0700

    Fix tests

--- test/unit/directives/newContainer/newContainerController.unit.js
+++ test/unit/directives/newContainer/newContainerController.unit.js
@@ -528,7 +528,7 @@ describe('NewContainerController'.bold.underline.blue, function () {
     });
   });
 
-  describe.only('docker compose test cluster creation', function () {
+  describe('docker compose test cluster creation', function () {
     beforeEach(function () {
       featureFlagsMock.composeNewService = true;
       NCC.state.dockerComposeFile = null;
@@ -550,13 +550,12 @@ describe('NewContainerController'.bold.underline.blue, function () {
     it('should create one test cluster', function () {
       NCC.createComposeCluster();
       sinon.assert.calledOnce(createNewClusterStub);
-      sinon.assert.calledWithMatch(createNewClusterStub,
+      sinon.assert.calledWith(createNewClusterStub,
         NCC.state.repo.attrs.full_name,
         branchName,
         NCC.state.dockerComposeTestFile.path,
         NCC.state.instanceName,
-        mockCurrentOrg.github.attrs.id,
-        clusterOpts
+        mockCurrentOrg.github.attrs.id
       );
     });
   });
